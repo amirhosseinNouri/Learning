fda413211dea831dd2e2f20556cf1ffe232f2e95
Amirhossein <amir.h.nouri2000@gmail.com>
2020-12-06 00:51:13 +0330

    import and export

---
+++ tutorial-second/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+// import { name, age, person } from "./data";
+// import * as data from './data'
+
+// const {name,age,person} = data;
+
+import Banner from './components/Header/Banner'
+
+
+const App = () => (
+  <section>
+    <Banner></Banner>
+  </section>
+);
+
+
+
+export default App;
---
+++ tutorial-second/src/components/Header/Banner.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { name } from "../../data";
+
+export default function Banner() {
+  return (
+    <div>
+      <h1>Hello from banner</h1>
+      <p>{name}</p>
+    </div>
+  );
+}
