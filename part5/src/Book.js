79b89b970da36aa9165502e756dc0bc03d39014b
Amirhossein <amir.h.nouri2000@gmail.com>
2020-12-06 20:08:00 +0330

    state

--- part5/src/Book.js
+++ part5/src/Book.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 export default class Book extends Component {
   render() {
     // console.log(this.props);
-    const { book, author } = this.props.book;
+    const { book, author } = this.props.info;
     return (
       <article>
         <h3>Book: {book}</h3>
--- part5/src/BookList.js
+++ part5/src/BookList.js
@@ -2,23 +2,34 @@ import React, { Component } from "react";
 import Book from "./Book";
 
 export default class BookList extends Component {
-  books = [
-    {
-      book: "book numner one",
-      author: "amir",
-    },
-    {
-      book: "book numner two",
-      author: "bob",
-    },
-    
-  ];
+  state = {
+    books: [
+      {
+        id : 1,
+        book: "book numner one",
+        author: "amir",
+      },
+      {
+        id : 2,
+        book: "book numner two",
+        author: "bob",
+      },
+      {
+        id : 3,
+        book: "book numner three",
+        author: "john",
+      },
+    ],
+  };
 
   render() {
+    // const books = this.state.books.map((item) =>item.book)
+    // console.log(books);
+
     return (
       <section>
         <h3>This is our book list</h3>
-        <Book book={this.books[0]}></Book>
+        {this.state.books.map(item => <Book key={item.id} info={item}></Book>)}
       </section>
     );
   }
