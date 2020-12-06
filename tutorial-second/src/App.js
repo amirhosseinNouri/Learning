96baedc08ba9e0e695e1833e11031ac656a3d9b7
Amirhossein <amir.h.nouri2000@gmail.com>
2020-12-06 06:42:44 +0330

    tutorial second

--- tutorial-second/src/App.js
+++ tutorial-second/src/App.js
@@ -1,18 +1,29 @@
-import React from "react";
 // import { name, age, person } from "./data";
-// import * as data from './data'
-
-// const {name,age,person} = data;
-
-import Banner from './components/Header/Banner'
-
-
-const App = () => (
-  <section>
-    <Banner></Banner>
-  </section>
-);
+import React, { Component } from "react";
+import * as data from './data'
+import Banner from "./components/Header/Banner";
+const { name, age, person } = data;
 
 
+
+
+// const App = () => (
+//   <section>
+//     <Banner></Banner>
+//   </section>
+// );
+
+class App extends Component {
+  render() {
+    return (
+      <section>
+        <Banner></Banner>
+        <p>{name}</p>
+        <p>{age}</p>
+        <p>{person.name}</p>
+      </section>
+    );
+  }
+}
 
 export default App;
