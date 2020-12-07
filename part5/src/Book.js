7e4c4bcde6c4dd25e0d4f577c2aae1577c7a6e76
Amirhossein <amir.h.nouri2000@gmail.com>
2020-12-07 19:13:14 +0330

    event handling

--- part5/src/Book.js
+++ part5/src/Book.js
@@ -1,6 +1,28 @@
 import React, { Component } from "react";
 
 export default class Book extends Component {
+
+  constructor(props){
+    super(props)
+    this.handleClick = this.handleClick.bind(this)
+    this.state = {
+      count : 1
+    }
+
+  }
+
+
+
+  // handleClick = () =>{
+  //   console.log('clicked');
+  //   console.log(this.state.count);
+  // }
+
+  handleClick(){
+    console.log('clicked');
+    console.log(this.state.count);
+  }
+
   render() {
     // console.log(this.props);
     const { img, title, author } = this.props.info;
@@ -10,6 +32,7 @@ export default class Book extends Component {
         <div>
           <h4>Title : {title}</h4>
           <h6>Author : {author}</h6>
+          <button type="button" onClick={this.handleClick}>Add to cart</button>
         </div>
       </article>
     );
