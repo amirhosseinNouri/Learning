65e9cf03e09e1c4713de893d9d88b9ada6ca0496
Amirhossein <amir.h.nouri2000@gmail.com>
2020-12-10 11:35:12 +0330

    controlled inputs and form submission

--- basics3/src/App.js
+++ basics3/src/App.js
@@ -1,71 +1,58 @@
 import React, { Component } from "react";
 import "./App.css";
-import PropTypes from "prop-types";
 
-const Person = ({ person }) => {
-  const { name, img, age, info } = person;
-  return (
-    <article>
-      <img src={img} alt="person" />
-      <h2>Name : {name}</h2>
-      <h3>Age : {age}</h3>
-      <h3>Info : {info}</h3>
-    </article>
-  );
-};
-
-Person.propTypes = {
-  person : PropTypes.shape({
-  img: PropTypes.string.isRequired,
-  name: PropTypes.string.isRequired,
-  age: PropTypes.number.isRequired,
-  info: PropTypes.string.isRequired,
-  })
-
-};
-
-person,defaultProps = {
-  
-}
-
-// Person.defaultProps = {
-//   img: "https://randomuser.me/api/portraits/men/45.jpg",
-//   name: "james doe",
-//   age: 34,
-//   info: "default info about the person",
-// };
-
-class PersonList extends Component {
+class Form extends Component {
   state = {
-    people: [
-      {
-        id: 1,
-        name: "amir",
-        age: 22,
-        img: "https://randomuser.me/api/portraits/men/75.jpg",
-      },
-      {
-        id: 2,
-        name: "bob",
-        age: 27,
-        img: "https://randomuser.me/api/portraits/men/72.jpg",
-      },
-      {
-        id: 3,
-        name: "peter",
-        age: 27,
-        img: "https://randomuser.me/api/portraits/men/82.jpg",
-        info: "some info about peter",
-      },
-    ],
+    firstName: "",
+    lastName: "",
+    people: [],
+  };
+
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: [event.target.value],
+    });
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    const firstName = this.state.firstName;
+    const lastName = this.state.lastName;
+
+    if (firstName && lastName) {
+      const person = ` ${firstName} ${lastName} `;
+      this.setState({
+        people: [...this.state.people, person],
+        firstName: "",
+        lastName: "",
+      });
+    }
   };
 
   render() {
     return (
       <section>
-        {this.state.people.map((person) => (
-          <Person key={person.id} person={person}></Person>
-        ))}
+        <article>
+          <form onSubmit={this.handleSubmit}>
+            <input
+              type="text"
+              name="firstName"
+              value={this.state.firstName}
+              onChange={this.handleChange}
+            ></input>
+            <input
+              type="text"
+              name="lastName"
+              value={this.state.lastName}
+              onChange={this.handleChange}
+            ></input>
+            <button type="submit">Suhmit</button>
+          </form>
+        </article>
+        <article>
+          <h1>People</h1>
+          <div>{this.state.people}</div>
+        </article>
       </section>
     );
   }
@@ -73,6 +60,6 @@ class PersonList extends Component {
 
 export default class App extends Component {
   render() {
-    return <PersonList></PersonList>;
+    return <Form></Form>;
   }
 }
