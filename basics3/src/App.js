06cdf87a590045754629ab0944194d033e9c0145
Amirhossein <amir.h.nouri2000@gmail.com>
2020-12-10 14:46:55 +0330

    uncontrolled forms and ref

--- basics3/src/App.js
+++ basics3/src/App.js
@@ -2,59 +2,33 @@ import React, { Component } from "react";
 import "./App.css";
 
 class Form extends Component {
-  state = {
-    firstName: "",
-    lastName: "",
-    people: [],
-  };
 
-  handleChange = (event) => {
-    this.setState({
-      [event.target.name]: [event.target.value],
-    });
-  };
 
-  handleSubmit = (event) => {
-    event.preventDefault();
-    const firstName = this.state.firstName;
-    const lastName = this.state.lastName;
+  handleSubmit = event => {
+    event.preventDefault()
+    const name = this.refs.myName
+    const nameValue = name.value
+    const email = this.email.value;
 
-    if (firstName && lastName) {
-      const person = ` ${firstName} ${lastName} `;
-      this.setState({
-        people: [...this.state.people, person],
-        firstName: "",
-        lastName: "",
-      });
-    }
-  };
+    const pText = this.refs.myText ;
+    pText.style.color = "red"
 
-  render() {
-    return (
+    console.log(nameValue , email , pText);
+
+  }
+
+
+  render(){
+    return(
       <section>
-        <article>
-          <form onSubmit={this.handleSubmit}>
-            <input
-              type="text"
-              name="firstName"
-              value={this.state.firstName}
-              onChange={this.handleChange}
-            ></input>
-            <input
-              type="text"
-              name="lastName"
-              value={this.state.lastName}
-              onChange={this.handleChange}
-            ></input>
-            <button type="submit">Suhmit</button>
-          </form>
-        </article>
-        <article>
-          <h1>People</h1>
-          <div>{this.state.people}</div>
-        </article>
+        <form onSubmit={this.handleSubmit}>
+          <input type="text"  ref="myName" />
+          <input type="email" ref={(orange)=> this.email= orange} />
+          <button type="submit">Submit</button>
+        </form>
+        <p ref="myText">Hello world</p>
       </section>
-    );
+    )
   }
 }
 
