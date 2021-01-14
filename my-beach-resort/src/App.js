9b517866c8626188fc87816e3596b0db1bcf6815
Amirhossein <amir.h.nouri2000@gmail.com>
2021-01-14 14:35:38 +0330

    Navbar component compeleted

--- my-beach-resort/src/App.js
+++ my-beach-resort/src/App.js
@@ -4,16 +4,19 @@ import Error from './pages/Error'
 import SingleRoom from './pages/SingleRoom'
 import Rooms from './pages/Rooms'
 import {Route, Switch} from 'react-router-dom'
+import Navbar from './components/Navbar'
 
 function App() {
   return (
-      
+    <>
+      <Navbar></Navbar>
       <Switch>
       <Route path="/" exact component={Home}></Route>
       <Route path="/rooms" exact component={Rooms}></Route>
       <Route path="/rooms/:slug" exact component={SingleRoom}></Route>
       <Route  component={Error}></Route>
       </Switch>
+    </>
   );
 }
 
