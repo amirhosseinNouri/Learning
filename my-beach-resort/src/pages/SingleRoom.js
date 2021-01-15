5232b235e55892e89ecf3152ada9595b3462b36f
Amirhossein <amir.h.nouri2000@gmail.com>
2021-01-15 14:28:32 +0330

    Working on single room

--- my-beach-resort/src/pages/SingleRoom.js
+++ my-beach-resort/src/pages/SingleRoom.js
@@ -1,7 +1,26 @@
 import React, { Component } from 'react'
+import defaultBcg from '../images/room-1.jpeg'
+import Hero from '../components/Hero'
+import Banner from '../components/Banner'
+import {Link} from 'react-router-dom'
+import {RoomContext} from '../Context'
 
 export default class SingleRoom extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            slug : this.props.match.prams.slug,
+            defaultBcg
+        }
+
+    }
+
+    static contextType = RoomContext;
+
     render() {
+        const {getRoom} = this.context
+        const room = getRoom(this.state.slug)
+        
         return (
             <div>
                 Single Room
