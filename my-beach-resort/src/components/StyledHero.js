0907d9eca4c3593a63170d0a503954846b232f9d
Amirhossein <amir.h.nouri2000@gmail.com>
2021-01-15 19:09:02 +0330

    SingleRoom Page done

--- my-beach-resort/src/components/StyledHero.js
+++ my-beach-resort/src/components/StyledHero.js
@@ -3,7 +3,7 @@ import defaultBcg from '../images/room-1.jpeg'
 
 const styledHero = styled.header`
    min-height : 60vh;
-    background : url(${props =>props.img ?  props.img : default}) center/cover no-repeat;
+    background : url(${props =>props.img ?  props.img : defaultBcg}) center/cover no-repeat;
     display : flex;
     align-items : center;
     justify-content : center;
--- my-beach-resort/src/pages/SingleRoom.js
+++ my-beach-resort/src/pages/SingleRoom.js
@@ -4,7 +4,7 @@ import Hero from "../components/Hero";
 import Banner from "../components/Banner";
 import { Link } from "react-router-dom";
 import { RoomContext } from "../Context";
-import StyledHero from '../components/StyledHero'
+import StyledHero from "../components/StyledHero";
 
 export default class SingleRoom extends Component {
   constructor(props) {
@@ -41,14 +41,48 @@ export default class SingleRoom extends Component {
       images,
       breakfast,
     } = room;
+    const [mainImage, ...otherImages] = images;
     return (
-      <StyledHero img={images[0] || this.state.defaultBcg}>
-        <Banner title={`${name} room`}>
-          <Link to="/rooms" className="btn-primary">
-            Back to rooms
-          </Link>
-        </Banner>
-      </StyledHero>
+      <>
+        <StyledHero img={mainImage || this.state.defaultBcg}>
+          <Banner title={`${name} room`}>
+            <Link to="/rooms" className="btn-primary">
+              Back to rooms
+            </Link>
+          </Banner>
+        </StyledHero>
+        <section className="single-room">
+          <div className="single-room-images">
+            {otherImages.map((item) => {
+              return <img src={item} alt={name} />;
+            })}
+          </div>
+          <div className="single-room-info">
+            <article className="desc">
+              <h3>details</h3>
+              <p>{description}</p>
+            </article>
+            <article className="info">
+              <h3>info</h3>
+              <h6>price : ${price}</h6>
+              <h6>Size : {size} SQFT</h6>
+              <h6>
+                max capacity :{capacity > 1 ? `${capacity} people` : `${capacity} person`}
+              </h6>
+              <h6>{pets ? "pets allowed" : "no pets allowed"}</h6>
+              <h6>{breakfast && "free breakfast included"}</h6>
+            </article>
+          </div>
+        </section>
+        <section className="room-extras">
+            <h6>Extras</h6>
+            <ul className="extras">
+                {extras.map((item, index) =>{
+                    return <li key={index}>- {item}</li>
+                })}
+            </ul>
+        </section>
+      </>
     );
   }
 }
