73bafe3cb1607cbbb3a644b916c56b8b3eca176e
Amirhossein <amir.h.nouri2000@gmail.com>
2021-01-15 14:19:01 +0330

    getRoom method added to context

--- my-beach-resort/src/Context.js
+++ my-beach-resort/src/Context.js
@@ -32,9 +32,14 @@ class RoomProvider extends Component {
     return tempItems;
   }
 
+  getRoom = (slug) => {
+    let tempRooms = [...this.state.rooms];
+    return tempRooms.find((item) => item.slug === slug);
+  };
+
   render() {
     return (
-      <RoomContext.Provider value={{ ...this.state }}>
+      <RoomContext.Provider value={{ ...this.state, getRoom: this.getRoom }}>
         {this.props.children}
       </RoomContext.Provider>
     );
