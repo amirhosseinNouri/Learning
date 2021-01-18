3ca133fa5aacb07488068d6a3dccfb66fc3e9c5b
Amirhossein <amir.h.nouri2000@gmail.com>
2021-01-18 11:14:54 +0330

    Room type filter added

--- my-beach-resort/src/components/RoomsFilter.js
+++ my-beach-resort/src/components/RoomsFilter.js
@@ -1,9 +1,58 @@
-import React from 'react'
+import React from "react";
+import { useContext } from "react";
+import { RoomConsumer, RoomContext } from "../Context";
+import Title from "./Title";
 
-export default function RoomsFilter() {
+const getUnique = (items, value) => {
+  return [...new Set(items.map((item) => item[value]))];
+};
+
+export default function RoomsFilter({rooms}) {
+  const context = useContext(RoomContext);
+  const {
+    handleChange,
+    type,
+    capacity,
+    price,
+    minPrice,
+    maxPrice,
+    minSize,
+    maxSize,
+    breakfast,
+    pets,
+  } = context;
+
+  // get unique types
+  let types = getUnique(rooms, "type");
+  // add all
+  types = ["all", ...types];
+  // create jsx
+  types = types.map((item, index) => {
     return (
-        <div>
-            Room Filter
+      <option value={item} key={index}>
+        {item}
+      </option>
+    );
+  });
+  return (
+    <section className="filter-container">
+      <Title title="search rooms"></Title>
+      <from className="filter-form">
+        {/* select type */}
+        <div className="form-group">
+          <label htmlFor="type">room type</label>
+          <select
+            name="type"
+            id="type"
+            value={type}
+            className="form-control"
+            onChange={handleChange}
+          >
+            {types}
+          </select>
         </div>
-    )
+        {/* end of select type */}
+      </from>
+    </section>
+  );
 }
