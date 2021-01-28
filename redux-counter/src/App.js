20e0bf0f99499c9695f9be8c93dfb2176087b13a
Amirhossein <amir.h.nouri2000@gmail.com>
2021-01-28 10:35:11 +0330

    Store and reducer created

--- redux-counter/src/App.js
+++ redux-counter/src/App.js
@@ -1,16 +1,19 @@
-import './App.css';
-import Counter from './components/Counter'
-import {createStore} from 'redux'
+import "./App.css";
+import Counter from "./components/Counter";
+import { createStore } from "redux";
 
-const reducer = () =>{
-  console.log("reducer");
-}
-const store = createStore(reducer)
+const defaultState = {
+  count: 0,
+  name: "amir",
+};
+const reducer = (state, action) => {
 
+  return state
+};
+const store = createStore(reducer, defaultState);
+console.log(store.getState());
 function App() {
-  return (
-    <Counter></Counter>
-  );
+  return <Counter state={store.getState()}></Counter>;
 }
 
 export default App;
--- redux-counter/src/components/Counter.js
+++ redux-counter/src/components/Counter.js
@@ -1,11 +1,12 @@
 import React from "react";
 
-export default function Counter() {
+export default function Counter({state}) {
+    const amount  =state.count
   const [count, setCount] = React.useState(0);
   return (
     <div className="container">
       <h1>Counter</h1>
-      <p className="counter">{count}</p>
+      <p className="counter">{amount}</p>
       <div className="buttons">
         <button
           type="button"
