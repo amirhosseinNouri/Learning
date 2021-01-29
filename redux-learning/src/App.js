325887dfc017a863bd8fa821f12f2ccdd4d93a2d
Amirhossein <amir.h.nouri2000@gmail.com>
2021-01-29 11:48:41 +0330

    Decrement action added

--- redux-learning/src/App.js
+++ redux-learning/src/App.js
@@ -1,16 +1,16 @@
 import "./App.css";
-import { useSelector , useDispatch } from "react-redux";
-import {increment} from './actions'
+import { useSelector, useDispatch } from "react-redux";
+import { increment, decrement } from "./actions";
 
 function App() {
   const counter = useSelector((state) => state.counter);
   const logged = useSelector((state) => state.logged);
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
   return (
     <div className="App">
       <h1>Counter {counter}</h1>
       <button onClick={() => dispatch(increment())}>+</button>
-      <button>-</button>
+      <button onClick={() => dispatch(decrement())}>-</button>
       {logged && <h3>Valuable information I should't see</h3>}
     </div>
   );
--- redux-learning/src/actions/index.js
+++ redux-learning/src/actions/index.js
@@ -3,3 +3,9 @@ export const increment = () =>{
         type : "INCREMENT"
     }
 }
+
+export const decrement = () =>{
+    return {
+        type : "DECREMENT"
+    }
+}
