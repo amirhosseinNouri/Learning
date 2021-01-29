18f8f040b2de6de15204e13b575097dcbf80ae55
Amirhossein <amir.h.nouri2000@gmail.com>
2021-01-29 11:42:31 +0330

    Use a state using useSelectore hook

--- redux-learning/src/App.js
+++ redux-learning/src/App.js
@@ -1,9 +1,13 @@
-import './App.css';
+import "./App.css";
+import { useSelector } from "react-redux";
 
 function App() {
+  const counter = useSelector((state) => state.counter);
+  const logged = useSelector((state) => state.logged);
   return (
     <div className="App">
-      App
+      <h1>Counter {counter}</h1>
+      {logged && <h3>Valuable information I should't see</h3>}
     </div>
   );
 }
