09484e847bf10f5416634e6c2e0bec78e761b20a
Amirhossein <amir.h.nouri2000@gmail.com>
2021-01-29 12:25:02 +0330

    PostForm component changed to functional-based component

--- redux-crash-course/src/components/PostForm.js
+++ redux-crash-course/src/components/PostForm.js
@@ -1,24 +1,36 @@
-import React from 'react'
+import React from "react";
 
 export default function PostForm() {
-    const [title, setTitle] = React.useState()
-    return (
-        
-            <div>
-                <h1>Add Post</h1>
-                <form>
-                    <div>
-                        <label htmlFor="title">Title</label> <br/>
-                        <input type="text"/>
-                    </div>
-                    <br/>
-                    <div>
-                        <label htmlFor="body">Body</label> <br/>
-                        <textarea name="body" id="body" cols="30" rows="10"></textarea>
-                    </div>
-                    <br/>
-                    <button type="submit">Submit</button>
-                </form>
-            </div>
-    )
+  const [title, setTitle] = React.useState("");
+  const [body, setBody] = React.useState("");
+  return (
+    <div>
+      <h1>Add Post</h1>
+      <form>
+        <div>
+          <label htmlFor="title">Title</label> <br />
+          <input
+            type="text"
+            id="title"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+        </div>
+        <br />
+        <div>
+          <label htmlFor="body">Body</label> <br />
+          <textarea
+            name="body"
+            id="body"
+            cols="30"
+            rows="10"
+            value={body}
+            onChange={(e) => setBody(e.target.value)}
+          ></textarea>
+        </div>
+        <br />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
 }
