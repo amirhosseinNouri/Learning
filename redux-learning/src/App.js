3abbdf48beef98b3f1f41bad14b2d4cacaacfcc2
Amirhossein <amir.h.nouri2000@gmail.com>
2021-01-29 11:46:53 +0330

    Dispatch action using useDispatch hook

--- redux-learning/src/App.js
+++ redux-learning/src/App.js
@@ -1,12 +1,16 @@
 import "./App.css";
-import { useSelector } from "react-redux";
+import { useSelector , useDispatch } from "react-redux";
+import {increment} from './actions'
 
 function App() {
   const counter = useSelector((state) => state.counter);
   const logged = useSelector((state) => state.logged);
+  const dispatch = useDispatch()
   return (
     <div className="App">
       <h1>Counter {counter}</h1>
+      <button onClick={() => dispatch(increment())}>+</button>
+      <button>-</button>
       {logged && <h3>Valuable information I should't see</h3>}
     </div>
   );
