3d595fdca1fd2b41e93ef70b72f06b1eb2db0380
Amirhossein <amir.h.nouri2000@gmail.com>
2021-01-29 12:20:01 +0330

    PostForm component structured

--- redux-crash-course/src/App.js
+++ redux-crash-course/src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Posts from './components/Posts'
+import PostForm from './components/PostForm'
 
 function App() {
   return (
     <div className="App">
+      <PostForm></PostForm>
       <Posts></Posts>
+      
     </div>
   );
 }
--- redux-crash-course/src/components/PostForm.js
+++ redux-crash-course/src/components/PostForm.js
@@ -4,7 +4,20 @@ export default class PostForm extends Component {
     render() {
         return (
             <div>
-                Post form
+                <h1>Add Post</h1>
+                <form>
+                    <div>
+                        <label htmlFor="title">Title</label> <br/>
+                        <input type="text"/>
+                    </div>
+                    <br/>
+                    <div>
+                        <label htmlFor="body">Body</label> <br/>
+                        <textarea name="body" id="body" cols="30" rows="10"></textarea>
+                    </div>
+                    <br/>
+                    <button type="submit">Submit</button>
+                </form>
             </div>
         )
     }
