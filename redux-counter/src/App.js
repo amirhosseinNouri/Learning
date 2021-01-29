fd5bac0d586f2d33fff5e77124dd1f7af87ca539
Amirhossein <amir.h.nouri2000@gmail.com>
2021-01-29 13:35:15 +0330

    Reducers structured

--- redux-counter/src/App.js
+++ redux-counter/src/App.js
@@ -2,22 +2,9 @@ import "./App.css";
 import Counter from "./components/Counter";
 import { Provider } from "react-redux";
 import { createStore } from "redux";
+import counterReducer from './reducers/counterReducer'
 
-const counter = (state = 0, action) => {
-  switch (action.type) {
-    case "INCREMENT":
-      return state + 1;
-    case "DECREMENT":
-      return state - 1;
-
-    case "RESET":
-      return 0;
-    default:
-      return state;
-  }
-};
-
-const store = createStore(counter);
+const store = createStore(counterReducer);
 
 function App() {
   return (
