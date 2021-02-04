5af455195592a4fb48f80099705168d5f5aa646e
Amirhossein <amir.h.nouri2000@gmail.com>
2021-02-05 01:46:40 +0330

    State added

--- seasons/src/components/Accordion.js
+++ seasons/src/components/Accordion.js
@@ -1,12 +1,22 @@
-import React from "react";
+import React , {useState} from "react";
 import { GoChevronDown } from "react-icons/go";
 
 export default function Accordion({ items }) {
+    const [activeIndex , setActiveIndex] = useState(-1)
+  const handleClick = (id) => {
+    setActiveIndex(id)
+  };
   return (
     <div>
       {items.map((item) => {
         return (
-          <div className="item" key={item.id} clas>
+          <div
+            className="item"
+            key={item.id}
+            onClick={() => {
+              handleClick(item.id);
+            }}
+          >
             <div className="title">
               <GoChevronDown></GoChevronDown>
               <h4>{item.title}</h4>
