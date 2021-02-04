e987f65dde2f5ee897492c0cc8e82e26abae2cdb
Amirhossein <amir.h.nouri2000@gmail.com>
2021-02-04 21:45:09 +0330

    Grid style fixed

--- seasons/src/components/ImageCard.js
+++ seasons/src/components/ImageCard.js
@@ -1,22 +1,25 @@
 import React, { Component } from "react";
 
 export default class ImageCard extends Component {
+  constructor(props) {
+    super(props);
+    this.imageRef = React.createRef();
+    this.state = {span : 0}
+  }
 
-    constructor(props){
-        super(props)
-        this.imageRef  =React.createRef()
-    }
-
-    componentDidMount(){
-        this.imageRef.current.addEventListener('load' , () =>{
-            
-            console.log(this.imageRef.current.clientHeight);
-        })
-    }
+  componentDidMount() {
+    this.imageRef.current.addEventListener("load", () => {
+      console.log(this.imageRef.current.clientHeight);
+      const span = Math.ceil(this.imageRef.current.clientHeight / 10);
+      this.setState({
+        span,
+      });
+    });
+  }
 
   render() {
     return (
-      <div>
+      <div style={{gridRowEnd: `span ${this.state.span}`}}>
         <img ref={this.imageRef} alt={this.props.desc} src={this.props.image} />
       </div>
     );
