039994ee39f6392af97292e534f84d59e2bac269
Amirhossein <amir.h.nouri2000@gmail.com>
2021-02-04 18:44:34 +0330

    Style SeasonDisplay component

--- seasons/src/components/SeasonDisplay.js
+++ seasons/src/components/SeasonDisplay.js
@@ -1,15 +1,27 @@
 import React from "react";
+import { FaSnowflake, FaSun } from "react-icons/fa";
 
 const getSeason = (lat, month) => {
   if (month > 2 && month < 9) {
     return lat > 0 ? "summer" : "winter";
-  }
-  else{
+  } else {
     return lat > 0 ? "winter" : "summer";
   }
 };
 
 export default function SeasonDisplay({ lat }) {
   const season = getSeason(lat, new Date().getMonth());
-  return <div>seasion display</div>;
+  return (
+    <div className="center">
+      <div className={season === 'winter' ? "icon icon--top snow" : "icon icon--top sun"}>
+        {season === "winter" ? <FaSnowflake></FaSnowflake> : <FaSun></FaSun>}
+      </div>
+      <h1>
+        {season === "winter" ? "Burr, it is chilly" : "Lets hit the beach"}
+      </h1>
+      <div className={season === 'winter' ? "icon icon--bottom snow" : "icon icon--bottom sun"}>
+        {season === "winter" ? <FaSnowflake></FaSnowflake> : <FaSun></FaSun>}
+      </div>
+    </div>
+  );
 }
