8e0d42ef35a1972e373bdbb5d54747a1c9fca1d4
Amirhossein <amir.h.nouri2000@gmail.com>
2021-02-05 10:08:11 +0330

    Ajax request added

--- seasons/src/components/Search.js
+++ seasons/src/components/Search.js
@@ -1,7 +1,29 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 export default function Search() {
-  const [term, setTerm] = useState("");
+  const [term, setTerm] = useState("car");
+  const [results , setResults] = useState([])
+
+  useEffect(() => {
+    const search = async () => {
+      const {data} = await axios.get("https://en.wikipedia.org/w/api.php", {
+        params: {
+          action: "query",
+          list: "search",
+          origin: "*",
+          format: "json",
+          srsearch: term,
+        },
+      });
+      setResults(data.query.search)
+      console.log(results);
+    };
+    if(term){
+        search();
+
+    }
+  }, [term]);
   return (
     <div className="search">
       <div className="search__control">
