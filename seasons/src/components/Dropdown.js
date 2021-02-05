4b377d1a2fa51af97e0c87e5843f86278bb86ec1
Amirhossein <amir.h.nouri2000@gmail.com>
2021-02-05 15:52:51 +0330

    Handle dropdown opening and closing

--- seasons/src/components/Dropdown.js
+++ seasons/src/components/Dropdown.js
@@ -2,36 +2,52 @@ import React from "react";
 import { GoChevronDown } from "react-icons/go";
 
 export default function Dropdown({ options }) {
-  const [selected, setSelected] = React.useState({label : "Select a color"});
-  const [open , setOpen ] = React.useState(false)
+  const [selected, setSelected] = React.useState({ label: "Select a color" });
+  const [open, setOpen] = React.useState(false);
+  const ref = React.useRef();
 
-  const itemsClassName = open ? "options__item show" : "options__item"
+  React.useEffect(() => {
+    document.addEventListener("click", (e) => {
 
-  const toggleDropDown = () =>{
-      setOpen(! open)
-  }
+      if (!ref.current.contains(e.target)) setOpen(false);
+    });
+  }, []);
+  const itemsClassName = open ? "options__item show" : "options__item";
+
+  const toggleDropDown = () => {
+    setOpen(!open);
+  };
   return (
     <>
       <h3>Select a color</h3>
-      
-      <div className="options">
-      <div className="selected" onClick={toggleDropDown}>
+
+      <div className="options" ref={ref}>
+        <div className="selected" onClick={toggleDropDown}>
           <span>{selected.label}</span>
           <GoChevronDown></GoChevronDown>
-      </div>
+        </div>
         {options.map((item, index) => {
           if (item.label !== selected)
             return (
-              <div key={index} className={itemsClassName} onClick={() => {
-                setSelected(item)
-                toggleDropDown()
-              }}>
+              <div
+                key={index}
+                className={itemsClassName}
+                onClick={() => {
+                  setSelected(item);
+                  setOpen(!open);
+                }}
+              >
                 {item.label}
               </div>
             );
         })}
       </div>
-      <h3 className="selected__result" style={{backgroundColor : `${selected.value}`}}>Selected Color : {selected.label}</h3>
+      <h3
+        className="selected__result"
+        style={{ backgroundColor: `${selected.value}` }}
+      >
+        Selected Color : {selected.label}
+      </h3>
     </>
   );
 }
