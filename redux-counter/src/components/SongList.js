213e9c0681d8ef1adf0b6ec92ee0d2dc24c008b3
Amirhossein <amir.h.nouri2000@gmail.com>
2021-02-06 16:51:34 +0330

    :heavy_plus_sign: Add Selecting song
    ActionCreator passed as second arg of connect and now it is one the SongList props

--- redux-counter/src/components/SongList.js
+++ redux-counter/src/components/SongList.js
@@ -1,13 +1,30 @@
 import React from "react";
 import { connect } from "react-redux";
+import { selectSong } from "../actions";
 
-function SongList({songs}) {
-    console.log(songs);
-  return <div>Song list</div>;
+function SongList({ songs , selectSong }) {
+
+  return (
+    <section className="song__list">
+      {songs.map((item, index) => {
+        return (
+          <article key={index} className="song">
+            <h4>{item.title}</h4>
+            <button onClick={() => selectSong(item)} type="button" className="btn">
+              Select
+            </button>
+          </article>
+        );
+      })}
+    </section>
+  );
 }
 
-const mapStateToProps = (state) =>{
-    return {songs : state.song.songs}
-}
+const mapStateToProps = (state) => {
+    console.log(state);
+  return { songs: state.song.songs };
+};
 
-export default connect(mapStateToProps)(SongList)
+export default connect(mapStateToProps, {
+  selectSong,
+})(SongList);
