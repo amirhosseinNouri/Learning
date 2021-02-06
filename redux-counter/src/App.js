69835363bab9dfe2ec08f8ca6f3c94499b6091c3
Amirhossein <amir.h.nouri2000@gmail.com>
2021-02-06 14:55:50 +0330

    Cleanup

--- redux-counter/src/App.js
+++ redux-counter/src/App.js
@@ -1,27 +1,10 @@
 import "./App.css";
-import Counter from "./components/Counter";
-import { Provider } from "react-redux";
-import { createStore, applyMiddleware } from "redux";
-import reducer from "./reducers";
-import thunk from "redux-thunk";
 
-const defaultState = {
-  count: 0,
-  name: "amir",
-};
-const middlewares = [thunk];
 
-const store = createStore(
-  reducer,
-  defaultState,
-  applyMiddleware(...middlewares)
-);
 
 function App() {
   return (
-    <Provider store={store}>
-      <Counter></Counter>;
-    </Provider>
+    <div>App</div>
   );
 }
 
