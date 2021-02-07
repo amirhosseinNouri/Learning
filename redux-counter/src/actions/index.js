ade22f18c0099789963cc8df1d63fcaef8b6033f
Amirhossein <amir.h.nouri2000@gmail.com>
2021-02-07 09:58:24 +0330

    :heavy_plus_sign: Add fetchUser action creator

--- redux-counter/src/actions/index.js
+++ redux-counter/src/actions/index.js
@@ -1,7 +1,12 @@
 import axios from "../api/jsonPlaceHolder";
 
-export const fetchPosts = () => async dispatch => {
-  const response = await axios.get('/posts');
+export const fetchPosts = () => async (dispatch) => {
+  const response = await axios.get("/posts");
 
-  dispatch({ type: 'FETCH_POSTS', payload: response.data });
+  dispatch({ type: "FETCH_POSTS", payload: response.data });
+};
+
+export const fetchUser = (id) => async (dispatch) => {
+  const response = await axios.get(`/users/${id}`);
+  dispatch({ type: "FETCH_USER", payload: response.data });
 };
