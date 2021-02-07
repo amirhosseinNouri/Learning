5d6c34ba2154d80cdf41a40d8f94ae211564785f
Amirhossein <amir.h.nouri2000@gmail.com>
2021-02-07 20:22:03 +0330

    :green_heart:​ Avoid dublicate requests by memoizing the action creator function

--- redux-counter/src/actions/index.js
+++ redux-counter/src/actions/index.js
@@ -1,4 +1,5 @@
 import axios from "../api/jsonPlaceHolder";
+import _ from "lodash";
 
 export const fetchPosts = () => async (dispatch) => {
   const response = await axios.get("/posts");
@@ -6,7 +7,9 @@ export const fetchPosts = () => async (dispatch) => {
   dispatch({ type: "FETCH_POSTS", payload: response.data });
 };
 
-export const fetchUser = (id) => async (dispatch) => {
+export const fetchUser = (id) => (dispatch) => _fetchUser(id, dispatch);
+
+const _fetchUser = _.memoize(async (id, dispatch) => {
   const response = await axios.get(`/users/${id}`);
   dispatch({ type: "FETCH_USER", payload: response.data });
-};
+});
