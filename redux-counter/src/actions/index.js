270f5cbded1e2664f29e4db231026e062285f4c5
Amirhossein <amir.h.nouri2000@gmail.com>
2021-02-07 20:25:23 +0330

    :x: Remove memoized function to try another way

--- redux-counter/src/actions/index.js
+++ redux-counter/src/actions/index.js
@@ -7,9 +7,15 @@ export const fetchPosts = () => async (dispatch) => {
   dispatch({ type: "FETCH_POSTS", payload: response.data });
 };
 
-export const fetchUser = (id) => (dispatch) => _fetchUser(id, dispatch);
+// export const fetchUser = (id) => (dispatch) => _fetchUser(id, dispatch);
 
-const _fetchUser = _.memoize(async (id, dispatch) => {
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+//   const response = await axios.get(`/users/${id}`);
+//   dispatch({ type: "FETCH_USER", payload: response.data });
+// });
+
+export const fetchUser = id => async (dispatch) =>{
   const response = await axios.get(`/users/${id}`);
   dispatch({ type: "FETCH_USER", payload: response.data });
-});
+};
+
