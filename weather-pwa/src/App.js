d5722dc9fc10886bf21f7a21b8411b4000c7145e
Amirhossein <amir.h.nouri2000@gmail.com>
2021-02-27 09:30:27 +0330

    :hammer: Wrap input into a form element to prevent CORS policy error

--- weather-pwa/src/App.js
+++ weather-pwa/src/App.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useState } from "react";
+import fetchWeather from "./api/fetchWeather";
 
 export default function App() {
-    return (
-        <div>
-           App 
-        </div>
-    )
+  const [query, setQuery] = useState("");
+
+  const handleSearchClick = async (e) => {
+    e.preventDefault();
+    console.log("enter");
+    const data = await fetchWeather(query);
+    console.log(data);
+  };
+  return (
+    <div className="main-container">
+      <form>
+        <input
+          type="text"
+          placeholder="Search..."
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <button onClick={handleSearchClick} type="submit">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 }
