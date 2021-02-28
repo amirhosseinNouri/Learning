782f6f6341a0fc8b354e78a606bbc6bbac80c70e
Amirhossein <amir.h.nouri2000@gmail.com>
2021-02-28 17:12:23 +0330

    :heavy_plus_sign: Rewrite service worker fetch event listener

--- weather-pwa/public/serviceWorker.js
+++ weather-pwa/public/serviceWorker.js
@@ -1,45 +1,3 @@
-// const CAHCE_NAME = "version-1";
-// const urlsToCache = ["index.html", "offline.html"];
-
-// const self = this;
-
-// // Install SW
-// self.addEventListener("install", (event) => {
-//   event.waitUntil(
-//     caches.open(CAHCE_NAME).then((cache) => {
-//       console.log("opend cache");
-//       return cache.addAll(urlsToCache);
-//     })
-//   );
-// });
-
-// // Listen for requests
-// self.addEventListener("fetch", (event) => {
-//   event.respondWith(
-//     caches.match(event.request).then(() => {
-//       return fetch(event.request).catch(() => caches.match("offline.html"));
-//     })
-//   );
-// });
-
-// // Activate SW
-// self.addEventListener("activate", (event) => {
-//   const cacheWhiteList = [];
-//   cacheWhiteList.push(CAHCE_NAME);
-
-//   event.waitUntil(
-//     caches.keys().then((cacheNames) =>
-//       Promise.all(
-//         cacheNames.map((item) => {
-//           if (!cacheWhiteList.includes(item)) {
-//             return caches.delete(item);
-//           }
-//         })
-//       )
-//     )
-//   );
-// });
-
 const CACHE_NAME = "version-1";
 const urlToCache = ["index.html", "offline.html"];
 const self = this;
@@ -47,7 +5,7 @@ const self = this;
 /* 
     Install Service Worker.
     Create (or open if exists) the cache.
-    Retrieve the urls and save the result in the cache.
+    Retrieve the urls and save the result in the cache (initialize cache for offline use).
 */
 self.addEventListener("install", (event) => {
   event.waitUntil(
@@ -55,5 +13,21 @@ self.addEventListener("install", (event) => {
       console.log("Cache opened successfully.");
       return cache.addAll(urlToCache);
     })
+  );
+});
+
+/*
+    Listen for requests.
+    Fires after each request.
+    We can respond to the request the way we want to.
+    If there is an error (in network probably),we render offline.html
+*/
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches
+      .match(event.request)
+      .then(() =>
+        fetch(event.request).catch(() => caches.match("offline.html"))
+      )
   );
 });
