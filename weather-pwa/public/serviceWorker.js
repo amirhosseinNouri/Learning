90fcec1cde688cbbd73008551ae0d96a857d6670
Amirhossein <amir.h.nouri2000@gmail.com>
2021-02-28 16:12:57 +0330

    :heavy_plus_sign: Rewrite service worker installation handler

--- weather-pwa/public/serviceWorker.js
+++ weather-pwa/public/serviceWorker.js
@@ -1,41 +1,59 @@
-const CAHCE_NAME = "version-1";
-const urlsToCache = ["index.html", "offline.html"];
+// const CAHCE_NAME = "version-1";
+// const urlsToCache = ["index.html", "offline.html"];
 
+// const self = this;
+
+// // Install SW
+// self.addEventListener("install", (event) => {
+//   event.waitUntil(
+//     caches.open(CAHCE_NAME).then((cache) => {
+//       console.log("opend cache");
+//       return cache.addAll(urlsToCache);
+//     })
+//   );
+// });
+
+// // Listen for requests
+// self.addEventListener("fetch", (event) => {
+//   event.respondWith(
+//     caches.match(event.request).then(() => {
+//       return fetch(event.request).catch(() => caches.match("offline.html"));
+//     })
+//   );
+// });
+
+// // Activate SW
+// self.addEventListener("activate", (event) => {
+//   const cacheWhiteList = [];
+//   cacheWhiteList.push(CAHCE_NAME);
+
+//   event.waitUntil(
+//     caches.keys().then((cacheNames) =>
+//       Promise.all(
+//         cacheNames.map((item) => {
+//           if (!cacheWhiteList.includes(item)) {
+//             return caches.delete(item);
+//           }
+//         })
+//       )
+//     )
+//   );
+// });
+
+const CACHE_NAME = "version-1";
+const urlToCache = ["index.html", "offline.html"];
 const self = this;
 
-// Install SW
+/* 
+    Install Service Worker.
+    Create (or open if exists) the cache.
+    Retrieve the urls and save the result in the cache.
+*/
 self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CAHCE_NAME).then((cache) => {
-      console.log("opend cache");
-      return cache.addAll(urlsToCache);
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("Cache opened successfully.");
+      return cache.addAll(urlToCache);
     })
-  );
-});
-
-// Listen for requests
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then(() => {
-      return fetch(event.request).catch(() => caches.match("offline.html"));
-    })
-  );
-});
-
-// Activate SW
-self.addEventListener("activate", (event) => {
-  const cacheWhiteList = [];
-  cacheWhiteList.push(CAHCE_NAME);
-
-  event.waitUntil(
-    caches.keys().then((cacheNames) =>
-      Promise.all(
-        cacheNames.map((item) => {
-          if (!cacheWhiteList.includes(item)) {
-            return caches.delete(item);
-          }
-        })
-      )
-    )
   );
 });
