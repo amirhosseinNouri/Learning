345e90d29758686aaf4ffe267e5abba53cd62a97
Amirhossein <amir.h.nouri2000@gmail.com>
2021-02-28 09:36:44 +0330

    :tada: Activate the service worker

--- weather-pwa/public/serviceWorker.js
+++ weather-pwa/public/serviceWorker.js
@@ -1,31 +1,41 @@
-const CAHCE_NAME = "version-1"
-const urlsToCache = ["index.html" , "offline.html"]
+const CAHCE_NAME = "version-1";
+const urlsToCache = ["index.html", "offline.html"];
 
 const self = this;
 
 // Install SW
-self.addEventListener('install' , (event) => {
-    event.waitUntil(
-        caches.open(CAHCE_NAME)
-        .then((cache) => {
-            console.log('opend cache');
-            return cache.addAll(urlsToCache)
-        })
-    )
-})
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CAHCE_NAME).then((cache) => {
+      console.log("opend cache");
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
 
 // Listen for requests
-self.addEventListener('fetch' , (event) => {
-    event.respondWith(
-        caches.match(event.request).then(() =>{
-            return fetch(event.request)
-            .catch(() => caches.match('offline.html'))
-        })
-    )
-})
-
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then(() => {
+      return fetch(event.request).catch(() => caches.match("offline.html"));
+    })
+  );
+});
 
 // Activate SW
-self.addEventListener('activate' , (event) => {
-    
-})
+self.addEventListener("activate", (event) => {
+  const cacheWhiteList = [];
+  cacheWhiteList.push(CAHCE_NAME);
+
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames.map((item) => {
+          if (!cacheWhiteList.includes(item)) {
+            return caches.delete(item);
+          }
+        })
+      )
+    )
+  );
+});
