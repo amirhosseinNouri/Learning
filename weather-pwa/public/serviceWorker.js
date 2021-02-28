2889a21b70f19e8dbbf59aa697e1434585ceb28a
Amirhossein <amir.h.nouri2000@gmail.com>
2021-02-28 09:33:20 +0330

    :tada: Listen for requests and load offline.html if there is no internet

--- weather-pwa/public/serviceWorker.js
+++ weather-pwa/public/serviceWorker.js
@@ -16,7 +16,12 @@ self.addEventListener('install' , (event) => {
 
 // Listen for requests
 self.addEventListener('fetch' , (event) => {
-    
+    event.respondWith(
+        caches.match(event.request).then(() =>{
+            return fetch(event.request)
+            .catch(() => caches.match('offline.html'))
+        })
+    )
 })
 
 
