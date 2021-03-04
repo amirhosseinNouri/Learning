bea4c997ee022be3a300a7cb1be24fa3c2422b82
Amirhossein <amir.h.nouri2000@gmail.com>
2021-03-04 12:41:35 +0330

    Initial setup

--- worker-in-js/index.js
+++
@@ -1,6 +0,0 @@
-const worker = new Worker('task.js')
-worker.addEventListener('message', function(e) {
-    console.log('Worker said: ', e.data);
-  }, false);
-  
-worker.postMessage('Hello World');
--- worker-in-js/task.js
+++
@@ -1,4 +0,0 @@
-self.addEventListener('message', function(e) {
-    self.postMessage(e.data);
-  }, false);
-
