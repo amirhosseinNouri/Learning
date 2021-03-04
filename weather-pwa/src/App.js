7be38f2a36b0303d8ab1629fea20b7a33a0a772b
Amirhossein <amir.h.nouri2000@gmail.com>
2021-03-04 17:54:38 +0330

    Fix manifeast icon

--- weather-pwa/src/App.js
+++ weather-pwa/src/App.js
@@ -1,15 +1,28 @@
-import {useState} from 'react'
-import SearchBar from './components/SearchBar'
-import ResultCard from './components/ResultCard'
-import "./App.css"
+import { useState } from "react";
+import SearchBar from "./components/SearchBar";
+import ResultCard from "./components/ResultCard";
+import "./App.css";
+
 
 export default function App() {
-  const [weather, setWeather] = useState({})
-  
+  const [weather, setWeather] = useState({});
+  const [loading, setLoading] = useState(false);
+
+  if (loading) {
+    return (
+      <div className="container">
+        <div className="loading content">
+        <h1 className="loading__title">Loading...</h1>
+       
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div className="container">
-        <SearchBar setWeather={setWeather}></SearchBar>
-        {weather.main && <ResultCard weather={weather}></ResultCard> }
+      <SearchBar setLoading={setLoading} setWeather={setWeather}></SearchBar>
+      {weather.main && <ResultCard weather={weather}></ResultCard>}
     </div>
   );
 }
--- weather-pwa/src/components/SearchBar.js
+++ weather-pwa/src/components/SearchBar.js
@@ -1,15 +1,18 @@
 import React, { useState } from "react";
 import fetchWeather from "../api/fetchWeather";
 
-export default function SearchBar({ setWeather }) {
+export default function SearchBar({ setWeather  , setLoading}) {
   const [query, setQuery] = useState("");
 
   const handleSearchClick = async (e) => {
+    setLoading(true)
     e.preventDefault();
     if (!query) return;
     const data = await fetchWeather(query);
+    console.log(data);
     setWeather(data);
     setQuery("");
+    setLoading(false)
   };
 
   return (
