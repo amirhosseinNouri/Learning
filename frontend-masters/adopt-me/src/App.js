b473976996f6432c97620196cfadede32e027bfb
Amirhossein <amir.h.nouri2000@gmail.com>
2021-03-07 18:49:43 +0330

    Pure React part completed

---
+++ frontend-masters/adopt-me/src/App.js
@@ -0,0 +1,19 @@
+const Pet = ({name , animal , breed}) => {
+
+  return React.createElement("div", {}, [
+      React.createElement("h1", {}, name),
+      React.createElement("h2", {}, animal),
+      React.createElement("h1", {}, breed),
+  ]);
+};
+
+const App = () => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt me"),
+    React.createElement(Pet, { name: "pepper", animal: "bird", breed: "c1" }),
+    React.createElement(Pet, { name: "Doink", animal: "cat", breed: "c2" }),
+    React.createElement(Pet, { name: "Luna", animal: "Dog", breed: "Havanes" }),
+  ]);
+};
+
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
