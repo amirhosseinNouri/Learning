f36fc0996eb25bf76a7f6dcc1cbffbccd1d5025c
Amirhossein <amir.h.nouri2000@gmail.com>
2021-03-08 14:22:21 +0330

    Convert codes to jsx

--- frontend-masters/adopt-me/src/Pet.js
+++ frontend-masters/adopt-me/src/Pet.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
 export default function Pet({ name, animal, breed }) {
-  return React.createElement(' div', {}, [
-    React.createElement('h1', {}, name),
-    React.createElement('h2', {}, animal),
-    React.createElement('h1', {}, breed),
-  ]);
+  return (
+    <div>
+      <h1>{name}</h1>
+      <h2>{animal}</h2>
+      <h2>{breed}</h2>
+    </div>
+  );
 }
