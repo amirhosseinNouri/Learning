00137d6f9dd6e0087608bc61c7485e6443e4f46d
Amirhossein <amir.h.nouri2000@gmail.com>
2021-03-08 14:33:20 +0330

    Convert App component to jsx

--- frontend-masters/adopt-me/src/App.js
+++ frontend-masters/adopt-me/src/App.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Pet  from './Pet';
+import Pet from './Pet';
 
 const App = () => {
-  return React.createElement('div', {}, [
-    React.createElement('h1', {}, 'Adopt me'),
-    React.createElement(Pet, { name: 'pepper', animal: 'bird', breed: 'c1' }),
-    React.createElement(Pet, { name: 'Doink', animal: 'cat', breed: 'c2' }),
-    React.createElement(Pet, { name: 'Luna', animal: 'Dog', breed: 'Havanes' }),
-  ]);
+  // return React.createElement('div', {}, [
+  //   React.createElement('h1', {}, 'Adopt me'),
+  //   React.createElement(Pet, { name: 'pepper', animal: 'bird', breed: 'c1' }),
+  //   React.createElement(Pet, { name: 'Doink', animal: 'cat', breed: 'c2' }),
+  //   React.createElement(Pet, { name: 'Luna', animal: 'Dog', breed: 'Havanes' }),
+  // ]);
+  return (
+    <div>
+      <h1>Adopt Me</h1>
+      <Pet name='Lina' animal='dog' breed='b1'></Pet>
+      <Pet name='pepper' animal='bird' breed='b2'></Pet>
+      <Pet name='doink' animal='cat' breed='b3'></Pet>
+    </div>
+  );
 };
 
-ReactDOM.render(React.createElement(App), document.querySelector('#root'));
+ReactDOM.render(<App></App>, document.querySelector('#root'));
