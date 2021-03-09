2366335247b9aaca14081573b07d10bae4b5dd22
Amirhossein <amir.h.nouri2000@gmail.com>
2021-03-09 15:49:10 +0330

    Add prefix to class names

--- jss/src/components/Button/style.js
+++ jss/src/components/Button/style.js
@@ -1,16 +1,19 @@
 import { createUseStyles } from 'react-jss';
 
-const useStyle = createUseStyles({
-  myButton: {
-    padding: (props) => props.spacing,
-  },
+const useStyle = createUseStyles(
+  {
+    myButton: {
+      padding: (props) => props.spacing,
+    },
 
-  myLabel: (props) => ({
-    display: 'block',
-    color: props.labelColor,
-    fontWeight: props.fontWeight,
-    fontStyle: props.fontStyle,
-  }),
-});
+    myLabel: (props) => ({
+      display: 'block',
+      color: props.labelColor,
+      fontWeight: props.fontWeight,
+      fontStyle: props.fontStyle,
+    }),
+  },
+  { name: 'button' },
+);
 
 export default useStyle;
