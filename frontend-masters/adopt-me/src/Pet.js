608aab50816d0e14e978c67ae16ec547719e6bff
Amirhossein <amir.h.nouri2000@gmail.com>
2021-03-10 11:57:18 +0330

    Refactor pet component

--- frontend-masters/adopt-me/src/Pet.js
+++ frontend-masters/adopt-me/src/Pet.js
@@ -1,11 +1,21 @@
 import React from 'react';
 
-export default function Pet({ name, animal, breed }) {
+export default function Pet({ name, animal, breed, media, location, id }) {
+  let hero = 'http://placeorgi.com/300/300';
+  if (media.length) {
+    hero = media[0].small;
+  }
+
   return (
-    <div>
-      <h1>{name}</h1>
-      <h2>{animal}</h2>
-      <h2>{breed}</h2>
-    </div>
+    <a href={`/details/${id}`} className='pet'>
+      <div className='image-container'>
+        <img src={hero} alt={name} />
+      </div>
+
+      <div className='info'>
+        <h1>{name}</h1>
+        <h2>{`${animal} - ${breed} - ${location}`}</h2>
+      </div>
+    </a>
   );
 }
