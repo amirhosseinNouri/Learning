604294fefeb6c4bc9d1d5de49ca7c56fe9fc5b0d
Amirhossein <amir.h.nouri2000@gmail.com>
2021-03-13 06:58:10 +0330

    Build details component

--- frontend-masters/adopt-me/src/Details.js
+++ frontend-masters/adopt-me/src/Details.js
@@ -1,4 +1,44 @@
-import React from 'react';
-export default function Details(props) {
-  return <pre><code>{JSON.stringify(props , null , 4)}</code></pre>
+import React, { Component } from 'react';
+import pet from '@frontendmasters/pet';
+
+export default class Details extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: true,
+    };
+  }
+
+  componentDidMount() {
+    pet.animal(this.props.id).then(({ animal }) => {
+      this.setState({
+        name: animal.name,
+        animal: animal.type,
+        location: `${animal.contact.address.city}, ${animal.contact.address.state}`,
+        description: animal.description,
+        media: animal.photos,
+        breed: animal.breeds.primary,
+        loading: false,
+      });
+    }, console.error);
+  }
+
+  render() {
+    if (this.state.loading) {
+      return <h1>Loading...</h1>;
+    }
+
+    const { animal, breed, location, description, name } = this.state;
+
+    return (
+      <div className='details'>
+        <div>
+          <h1>{name}</h1>
+          <h2>{`${animal} - ${breed} - ${location}`}</h2>
+          <button>Adopt {name}</button>
+          <p>{description}</p>
+        </div>
+      </div>
+    );
+  }
 }
