3bbe54300b7d7b467b20f8915082fc473cdce674
Amirhossein <amir.h.nouri2000@gmail.com>
2021-04-12 09:06:08 +0430

    Complete part one

--- deep-js/scope-exercises/function-expressions/ex.js
+++ deep-js/scope-exercises/function-expressions/ex.js
@@ -1,37 +1,71 @@
+function getStudentById(studentId) {
+  return studentRecords.find(function mathId(record) {
+    return record.id == studentId;
+  });
+}
+
 function printRecords(recordIds) {
-	// TODO
+  const records = recordIds.map(getStudentById);
+
+  records.sort(function sortByNameAsc(record1, record2) {
+    if (record1.name < record2.name) {
+      return -1;
+    } else if (record1.name > record2.name) {
+      return 1;
+    } else {
+      return 0;
+    }
+  });
+
+  records.forEach(function printRecords(record) {
+    console.log(
+      `${record.name} (${record.id}: ${record.paid ? 'Paid' : 'Not Paid'})`,
+    );
+  });
 }
 
 function paidStudentsToEnroll() {
-	// TODO
+  const idsToEnroll = studentRecords
+    .filter(function needsToEnroll(record) {
+      return record.paid && !currentEnrollment.includes(record.id);
+    })
+    .map(function getStudentId(record) {
+      return record.id;
+    });
+
+  return [...currentEnrollment, ...idsToEnroll];
 }
 
 function remindUnpaid(recordIds) {
-	// TODO
-}
+  const unpaidIds = recordIds.filter(function isUnpaid(studentId) {
+    const record = getStudentById(studentId);
+    return record.paid;
+  });
 
+  printRecords(unpaidIds);
+}
 
 // ********************************
 
-var currentEnrollment = [ 410, 105, 664, 375 ];
+var currentEnrollment = [410, 105, 664, 375];
 
 var studentRecords = [
-	{ id: 313, name: "Frank", paid: true, },
-	{ id: 410, name: "Suzy", paid: true, },
-	{ id: 709, name: "Brian", paid: false, },
-	{ id: 105, name: "Henry", paid: false, },
-	{ id: 502, name: "Mary", paid: true, },
-	{ id: 664, name: "Bob", paid: false, },
-	{ id: 250, name: "Peter", paid: true, },
-	{ id: 375, name: "Sarah", paid: true, },
-	{ id: 867, name: "Greg", paid: false, },
+  { id: 313, name: 'Frank', paid: true },
+  { id: 410, name: 'Suzy', paid: true },
+  { id: 709, name: 'Brian', paid: false },
+  { id: 105, name: 'Henry', paid: false },
+  { id: 502, name: 'Mary', paid: true },
+  { id: 664, name: 'Bob', paid: false },
+  { id: 250, name: 'Peter', paid: true },
+  { id: 375, name: 'Sarah', paid: true },
+  { id: 867, name: 'Greg', paid: false },
 ];
 
 printRecords(currentEnrollment);
-console.log("----");
+console.log('----');
 currentEnrollment = paidStudentsToEnroll();
 printRecords(currentEnrollment);
-console.log("----");
+console.log('----');
 remindUnpaid(currentEnrollment);
 
 /*
