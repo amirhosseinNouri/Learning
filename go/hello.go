2b6ef2f8cfbe95ac55e9a5cd47201ec5969d17d0
Amirhossein <amir.h.nouri2000@gmail.com>
2021-04-19 10:07:47 +0430

    Implement addTwo function

---
+++ go/hello.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+
+func main() {
+	x := 10
+
+	if x == 10 {
+		fmt.Pri
+	}
+}
---
+++ js/js-hard-parts/app.js
@@ -0,0 +1,5 @@
+function addTwo(num) {
+  return num + 2;
+}
+
+console.log(addTwo(2));
