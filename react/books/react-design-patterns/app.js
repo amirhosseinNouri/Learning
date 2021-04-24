8e36e241f834db78e3a958254dd6df4b08942fea
Amirhossein <amir.h.nouri2000@gmail.com>
2021-04-24 17:56:48 +0430

    Implement delayPrint high order function - with parameters

--- react/books/react-design-patterns/app.js
+++ react/books/react-design-patterns/app.js
@@ -7,3 +7,19 @@ const log = (func) => (...args) => {
 
 const logAdd = log(add);
 logAdd(2, 3);
+
+const delay = (timeout) =>
+  new Promise((callback) => setTimeout(callback, timeout));
+
+const print = (a, b) => {
+  console.log(a);
+  console.log(b);
+};
+
+const delayPrint = (func) => async (...args) => {
+  await delay(2000);
+  func(...args);
+};
+
+const delayPrintFunction = delayPrint(print);
+delayPrintFunction(5, 6);
