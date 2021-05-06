9273908877094294d42b2722a6b92237f04ba974
Amirhossein <amir.h.nouri2000@gmail.com>
2021-05-06 15:06:38 +0430

    Now, convert it to async await syntax

--- js/js-hard-parts/async-await-scratch.js
+++ js/js-hard-parts/async-await-scratch.js
@@ -1,13 +1,22 @@
-const flow = createFlow();
-const futureData = flow.next();
-console.log(futureData);
-futureData.then(whenDataIsReady);
+// const flow = createFlow();
+// const futureData = flow.next();
+// console.log(futureData);
+// futureData.then(whenDataIsReady);
 
-function* createFlow() {
-  const data = yield fetch('https://jsonplaceholder.typicode.com/todos/1');
+// function* createFlow() {
+//   const data = yield fetch('https://jsonplaceholder.typicode.com/todos/1');
+//   console.log(data);
+// }
+
+// function whenDataIsReady(value) {
+//   promise.next(value);
+// }
+
+async function createFlow() {
+  console.log('Me first');
+  const data = await fetch('https://jsonplaceholder.typicode.com/todos/1');
   console.log(data);
 }
 
-function whenDataIsReady(value) {
-  promise.next(value);
-}
+createFlow();
+console.log('Me second');
