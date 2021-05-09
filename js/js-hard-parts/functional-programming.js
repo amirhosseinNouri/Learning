8f70495835e318a948fb67bd8ec92559a9f195fc
Amirhossein <amir.h.nouri2000@gmail.com>
2021-05-09 10:40:02 +0430

    Challenge 3

--- js/js-hard-parts/functional-programming.js
+++ js/js-hard-parts/functional-programming.js
@@ -5,3 +5,14 @@ function addTwo(num) {
 function addS(word) {
   return word + 's';
 }
+
+function map(array, callback) {
+  var result = [];
+  for (let i = 0; i < array.length; i++) {
+    result.push(callback(array[i]));
+  }
+
+  return result;
+}
+
+// console.log(map([1, 2, 3], addTwo));
