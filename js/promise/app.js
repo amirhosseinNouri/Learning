df50cb2714b03b92d60f68e8f150e087857e89e8
Amirhossein <amir.h.nouri2000@gmail.com>
2021-05-10 15:41:22 +0430

    Compose promises to run in parallel

---
+++ js/promise/app.js
@@ -0,0 +1,20 @@
+const delay = (timeout) => new Promise((done) => setTimeout(done, timeout));
+
+const functionA = async () => {
+  await delay(1000);
+  console.log('A is done');
+};
+
+const functionB = async () => {
+  await delay(2000);
+  console.log('B is done');
+};
+
+const functionC = async () => {
+  await delay(3000);
+  console.log('C is done');
+};
+
+Promise.all([functionA(), functionB(), functionC()]).then(() =>
+  console.log('All resolved'),
+);
