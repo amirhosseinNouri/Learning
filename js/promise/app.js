02ef842e04a6800eeced0668b59df5d03332483b
Amirhossein <amir.h.nouri2000@gmail.com>
2021-05-10 15:57:40 +0430

    create a higher order function to run sequential async operations

--- js/promise/app.js
+++ js/promise/app.js
@@ -25,6 +25,12 @@ const functionC = async () => {
 
 const applyAsync = (acc, curr) => acc.then(curr);
 
-[(functionA(), functionB(), functionC())]
-  .reduce(applyAsync, Promise.resolve())
-  .then(console.log('All resolved'));
+// [(functionA(), functionB(), functionC())]
+//   .reduce(applyAsync, Promise.resolve())
+//   .then(console.log('All resolved'));
+
+const composeAsync = (...funcs) => (x) =>
+  funcs.reduce(applyAsync, Promise.resolve(x));
+
+const sequential = composeAsync(functionA, functionB, functionC);
+sequential();
