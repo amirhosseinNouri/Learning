ef7714ebb6693acfee29ed0aa26ef2d33f01a72d
Amirhossein <amir.h.nouri2000@gmail.com>
2021-05-14 01:48:26 +0430

    Restructure directories

--- leet-code/twoSum.js
+++
@@ -1,9 +0,0 @@
-function twoSum(nums, target) {
-  for (let i = 0; i < nums.length; i++) {
-    const diff = target - nums[i];
-    const diffIndex = nums.indexOf(diff);
-    if (diffIndex !== -1 && diffIndex !== i) {
-      return [i, diffIndex];
-    }
-  }
-}
