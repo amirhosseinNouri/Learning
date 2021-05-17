ce6e70b6621b57ce8a608a7abed6cd64714cd354
Amirhossein <amir.h.nouri2000@gmail.com>
2021-05-17 17:15:42 +0430

    Measure performance of js application

---
+++ js/web-performance/benchmark/benchmark.js
@@ -0,0 +1,24 @@
+const { performance, PerformanceObserver } = require('perf_hooks');
+
+const obs = new PerformanceObserver((items) => {
+  items.getEntries().forEach((item) => {
+    console.log(item.name, +' ' + item.duration);
+  });
+});
+obs.observe({ entryTypes: ['measure'] });
+
+let iterations = 1e7;
+
+const a = 1;
+const b = 2;
+const add = (x, y) => x + y;
+
+performance.mark('start');
+
+while (iterations--) {
+  add(a, b);
+}
+
+performance.mark('end');
+
+performance.measure('Benchmark', 'start', 'end');
