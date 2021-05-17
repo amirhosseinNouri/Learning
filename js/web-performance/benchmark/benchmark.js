01d1b300147a4e18689f585e9bba9a7b7c00dfb2
Amirhossein <amir.h.nouri2000@gmail.com>
2021-05-17 18:24:38 +0430

    Measure performance of deleting and undefining properties of classes

--- js/web-performance/benchmark/benchmark.js
+++ js/web-performance/benchmark/benchmark.js
@@ -15,3 +15,30 @@ function measure(name, fn, iterations) {
   performance.mark('end');
   performance.measure(name, 'start', 'end');
 }
+
+const iterations = 1e5;
+class Point {
+  constructor(x, y, z) {
+    this.x = x;
+    this.y = y;
+    this.z = z;
+  }
+}
+
+const undefineProperty = (property) => {
+  const point = new Point(1, 2, 3);
+  point[property] = undefined;
+};
+
+const deleteProperty = (property) => {
+  const point = new Point(1, 2, 3);
+  delete point[property];
+};
+
+measure('delete x', () => deleteProperty('x'), iterations);
+measure('delete y', () => deleteProperty('y'), iterations);
+measure('delete z', () => deleteProperty('z'), iterations);
+
+measure('undefine x', () => undefineProperty('x'), iterations);
+measure('delete y', () => deleteProperty('y'), iterations);
+measure('delete z', () => deleteProperty('z'), iterations);
