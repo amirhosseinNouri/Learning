91540b38f108d854ea907bfebd43848b821a9d2f
Amirhossein <amir.h.nouri2000@gmail.com>
2021-05-18 09:37:14 +0430

    Implement component that has the fetching logic inside itself

--- react/react-design-patterns/src/App.js
+++ react/react-design-patterns/src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import Counter from './components/counter';
+import Gists from './components/gists';
 // import Foo from './components/foo';
 // import Hello from './components/hello';
 
 function App() {
-  return <Counter />;
+  return <Gists />;
 }
 
 export default App;
