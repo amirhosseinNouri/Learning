78ce7e73ccd293ec3b06117a6e2f195087092d14
Amirhossein <amir.h.nouri2000@gmail.com>
2021-05-22 02:38:28 +0430

    Implement hand function

--- go/cards/deck.go
+++ go/cards/deck.go
@@ -24,3 +24,7 @@ func (d deck) print(){
 		fmt.Println(i , card)
 	}
 }
+
+func deal(d deck , handSize int) (deck , deck){
+	return d[:handSize] , d[handSize:]
+}
--- go/cards/main.go
+++ go/cards/main.go
@@ -3,5 +3,7 @@ package main
 
 func main(){
 	cards := newDeck()
-	cards.print()
+	hand , remainingParts := deal(cards , 5)
+	hand.print()
+	remainingParts.print()
 }
