5b0adcc86052cf99d843c34938711854e402f6c5
Amirhossein <amir.h.nouri2000@gmail.com>
2021-05-22 03:09:39 +0430

    Implement newDeckFromFile function

--- go/cards/deck.go
+++ go/cards/deck.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"os"
 	"strings"
 )
 
@@ -40,4 +41,15 @@ func (d deck) toString() string {
 func (d deck) saveToFile(filename string) error{
 	return ioutil.WriteFile(filename , []byte (d.toString()) , 0666 )
 	
+}
+
+func newDeckFromFile (filename string) deck{
+	bs , err := ioutil.ReadFile(filename)
+	if err != nil {
+		fmt.Println("Error: " , err)
+		os.Exit(1)
+	}
+
+	s := strings.Split(string(bs) , ",")
+	return deck(s)
 }
--- go/cards/main.go
+++ go/cards/main.go
@@ -1,10 +1,8 @@
 package main
 
-import "fmt"
-
 
 func main(){
-	cards := newDeck()
-	fmt.Println(cards.toString())
-	cards.saveToFile("cards")
+	cards := newDeckFromFile("cards")
+	cards.print()
+	
 }
