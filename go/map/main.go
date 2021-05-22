30b7dd7ece9c067a0cf266a88bc28a28e9f7e556
Amirhossein <amir.h.nouri2000@gmail.com>
2021-05-22 20:49:23 +0430

    Define map in two other ways

--- go/map/main.go
+++ go/map/main.go
@@ -3,10 +3,15 @@ package main
 import "fmt"
 
 func main(){
-	colors := map[string]string{
-		"red" : "#ff0000",
-		"green" : "#343233",
-	}
+	// colors := map[string]string{
+	// 	"red" : "#ff0000",
+	// 	"green" : "#343233",
+	// }
+
+	// var colors map[string]string
+	colors := make(map[string]string)
+
+	colors["white"] = "#fff"
 
 	fmt.Println(colors)
 }
