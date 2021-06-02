672df1031c57a52336f7996945c2e92e8307435e
Amirhossein <amir.h.nouri2000@gmail.com>
2021-06-02 09:51:49 +0430

    Definning array and tuples

--- type-script/typescript-fundamentals/notes/index.ts
+++ type-script/typescript-fundamentals/notes/index.ts
@@ -8,3 +8,15 @@ function foo(args: 'hellooooo') {}
 let z: number; // without initializer
 z = 2;
 // z = "hello"; // wrong
+
+// let aa  = [];
+// aa.push(1);
+// aa.push('Some string')
+
+let aa: number[] = [];
+aa.push(2);
+// aa.push('ss');
+
+let t: [number, string, string, number] = [123, 'str', 'str', 13243];
+
+t = [1 , " " , " " , 2];
