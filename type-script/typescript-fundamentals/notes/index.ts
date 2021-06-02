9264f32a7bc0d183c1ed10c16a9059011b387d24
Amirhossein <amir.h.nouri2000@gmail.com>
2021-06-02 09:40:47 +0430

    Defining variables

---
+++ type-script/typescript-fundamentals/notes/index.ts
@@ -0,0 +1,10 @@
+let a = 2;
+// a = "Hello";   // Wrong
+const b = 'hello';
+function foo(args: 'hellooooo') {}
+
+// foo(b); // wrong
+
+let z: number; // without initializer
+z = 2;
+// z = "hello"; // wrong
