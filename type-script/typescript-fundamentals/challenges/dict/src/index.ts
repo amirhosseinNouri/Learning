6fd4ff59b40a8935d3d6e4393be77376453ff0fc
Amirhossein <amir.h.nouri2000@gmail.com>
2021-06-19 11:12:30 +0430

    Implement dictionary map function

--- type-script/typescript-fundamentals/challenges/dict/src/index.ts
+++ type-script/typescript-fundamentals/challenges/dict/src/index.ts
@@ -1,7 +1,32 @@
-export type Dict = {};
+export type Dict<T> = {
+  [k: string]: T | undefined;
+};
 
 // Array.prototype.map, but for Dict
-export function mapDict() {}
+export function mapDict<T, S>(
+  dict: Dict<T>,
+  fn: (arg: T, index: number) => S,
+): Dict<S> {
+  const out: Dict<S> = {};
+  Object.keys(dict).forEach((dkey, idx) => {
+    const currentItem = dict[dkey];
+    if (typeof currentItem !== 'undefined') {
+      out[dkey] = fn(currentItem, idx);
+    }
+  });
+
+  return out;
+}
+
+mapDict(
+  {
+    a: 'a',
+    b: 'b',
+  },
+  (str) => [str],
+);
 
 // Array.prototype.reduce, but for Dict
-export function reduceDict() {}
+export function reduceDict() {
+  dict: Dict;
+}
