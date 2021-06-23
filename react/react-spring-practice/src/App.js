15764260f3275fc32921bf9905cd31a67baafffd
Amirhossein <amir.h.nouri2000@gmail.com>
2021-06-23 19:01:41 +0430

    Animate on mount and unmount

--- react/react-spring-practice/src/App.js
+++ react/react-spring-practice/src/App.js
@@ -1,23 +1,43 @@
 import { useState } from 'react';
 import './App.css';
-import { animated, useSpring, config } from 'react-spring';
+import { animated, useSpring, config, useTransition } from 'react-spring';
 
 function App() {
-  const [flip, setFlip] = useState(false);
-  const { number } = useSpring({
-    number: 1,
-    from: { number: 0 },
-    config: config.molasses,
-    reset: true,
-    reverse: flip,
-    onRest: () => setFlip(!flip),
-    delay: 200,
+  const [items, setItems] = useState([]);
+
+  const transition = useTransition(items, {
+    from: { opacity: 0, x: -100, y: 800, width: 10, height: 10 },
+    enter: (item) => async (next) => {
+      await next({ opacity: 1, y: item.y, delay: item.delay });
+      await next({ x: 0, width: 100, height: 100 });
+    },
+    leave: { opacity: 0, x: 100, y: 800 },
   });
+
+  const buttonText = () => (items.length ? 'Un Mount' : 'Mount');
+
+  const handleButtonClick = () => {
+    setItems((v) =>
+      v.length
+        ? []
+        : [
+            { y: -50, delay: 200 },
+            { y: 0, delay: 400 },
+            { y: 50, delay: 600 },
+          ],
+    );
+  };
+
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <animated.div>{number.to((n) => n.toFixed(2))}</animated.div>
-      </header>
+    <div className='app'>
+      <button onClick={handleButtonClick}>{buttonText()}</button>
+      <div className='container'>
+        {transition((style, item) => {
+          return item ? (
+            <animated.div style={style} className='item'></animated.div>
+          ) : null;
+        })}
+      </div>
     </div>
   );
 }
