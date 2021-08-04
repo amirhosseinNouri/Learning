1d502a686e986dac0d0b2ace5c056482a1b23b6c
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2021-08-04 09:41:28 +0430

    Array reverse exercise

--- js/eloquent-javascript/chapter-4/app.js
+++ js/eloquent-javascript/chapter-4/app.js
@@ -14,4 +14,22 @@ function sum(arr) {
   }
   return sum;
 }
-console.log(sum(range(1, 10)));
+// console.log(sum(range(1, 10)));
+
+function reverseArray(arr) {
+  const reversed = [];
+  for (let i = arr.length - 1; i >= 0; i--) {
+    reversed.push(arr[i]);
+  }
+  return reversed;
+}
+
+function reverseArrayInPlace(arr) {
+  return arr.reverse();
+}
+
+const arr = [1, 2, 3, 4, 5];
+console.log(reverseArray(arr));
+console.log(reverseArrayInPlace(arr));
+console.log(arr === reverseArrayInPlace(arr));
+console.log(arr === reverseArray(arr));
