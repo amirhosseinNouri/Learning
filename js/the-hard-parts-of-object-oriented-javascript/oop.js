b386b0e993a5c2541ee3a8dee1db78dcfe98a298
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2021-08-06 21:11:46 +0430

    Challenge 3

--- js/the-hard-parts-of-object-oriented-javascript/oop.js
+++ js/the-hard-parts-of-object-oriented-javascript/oop.js
@@ -8,4 +8,18 @@ const vicky = makePerson('Vicky', 24);
 const personStore = {
   greet: () => console.log('Hello'),
 };
-personStore.greet(); // -> Logs 'hello'
+// personStore.greet(); // -> Logs 'hello'
+
+function personFromPersonStore(name, age) {
+  const newPerson = Object.create(personStore);
+  newPerson.name = name;
+  newPerson.age = age;
+  return newPerson;
+}
+
+const sandra = personFromPersonStore('Sandra', 26);
+
+// /********* Uncomment these lines to test your work! *********/
+console.log(sandra.name); // -> Logs 'Sandra'
+console.log(sandra.age); //-> Logs 26
+sandra.greet(); //-> Logs 'hello'
