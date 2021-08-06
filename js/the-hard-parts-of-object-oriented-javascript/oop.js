f29b77f2234dda94ce8d0d3ab920ce7ee0ff4e6d
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2021-08-06 21:05:29 +0430

    Challenge 2

--- js/the-hard-parts-of-object-oriented-javascript/oop.js
+++ js/the-hard-parts-of-object-oriented-javascript/oop.js
@@ -2,5 +2,10 @@ const makePerson = (name, age) => ({ name, age });
 
 const vicky = makePerson('Vicky', 24);
 
-console.log(vicky.name); // -> Logs 'Vicky'
-console.log(vicky.age); // -> Logs 24
+// console.log(vicky.name); // -> Logs 'Vicky'
+// console.log(vicky.age); // -> Logs 24
+
+const personStore = {
+  greet: () => console.log('Hello'),
+};
+personStore.greet(); // -> Logs 'hello'
