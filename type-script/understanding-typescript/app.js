0eec52273993720c809aa04779f790a29467a33b
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2021-08-15 16:05:51 +0430

    Define enum

--- type-script/understanding-typescript/app.js
+++ type-script/understanding-typescript/app.js
@@ -1,10 +1,20 @@
-var add = function (n1, n2, logResult, phrase) {
-    var result = n1 + n2;
-    if (logResult) {
-        console.log(phrase + " " + result);
-    }
-    return n1 + n2;
+// const add = (n1: number, n2: number, logResult: boolean, phrase: string) => {
+//   const result: number = n1 + n2;
+//   if (logResult) {
+//     console.log(`${phrase} ${result}`);
+//   }
+//   return n1 + n2;
+// };
+// const a = 2;
+// const b = 3.2;
+// add(a, b, true, 'The result is:');
+var Role;
+(function (Role) {
+    Role[Role["ADMIN"] = 0] = "ADMIN";
+    Role[Role["USER"] = 1] = "USER";
+    Role[Role["MANAGER"] = 2] = "MANAGER";
+})(Role || (Role = {}));
+var person = {
+    role: Role.ADMIN
 };
-var a = 2;
-var b = 3.2;
-add(a, b, true, 'The result is:');
+console.log(person.role);
