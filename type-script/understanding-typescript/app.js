bd401bf66792a60349f0980b305064baba9fd83f
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2021-08-15 16:04:19 +0430

    Simple starter example

--- type-script/understanding-typescript/app.js
+++ type-script/understanding-typescript/app.js
@@ -1,1 +1,10 @@
-console.log('Start');
+var add = function (n1, n2, logResult, phrase) {
+    var result = n1 + n2;
+    if (logResult) {
+        console.log(phrase + " " + result);
+    }
+    return n1 + n2;
+};
+var a = 2;
+var b = 3.2;
+add(a, b, true, 'The result is:');
--- type-script/understanding-typescript/app.ts
+++ type-script/understanding-typescript/app.ts
@@ -1,1 +1,12 @@
-console.log('Start');
+const add = (n1: number, n2: number, logResult: boolean, phrase: string) => {
+  const result: number = n1 + n2;
+  if (logResult) {
+    console.log(`${phrase} ${result}`);
+  }
+  return n1 + n2;
+};
+
+const a = 2;
+const b = 3.2;
+
+add(a, b, true, 'The result is:');
