e2e4185eaea5f8918a9a9f215e43e0a7831376ed
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2021-08-21 08:18:05 +0430

    Enable noEmitOnError option

---
+++ type-script/understanding-typescript/src/app.js
@@ -0,0 +1,5 @@
+"use strict";
+var firstName = 'Amir';
+console.log(firstName);
+var button = document.querySelector('button');
+button === null || button === void 0 ? void 0 : button.addEventListener('click', function () { return console.log('Clicked'); });
