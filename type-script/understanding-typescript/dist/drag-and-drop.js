8a6a88207198adaf118c18318c7d48747b3939c2
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2021-09-02 17:03:30 +0430

    Create fields for inputs and attash event listener on form submition

--- type-script/understanding-typescript/dist/drag-and-drop.js
+++ type-script/understanding-typescript/dist/drag-and-drop.js
@@ -6,10 +6,21 @@ class ProjectInput {
         const importedNode = document.importNode(this.templateElement.content, true);
         this.element = importedNode.firstElementChild;
         this.element.id = 'user-input';
+        this.titleInput = this.element.querySelector('#title');
+        this.descriptionInput = this.element.querySelector('#description');
+        this.peopleInput = this.element.querySelector('#people');
+        this.setupSubmitHandler();
         this.attach();
     }
     attach() {
         this.root.insertAdjacentElement('afterbegin', this.element);
+    }
+    setupSubmitHandler() {
+        this.element.addEventListener('submit', this.handleSubmit.bind(this));
+    }
+    handleSubmit(e) {
+        e.preventDefault();
+        console.log(this.titleInput.value);
     }
 }
 const projectInput = new ProjectInput('project-input', 'app');
--- type-script/understanding-typescript/src/drag-and-drop.ts
+++ type-script/understanding-typescript/src/drag-and-drop.ts
@@ -2,6 +2,9 @@ class ProjectInput {
   templateElement: HTMLTemplateElement;
   root: HTMLDivElement;
   element: HTMLFormElement;
+  titleInput: HTMLInputElement;
+  descriptionInput: HTMLInputElement;
+  peopleInput: HTMLInputElement;
 
   constructor(templateId: string, rootId: string) {
     this.templateElement = document.querySelector(`#${templateId}`)!;
@@ -13,11 +16,30 @@ class ProjectInput {
     );
     this.element = importedNode.firstElementChild as HTMLFormElement;
     this.element.id = 'user-input';
+
+    this.titleInput = this.element.querySelector('#title')! as HTMLInputElement;
+    this.descriptionInput = this.element.querySelector(
+      '#description',
+    )! as HTMLInputElement;
+    this.peopleInput = this.element.querySelector(
+      '#people',
+    )! as HTMLInputElement;
+
+    this.setupSubmitHandler();
     this.attach();
   }
 
   private attach() {
     this.root.insertAdjacentElement('afterbegin', this.element);
+  }
+
+  private setupSubmitHandler() {
+    this.element.addEventListener('submit', this.handleSubmit.bind(this));
+  }
+
+  private handleSubmit(e: Event) {
+    e.preventDefault();
+    console.log(this.titleInput.value);
   }
 }
 
