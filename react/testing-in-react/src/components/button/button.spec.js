b8e3ce1fcda419f5b86b9c0beaee13b3ff92762f
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2021-09-03 12:25:43 +0430

    Setup project for testing react components

---
+++ react/testing-in-react/src/components/button/button.spec.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Button from './button';
+// import renderer from 'react-test-renderer';
+import TestUtils from 'react-dom/test-utils';
+
+test('Works', () => {
+  expect(true).toBe(true);
+});
