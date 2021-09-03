c9a79afa719ce17c61290e1f8f1faa472ee604a3
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2021-09-03 12:45:33 +0430

    Some basic react component tests using renderer

--- react/testing-in-react/src/components/button/button.spec.js
+++ react/testing-in-react/src/components/button/button.spec.js
@@ -1,8 +1,32 @@
 import React from 'react';
 import Button from './button';
-// import renderer from 'react-test-renderer';
+import renderer, { create, act } from 'react-test-renderer';
 import TestUtils from 'react-dom/test-utils';
 
-test('Works', () => {
-  expect(true).toBe(true);
+// test('Works', () => {
+//   expect(true).toBe(true);
+// });
+
+// test('Renders the text', () => {
+//   const text = 'text';
+//   const component = renderer.create(<Button text={text} />);
+//   expect(component.root.props.text).toBe(text);
+// });
+
+test('shit test', () => {
+  let root;
+  act(() => {
+    root = create(<Button text={'text1'} />);
+  });
+
+  // make assertions on root
+  expect(root.toJSON()).toMatchSnapshot();
+
+  // update with some different props
+  act(() => {
+    root.update(<Button text={'text2'} />);
+  });
+
+  // make assertions on root
+  expect(root.toJSON()).toMatchSnapshot();
 });
