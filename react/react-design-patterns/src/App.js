ca6b63d63c86a4e4bffe2f37dc55adf593faa970
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2021-09-09 13:47:36 +0430

    Make app stable

--- react/react-design-patterns/src/App.js
+++ react/react-design-patterns/src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div>
       <Nice />
-      <Evil />
+      {/* <Evil /> */}
       <Nice />
     </div>
   );
--- react/react-design-patterns/src/components/evil/evil.js
+++ react/react-design-patterns/src/components/evil/evil.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import wrapReactLifecycleMethods from 'react-component-errors';
 
-@wrapReactLifecycleMethods
+// @wrapReactLifecycleMethods
 class Evil extends Component {
   render() {
     return <div>{this.props.someProp.name}</div>;
