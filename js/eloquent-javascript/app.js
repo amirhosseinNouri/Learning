465e3c4fc28b23f5f6b5454343248e37ea1cfc15
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2021-09-15 08:43:40 +0430

    Draw a simple canvas

--- js/eloquent-javascript/app.js
+++ js/eloquent-javascript/app.js
@@ -1,2 +1,7 @@
 const circle = document.querySelector('circle');
 circle.setAttribute('fill', 'cyan');
+
+const canvas = document.querySelector('canvas');
+const context = canvas.getContext('2d');
+context.fillStyle = 'gray';
+context.fillRect(30, 10, 100, 50);
