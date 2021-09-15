d4b9fa592631cbdd69ec1b66b62e7a0ceeb21a5a
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2021-09-15 08:59:02 +0430

    Add path using canvas

--- js/eloquent-javascript/app.js
+++ js/eloquent-javascript/app.js
@@ -9,3 +9,12 @@ context.strokeRect(5, 5, 50, 50);
 context.lineWidth = 5;
 context.strokeRect(135, 5, 50, 50);
 // context.fillRect(30, 10, 100, 50);
+
+const pathCanvas = document.querySelector('.path').getContext('2d');
+pathCanvas.beginPath();
+for (let y = 10; y < 100; y += 10) {
+  pathCanvas.moveTo(10, y);
+  pathCanvas.lineTo(90, y);
+}
+
+pathCanvas.stroke();
