39592c492e6bd2c2341b29d455b581d2dd6809ee
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2021-09-15 08:53:10 +0430

    Draw canvas using their stroke

--- js/eloquent-javascript/app.js
+++ js/eloquent-javascript/app.js
@@ -3,5 +3,9 @@ circle.setAttribute('fill', 'cyan');
 
 const canvas = document.querySelector('canvas');
 const context = canvas.getContext('2d');
-context.fillStyle = 'gray';
-context.fillRect(30, 10, 100, 50);
+// context.fillStyle = 'green';
+context.strokeStyle = 'red';
+context.strokeRect(5, 5, 50, 50);
+context.lineWidth = 5;
+context.strokeRect(135, 5, 50, 50);
+// context.fillRect(30, 10, 100, 50);
