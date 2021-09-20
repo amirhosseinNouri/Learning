753eee9a8572793d34998589c0bfbd958e6c7912
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2021-09-20 10:01:30 +0430

    Read from and write info files using fileSystem module

--- node/app.js
+++ node/app.js
@@ -1,1 +1,26 @@
-console.log(process.argv);
+const { readFile, writeFile, promises, readFileSync } = require('fs');
+
+readFile('file', 'utf8', (error, content) => {
+  if (error) {
+    console.error(error);
+    return;
+  }
+
+  console.log(content);
+});
+
+writeFile('write.txt', 'Node was here', (error) => {
+  if (error) {
+    console.error(error);
+    return;
+  }
+
+  console.log('File written successfully.');
+});
+
+promises.readFile('write.txt', 'utf-8').then(console.log).catch(console.error);
+
+console.log('Before reading file sync');
+const content = readFileSync('write.txt', 'utf-8');
+console.log(content);
+console.log('After reading file sync');
