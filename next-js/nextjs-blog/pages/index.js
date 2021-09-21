59272dc390a94076fdf144bb0130d9ccd5a373c2
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2021-09-22 00:01:29 +0330

    Implement static generation with data using getStaticProps function

--- next-js/nextjs-blog/pages/index.js
+++ next-js/nextjs-blog/pages/index.js
@@ -1,8 +1,9 @@
 import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
+import { getStoredPostsData } from '../lib/posts';
 
-export default function Home() {
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
@@ -15,6 +16,30 @@ export default function Home() {
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
       </section>
+
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
+      </section>
     </Layout>
   );
+}
+
+export async function getStaticProps() {
+  const allPostsData = getStoredPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
 }
