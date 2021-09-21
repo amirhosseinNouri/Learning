da8f78aa50e3f5e6903a5d67685fe9d8322882b9
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2021-09-21 23:53:46 +0430

    Add client side navigation

--- next-js/nextjs-blog/pages/index.js
+++ next-js/nextjs-blog/pages/index.js
@@ -1,6 +1,7 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from '../styles/Home.module.css';
+import Link from 'next/link';
 
 export default function Home() {
   return (
@@ -13,7 +14,10 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Learn <a href="https://nextjs.org">Next.js!</a>
+          Read {` `}
+          <Link href="./posts/first-post">
+            <a>This page</a>
+          </Link>
         </h1>
 
         <p className={styles.description}>
@@ -65,5 +69,5 @@ export default function Home() {
         </a>
       </footer>
     </div>
-  )
+  );
 }
--- next-js/nextjs-blog/pages/posts/first-post.js
+++ next-js/nextjs-blog/pages/posts/first-post.js
@@ -1,3 +1,13 @@
+import Link from 'next/link';
 export default function FirstPost() {
-  return <h1>First Post page</h1>;
+  return (
+    <>
+      <h1>First Post</h1>
+      <h2>
+        <Link href="/">
+          <a>Back to home page</a>
+        </Link>
+      </h2>
+    </>
+  );
 }
