48ef09ceff00844a2c3af41ed1ecaf644b3bbb8b
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2021-09-21 23:04:55 +0330

    Change head of the page

--- next-js/nextjs-blog/pages/posts/first-post.js
+++ next-js/nextjs-blog/pages/posts/first-post.js
@@ -1,9 +1,13 @@
 import Link from 'next/link';
 import Image from 'next/image';
+import Head from 'next/head';
 
 export default function FirstPost() {
   return (
     <>
+      <Head>
+        <title>First Post Page</title>
+      </Head>
       <h1>First Post</h1>
       <h2>
         <Link href="/">
