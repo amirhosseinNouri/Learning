5f1e03ec584680b1bd212ae7e200a6bf4474cec7
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2021-09-26 14:15:02 +0330

    Implement server

---
+++ js/webrtc/video-chat-app/src/server.ts
@@ -0,0 +1,39 @@
+import express, { Application } from 'express';
+import socketIO, { Server as SocketIOServer } from 'socket.io';
+import { createServer, Server as HTTPServer } from 'http';
+
+export class Server {
+  private httpServer: HTTPServer;
+  private app: Application;
+  private io: SocketIOServer;
+
+  private readonly DEFAULT_PORT = 5000;
+
+  constructor() {
+    this.app = express();
+    this.httpServer = createServer(this.app);
+    this.io = new SocketIOServer(this.httpServer);
+
+    this.handleRoutes();
+
+    this.handleSocketConnection();
+  }
+
+  private handleRoutes(): void {
+    this.app.get('/', (req, res) => {
+      res.send(`<h1>Hello World</h1>`);
+    });
+  }
+
+  private handleSocketConnection(): void {
+    this.io.on('connection', (socket) => {
+      console.log('Socket connected');
+    });
+  }
+
+  public listen(callback: (port: number) => void): void {
+    this.httpServer.listen(this.DEFAULT_PORT, () =>
+      callback(this.DEFAULT_PORT),
+    );
+  }
+}
