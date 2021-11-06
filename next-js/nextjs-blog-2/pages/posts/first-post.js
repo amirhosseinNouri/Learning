b2ec5a201256279c880d0346be5794ad3dcbec14
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2021-11-07 00:21:17 +0330

    feat: change page meta data using Head component

--- next-js/nextjs-blog-2/pages/posts/first-post.js
+++ next-js/nextjs-blog-2/pages/posts/first-post.js
@@ -1,8 +1,12 @@
 import Link from 'next/link';
+import Head from 'next/head';
 
 export default function FirstPage() {
   return (
     <>
+      <Head>
+        <title>First Post</title>
+      </Head>
       <h1>First Post</h1>
 
       <h2>
