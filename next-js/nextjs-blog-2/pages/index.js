5fddb77ffacee318b52a4fd8d6356475b57160b1
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2021-11-07 00:00:36 +0330

    feat: add simple client side navigation

--- next-js/nextjs-blog-2/pages/index.js
+++ next-js/nextjs-blog-2/pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import Link from 'next/link';
 
 export default function Home() {
   return (
@@ -10,7 +11,10 @@ export default function Home() {
 
       <main>
         <h1 className="title">
-          Learn <a href="https://nextjs.org">Next.js!</a>
+          Read{' '}
+          <Link href="/posts/first-post">
+            <a>This page!</a>
+          </Link>
         </h1>
 
         <p className="description">
---
+++ next-js/nextjs-blog-2/pages/posts/first-post.js
@@ -0,0 +1,3 @@
+export default function FirstPage() {
+  return <h1>First Post</h1>;
+}
