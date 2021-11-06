940b0c3296273eda67631b7ebd0cdb8a0fa36091
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2021-11-06 23:53:50 +0330

    feat: simple change in home page

--- next-js/nextjs-blog-2/pages/index.js
+++ next-js/nextjs-blog-2/pages/index.js
@@ -1,4 +1,4 @@
-import Head from 'next/head'
+import Head from 'next/head';
 
 export default function Home() {
   return (
@@ -10,7 +10,7 @@ export default function Home() {
 
       <main>
         <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Learn <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <p className="description">
@@ -54,8 +54,7 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className="logo" />
+          Powered by <img src="/vercel.svg" alt="Vercel" className="logo" />
         </a>
       </footer>
 
@@ -205,5 +204,5 @@ export default function Home() {
         }
       `}</style>
     </div>
-  )
+  );
 }
