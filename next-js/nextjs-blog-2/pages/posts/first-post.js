fd4d1330d74d93749025a6876e53c8679a9faf7a
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2021-11-07 00:02:47 +0330

    feat: add link from first post page to home page

--- next-js/nextjs-blog-2/pages/posts/first-post.js
+++ next-js/nextjs-blog-2/pages/posts/first-post.js
@@ -1,3 +1,15 @@
+import Link from 'next/link';
+
 export default function FirstPage() {
-  return <h1>First Post</h1>;
+  return (
+    <>
+      <h1>First Post</h1>
+
+      <h2>
+        <Link href="/">
+          <a>Back to home</a>
+        </Link>
+      </h2>
+    </>
+  );
 }
