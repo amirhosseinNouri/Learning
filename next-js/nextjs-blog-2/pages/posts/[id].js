cbdb2082857981300a4da60dbc10dca68c39d69a
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2021-11-08 21:33:10 +0330

    feat: render date on post page

--- next-js/nextjs-blog-2/pages/posts/[id].js
+++ next-js/nextjs-blog-2/pages/posts/[id].js
@@ -1,6 +1,7 @@
 import Layout from '../../components/layout';
 import { getAllPostIds, getPostData } from '../../lib/posts';
 import Head from 'next/head';
+import Date from '../../components/date';
 
 export default function Post({ postData }) {
   return (
@@ -12,7 +13,7 @@ export default function Post({ postData }) {
       <br />
       {postData.id}
       <br />
-      {postData.date}
+      <Date dateString={postData.date} />
       <br />
       <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }}></div>
     </Layout>
