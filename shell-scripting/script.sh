6522187b0c20b22810687e34ffc40ea79163f25e
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2021-12-20 16:20:46 +0330

    use if and else and elif

--- shell-scripting/script.sh
+++ shell-scripting/script.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-MY_SHELL="bash"
+MY_SHELL="sh"
 echo "I am ${MY_SHELL}ing on my keyboard"
+
+if [ "$MY_SHELL" = "bash" ]
+then
+    echo "It was true"
+else
+    echo "It was not true"
+fi
