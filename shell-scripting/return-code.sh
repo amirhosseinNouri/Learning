a704840307ed1528cc51250611506879df999f1d
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2021-12-21 09:50:47 +0330

    check if host is reachable

---
+++ shell-scripting/return-code.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# ls /not/here
+# echo ${?}
+
+HOST="app.snapp.taxi"
+ping -c 1 $HOST > /dev/null
+
+if [ "$?" -eq 0 ]
+then
+    echo "$HOST is reachable"
+else
+    echo "$HOST is unreachable"
+fi
