2451c9abad30da254a04fed7aec0780de7b23323
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2021-12-21 11:55:08 +0330

    implement backup function

--- shell-scripting/function.sh
+++ shell-scripting/function.sh
@@ -1,15 +1,38 @@
 #!/bin/bash
 
-function hello(){
-    for NAME in $@
-    do
-        echo "Hello $NAME"
-    done
-    now
+# function hello(){
+#     for NAME in $@
+#     do
+#         echo "Hello $NAME"
+#     done
+#     now
+# }
+
+# function now(){
+#     echo "It's $(date +%r)"
+# }
+
+# hello amirhossein amir ali
+
+function backup_file(){
+    FILE=$1
+    if [ -f $FILE ]
+    then
+        BACKUP_FILE="/tmp/$(basename ${FILE}).$(date +%F).$$"
+        echo "Backing up ${FILE} to ${BACKUP_FILE}"
+        cp $FILE $BACKUP_FILE
+
+    else
+        echo "No such a file"
+        return 1   
+    fi
 }
 
-function now(){
-    echo "It's $(date +%r)"
-}
+backup_file /etc/hosts
 
-hello amirhossein amir ali
+if [ $? -eq 0 ]
+then
+    echo "Backup succeeded!"
+else
+    echo "Backup failed"
+fi
