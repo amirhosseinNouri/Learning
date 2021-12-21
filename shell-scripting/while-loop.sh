dc560bbd4b0e77216b642a7cea52a89411af849f
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2021-12-21 16:19:43 +0330

    implement a simple monitoring system using while loop

--- shell-scripting/while-loop.sh
+++ shell-scripting/while-loop.sh
@@ -1,8 +1,18 @@
-INDEX=1
+#!/bin/bash
 
-while [ $INDEX -lt 6 ]
+# INDEX=1
+# 
+# while [ $INDEX -lt 6 ]
+# do
+#     echo "Creating project-${INDEX}"
+#     mkdir local/project-${INDEX}
+#     ((INDEX++))
+# done
+
+while ping -c 1 app.snapp.taxi > /dev/null
 do
-    echo "Creating project-${INDEX}"
-    mkdir local/project-${INDEX}
-    ((INDEX++))
+    echo "snapp is sill up..."
+    sleep 5
 done
+
+echo "Snapp is down =("
