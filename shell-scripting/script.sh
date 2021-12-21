c058a768f3bf8529cd680ab19bd50b5ef299a860
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2021-12-21 08:52:24 +0330

    loop over list of items

--- shell-scripting/script.sh
+++ shell-scripting/script.sh
@@ -1,23 +1,8 @@
 #!/bin/bash
 
-MY_SHELL="sh"
-echo "I am ${MY_SHELL}ing on my keyboard"
-
-if [ "$MY_SHELL" = "bash" ]
-then
-    echo "It was true"
-else
-    echo "It was not true"
-fi
-
-for COLOR in red green blue
-do
-    echo "COLOR: $COLOR"
-done
-
 CHARACTERS="A B C D"
 
 for CHARACTER in $CHARACTERS
 do
-    echo "Character: $CHARACTERS"
+    echo "Character: $CHARACTER"
 done
