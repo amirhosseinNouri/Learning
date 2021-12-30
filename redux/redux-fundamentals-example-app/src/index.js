3436874ac920e8b6496f2f023799491766c6133c
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2021-12-30 20:14:56 +0330

    add store to index.js

--- redux/redux-fundamentals-example-app/src/index.js
+++ redux/redux-fundamentals-example-app/src/index.js
@@ -2,8 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
+import store from './store'
 
 import './api/server'
+
+console.log(store.getState())
+
+store.subscribe(() => console.log(store.getState()))
+
+store.dispatch({ type: 'todos/todoAdded', payload: 'Learn about actions' })
 
 ReactDOM.render(
   <React.StrictMode>
