64120c2e0f3d52a65575b97f2e19ca12eb9beb1b
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2021-12-30 20:06:26 +0330

    use redux combine reducer function to create root reducer

--- redux/redux-fundamentals-example-app/src/reducer.js
+++ redux/redux-fundamentals-example-app/src/reducer.js
@@ -1,9 +1,10 @@
 import todosReducer from './features/todos/todosSlice'
 import filtersReducer from './features/filters/filtersSlice'
+import { combineReducers } from 'redux'
 
-export default function rootReducer(state = {}, action) {
-  return {
-    todos: todosReducer(state.todos, action),
-    filters: filtersReducer(state.filters, action),
-  }
-}
+const rootReducer = combineReducers({
+  todos: todosReducer,
+  filters: filtersReducer,
+})
+
+export default rootReducer
