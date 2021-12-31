7c8f0786a65a227e075573a46da490cfe670fd55
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2021-12-31 16:15:23 +0330

    implement header component and dispatch an action using useDispatch hook

---
+++ redux/redux-fundamentals-example-app/src/components/header/header.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react'
+import { useDispatch } from 'react-redux'
+
+export default function Header() {
+  const [value, setValue] = useState('')
+  const dispatch = useDispatch()
+
+  const handleChange = (e) => setValue(e.target.value)
+
+  const handleKeyDown = (e) => {
+    const trimmedText = e.target.value.trim()
+
+    if (e.key === 'Enter' && trimmedText) {
+      dispatch({ type: 'todo/todoAdded', payload: trimmedText })
+      setValue('')
+    }
+  }
+
+  return (
+    <input
+      type="text"
+      placeholder="What need to be done?"
+      autoFocus={true}
+      value={value}
+      onChange={handleChange}
+      onKeyDown={handleKeyDown}
+    />
+  )
+}
