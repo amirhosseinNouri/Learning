d85d128e185bfa9b2e104d3004c7b437ae6f1f3c
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2021-12-31 16:55:52 +0330

    implement some action creators

--- redux/redux-fundamentals-example-app/src/features/todos/todosSlice.js
+++ redux/redux-fundamentals-example-app/src/features/todos/todosSlice.js
@@ -34,3 +34,17 @@ export default function todosReducer(state = initialState, action) {
       return state
   }
 }
+
+export const todosLoaded = (todos) => {
+  return {
+    type: 'todos/todosLoaded',
+    payload: todos,
+  }
+}
+
+export const todoAdded = (todo) => {
+  return {
+    type: 'todos/todoAdded',
+    payload: todo,
+  }
+}
