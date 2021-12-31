e1f7413c13f1bd6f55f9267a140c930954524dd9
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2021-12-31 15:42:19 +0330

    add some middlewares as special enhancer

--- redux/redux-fundamentals-example-app/src/store.js
+++ redux/redux-fundamentals-example-app/src/store.js
@@ -1,11 +1,17 @@
-import { createStore, compose } from 'redux'
+import { createStore, compose, applyMiddleware } from 'redux'
 import rootReducer from './reducer'
 import {
   sayHiOnDispatch,
   includeMeaningOfLife,
 } from './exampleAddons/enhancers'
+import { print1, print2, print3 } from './exampleAddons/middleware'
 
-const composedEnhancer = compose(sayHiOnDispatch, includeMeaningOfLife)
+const middlewareEnhancer = applyMiddleware(print1, print2, print3)
+const composedEnhancer = compose(
+  sayHiOnDispatch,
+  includeMeaningOfLife,
+  middlewareEnhancer
+)
 
 const store = createStore(rootReducer, undefined, composedEnhancer)
 
