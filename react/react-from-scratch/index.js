76a133c5eda1daf4260af56cc39f1c084fabdb58
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2022-01-14 12:30:08 +0330

    refactor: extract createElement function

--- react/react-from-scratch/index.js
+++ react/react-from-scratch/index.js
@@ -1,13 +1,8 @@
-import { render, createTextElement } from './utils.js';
+import { render, createTextElement, createElement } from './utils.js';
 
 const textElement = createTextElement('Hello');
 
-const element = {
-  type: 'h1',
-  props: {
-    children: [textElement],
-  },
-};
+const element = createElement('h1' , undefined , textElement);
 
 const container = document.getElementById('root');
 
--- react/react-from-scratch/utils.js
+++ react/react-from-scratch/utils.js
@@ -30,3 +30,15 @@ export function createTextElement(text) {
     },
   };
 }
+
+export function createElement(type, props, ...children) {
+  return {
+    type,
+    props: {
+      ...props,
+      children: children.map((child) =>
+        typeof child === 'object' ? child : createTextElement(child),
+      ),
+    },
+  };
+}
