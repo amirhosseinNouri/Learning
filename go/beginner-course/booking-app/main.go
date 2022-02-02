49730a2a3f18f2f3047b9849f4c30ab970aa3246
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2022-02-03 02:06:25 +0330

    print type of the variables

--- go/beginner-course/booking-app/main.go
+++ go/beginner-course/booking-app/main.go
@@ -7,6 +7,8 @@ func main(){
 	const conferenceTickets = 50
 	var remainingTickets = conferenceTickets
 
+	fmt.Printf("conferencTicket is %T, remainingTickets is %T and conferenceName is %T\n", conferenceTickets , remainingTickets , conferenceName)
+
 	fmt.Printf("Welcome to %v our booking application.\n" , conferenceName)
 	fmt.Printf("We have total of %v tickets and %v are still available.\n" , conferenceTickets ,remainingTickets)
 	fmt.Println("Get you tickets here.")
