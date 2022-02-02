18ef746c21b2a516f429e6963b3b8b5dc811b516
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2022-02-03 01:59:02 +0330

    use formatting function

--- go/beginner-course/booking-app/main.go
+++ go/beginner-course/booking-app/main.go
@@ -7,8 +7,8 @@ func main(){
 	const conferenceTickets = 50
 	var remainingTickets = conferenceTickets
 
-	fmt.Println("Welcome to" , conferenceName ,  "our booking application.")
-	fmt.Println("We have total of" , conferenceTickets , "tickets and" , remainingTickets , "are still available.")
+	fmt.Printf("Welcome to %v our booking application.\n" , conferenceName)
+	fmt.Printf("We have total of %v tickets and %v are still available.\n" , conferenceTickets ,remainingTickets)
 	fmt.Println("Get you tickets here.")
 
 
