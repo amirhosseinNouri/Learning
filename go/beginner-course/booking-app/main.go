6baaa17031d6a7889f17ed3c0221536222d552c8
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2022-02-03 02:33:25 +0330

    use slice

--- go/beginner-course/booking-app/main.go
+++ go/beginner-course/booking-app/main.go
@@ -14,7 +14,7 @@ func main(){
 	fmt.Println("Get you tickets here.")
 
 	// var bookings = [50]string{}
-	var bookings [50]string
+	var bookings []string
 
 	var firstname string
 	var lastname string
@@ -36,16 +36,11 @@ func main(){
 
 	remainingTickets = remainingTickets - userTickets
 
-	bookings[0] = firstname + " " + lastname
-
-	fmt.Printf("The whole array: %v\n", bookings)
-	fmt.Printf("The first value: %v\n", bookings[0])
-	fmt.Printf("Array type: %T\n", bookings)
-	fmt.Printf("Array length: %v\n", len(bookings))
+	bookings = append(bookings, firstname + " " + lastname)
 
 	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation Email at %v\n" ,firstname , lastname , userTickets , email)
 	fmt.Printf("%v tickets remaining for %v\n", remainingTickets , conferenceName)
 
 	
-	fmt.Printf("User %v booked %v tickets.\n" , firstname, userTickets)
+	fmt.Printf("These are all the bookings %v\n" , bookings)
 }	
