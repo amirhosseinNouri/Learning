954ef9f17b359d257a7f0523abe2e8b0f7ace923
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2022-02-03 11:31:22 +0330

    use loop

--- go/beginner-course/booking-app/main.go
+++ go/beginner-course/booking-app/main.go
@@ -2,45 +2,45 @@ package main
 
 import "fmt"
 
-func main(){
+func main() {
 	var conferenceName = "Go conference"
 	const conferenceTickets = 50
 	var remainingTickets uint = conferenceTickets
 
-	fmt.Printf("conferencTicket is %T, remainingTickets is %T and conferenceName is %T\n", conferenceTickets , remainingTickets , conferenceName)
+	fmt.Printf("conferencTicket is %T, remainingTickets is %T and conferenceName is %T\n", conferenceTickets, remainingTickets, conferenceName)
 
-	fmt.Printf("Welcome to %v our booking application.\n" , conferenceName)
-	fmt.Printf("We have total of %v tickets and %v are still available.\n" , conferenceTickets ,remainingTickets)
+	fmt.Printf("Welcome to %v our booking application.\n", conferenceName)
+	fmt.Printf("We have total of %v tickets and %v are still available.\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get you tickets here.")
 
-	// var bookings = [50]string{}
 	var bookings []string
 
-	var firstname string
-	var lastname string
-	var email string
-	var userTickets uint
+	for {
+		var firstname string
+		var lastname string
+		var email string
+		var userTickets uint
 
-	fmt.Print("Enter your first name: ")
-	fmt.Scan(&firstname)
+		fmt.Print("Enter your first name: ")
+		fmt.Scan(&firstname)
 
-	fmt.Print("Enter your last name: ")
-	fmt.Scan(&lastname)
+		fmt.Print("Enter your last name: ")
+		fmt.Scan(&lastname)
 
+		fmt.Print("Enter your Email address: ")
+		fmt.Scan(&email)
 
-	fmt.Print("Enter your Email address: ")
-	fmt.Scan(&email)
+		fmt.Print("Enter the number of tickets: ")
+		fmt.Scan(&userTickets)
 
-	fmt.Print("Enter the number of tickets: ")
-	fmt.Scan(&userTickets)
+		remainingTickets = remainingTickets - userTickets
 
-	remainingTickets = remainingTickets - userTickets
+		bookings = append(bookings, firstname+" "+lastname)
 
-	bookings = append(bookings, firstname + " " + lastname)
+		fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation Email at %v\n", firstname, lastname, userTickets, email)
+		fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
 
-	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation Email at %v\n" ,firstname , lastname , userTickets , email)
-	fmt.Printf("%v tickets remaining for %v\n", remainingTickets , conferenceName)
+		fmt.Printf("These are all the bookings %v\n", bookings)
+	}
 
-	
-	fmt.Printf("These are all the bookings %v\n" , bookings)
-}	
+}
