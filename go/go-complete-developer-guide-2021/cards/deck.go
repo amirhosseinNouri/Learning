6e0b84de362bacadb4d81c2b9f345906233cd1a6
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2022-02-04 10:50:00 +0330

    implement toString function

--- go/go-complete-developer-guide-2021/cards/deck.go
+++ go/go-complete-developer-guide-2021/cards/deck.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 // Create a new type of 'deck'
 // Which is a slice of strings
@@ -31,4 +34,8 @@ func (d deck) print() {
 
 func deal(d deck, handSize int) (deck, deck) {
 	return d[:handSize], d[handSize:]
+}
+
+func (d deck) toString() string {
+	return strings.Join([]string(d), ",")
 }
--- go/go-complete-developer-guide-2021/cards/main.go
+++ go/go-complete-developer-guide-2021/cards/main.go
@@ -3,9 +3,13 @@ package main
 import "fmt"
 
 func main() {
+	// cards := newDeck()
+	// hand, remainingDeck := deal(cards, 5)
+	// hand.print()
+	// fmt.Println("--")
+	// remainingDeck.print()
+
 	cards := newDeck()
-	hand, remainingDeck := deal(cards, 5)
-	hand.print()
-	fmt.Println("--")
-	remainingDeck.print()
+	fmt.Println(cards.toString())
+
 }
