f43ec2c4df49ca52cb84b9de56b2f2c062dcb576
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2022-02-04 17:49:39 +0330

    iterate over map

---
+++ go/go-complete-developer-guide-2021/map-playground/go.mod
@@ -0,0 +1,3 @@
+module map-playground
+
+go 1.17
--- go/go-complete-developer-guide-2021/map-playground/main.go
+++ go/go-complete-developer-guide-2021/map-playground/main.go
@@ -5,21 +5,18 @@ import "fmt"
 func main() {
 
 	/* First way */
-	// colors := map[string]string{
-	// 	"red":   "#ff0000",
-	// 	"black": "#111111",
-	// }
+	colors := map[string]string{
+		"red":   "#ff0000",
+		"black": "#111111",
+		"white": "#ffffff",
+	}
 
-	/* Second way */
-	// var colors map[string]string
+	// fmt.Println(colors)
+	printMap(colors)
+}
 
-	/* Third way */
-	colors := make(map[string]string)
-
-
-	colors["white"] = "#ffffff"
-
-	delete(colors , "white")
-
-	fmt.Println(colors)
+func printMap(c map[string]string) {
+	for key, value := range c {
+		fmt.Printf("%v=%v\n", key, value)
+	}
 }
