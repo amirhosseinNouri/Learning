84e954618321519ac424eed988b6904d71f0eec0
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2022-02-04 11:46:22 +0330

    improve new deck function test

--- go/go-complete-developer-guide-2021/cards/deck_test.go
+++ go/go-complete-developer-guide-2021/cards/deck_test.go
@@ -10,4 +10,12 @@ func TestNewDeck(t *testing.T) {
 	if len(d) != 16 {
 		t.Errorf("Expected deck length of 20, but got %v", len(d))
 	}
+
+	if d[0] != "Ace of Spades" {
+		t.Errorf("Expected the first card to be Ace of Spades, but got %v", d[0])
+	}
+
+	if d[len(d)-1] != "Four of Clubs" {
+		t.Errorf("Expected the first card to be Four of Clubs, but got %v", d[len(d)-1])
+	}
 }
