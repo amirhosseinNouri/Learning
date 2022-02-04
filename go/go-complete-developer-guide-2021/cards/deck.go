36c3fd7ee2c73ac49eb07d3959458f542bed868b
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2022-02-04 10:00:34 +0330

    implement new deck function

--- go/go-complete-developer-guide-2021/cards/deck.go
+++ go/go-complete-developer-guide-2021/cards/deck.go
@@ -7,6 +7,22 @@ import "fmt"
 
 type deck []string
 
+func newDeck() deck {
+	cards := deck{}
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four"}
+
+	for _, suit := range cardSuits {
+
+		for _, value := range cardValues {
+			cards = append(cards, fmt.Sprintf("%v of %v", suit, value))
+		}
+	}
+
+	return cards
+
+}
+
 func (d deck) print() {
 	for _, card := range d {
 		fmt.Println(card)
--- go/go-complete-developer-guide-2021/cards/main.go
+++ go/go-complete-developer-guide-2021/cards/main.go
@@ -1,16 +1,6 @@
 package main
 
-import "fmt"
-
 func main() {
-	cards := []string{newCard(), newCard(), "last"}
-	cards = append(cards, "A new one")
-
-	for _, card := range cards {
-		fmt.Println(card)
-	}
-}
-
-func newCard() string {
-	return "Five of Diamonds"
+	cards := newDeck()
+	cards.print()
 }
