9e4ea9b431b0c52ecb0c4e3d030d4131b2d76c93
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2022-02-04 11:41:26 +0330

    initial test of new deck function

---
+++ go/go-complete-developer-guide-2021/cards/deck_test.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestNewDeck(t *testing.T) {
+	d := newDeck()
+
+	if len(d) != 16 {
+		t.Errorf("Expected deck length of 20, but got %v", len(d))
+	}
+}
