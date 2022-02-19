de5be01c05c1fe409e5df4a4acb587d30e0849b9
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2022-02-19 17:33:55 +0330

    log the http response

---
+++ go/go-complete-developer-guide-2021/http/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func main() {
+	resp, err := http.Get("http://google.com")
+
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+
+	fmt.Println(resp)
+}
