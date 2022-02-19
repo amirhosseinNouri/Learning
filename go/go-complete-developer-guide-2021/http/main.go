de25853ffd18827dacb4d9e5b082c8ef3455647c
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2022-02-19 17:55:16 +0330

    use io.Copy function

--- go/go-complete-developer-guide-2021/http/main.go
+++ go/go-complete-developer-guide-2021/http/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 	"os"
 )
@@ -14,7 +15,5 @@ func main() {
 		os.Exit(1)
 	}
 
-	bs := make([]byte, 99999)
-	resp.Body.Read(bs)
-	fmt.Println(string(bs))
+	io.Copy(os.Stdout, resp.Body)
 }
