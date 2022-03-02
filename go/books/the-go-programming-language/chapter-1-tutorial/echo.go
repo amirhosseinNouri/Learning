adf6a7bf9b4a01f2975ac36952c3c4335bc6301b
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2022-03-02 13:35:07 +0330

    improve echo for large amount of arguments

--- go/books/the-go-programming-language/chapter-1-tutorial/echo.go
+++ go/books/the-go-programming-language/chapter-1-tutorial/echo.go
@@ -4,14 +4,17 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
 
-	s, sep := "", ""
-	for _, arg := range os.Args[1:] {
-		s += sep + arg
-		sep = " "
-	}
-	fmt.Println(s)
+	// s, sep := "", ""
+	// for _, arg := range os.Args[1:] {
+	// 	s += sep + arg
+	// 	sep = " "
+	// }
+	// fmt.Println(s)
+
+	fmt.Println(strings.Join(os.Args[1:], " "))
 }
