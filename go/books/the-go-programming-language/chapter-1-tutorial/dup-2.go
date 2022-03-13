a86fd7f0d4557bbca08bde606398991fdea8c60a
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2022-03-13 21:49:48 +0330

    feat: implement dup2 to accept file

---
+++ go/books/the-go-programming-language/chapter-1-tutorial/dup-2.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	counts := make(map[string]int)
+	files := os.Args[1:]
+
+	if len(files) == 0 {
+		countLines(os.Stdin, counts)
+	} else {
+		for _, arg := range files {
+			f, err := os.Open(arg)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "dup2: %v\n", err)
+				continue
+			}
+
+			countLines(f, counts)
+			f.Close()
+		}
+	}
+
+	fmt.Println(counts)
+}
+
+func countLines(f *os.File, counts map[string]int) {
+	input := bufio.NewScanner(f)
+
+	for input.Scan() {
+		counts[input.Text()]++
+	}
+}
