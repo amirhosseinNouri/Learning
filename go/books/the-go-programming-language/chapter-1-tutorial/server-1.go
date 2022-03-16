ecf3caddf2bf8c0e9fa2aeb1ef6d4421fdeeb9ab
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2022-03-16 23:21:21 +0330

    print info about request

--- go/books/the-go-programming-language/chapter-1-tutorial/server-1.go
+++ go/books/the-go-programming-language/chapter-1-tutorial/server-1.go
@@ -17,6 +17,23 @@ func main() {
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "%s\t%s\t%s\n", r.Method, r.URL.Path, r.Proto)
+
+	for k, v := range r.Header {
+		fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
+	}
+
+	fmt.Fprintf(w, "Host = %q\n", r.Host)
+	fmt.Fprintf(w, "RemoteAddr = %q\n", r.RemoteAddr)
+
+	if err := r.ParseForm(); err != nil {
+		log.Print(err)
+	}
+
+	for k, v := range r.Form {
+		fmt.Fprintf(w, "Form[%q] = %q\n", k, v)
+	}
+
 	lock.Lock()
 	count++
 	lock.Unlock()
