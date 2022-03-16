a88289cd067f517ca45a60055b816342c2700e65
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2022-03-16 23:09:47 +0330

    add count handler

--- go/books/the-go-programming-language/chapter-1-tutorial/server-1.go
+++ go/books/the-go-programming-language/chapter-1-tutorial/server-1.go
@@ -4,14 +4,27 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"sync"
 )
+
+var lock sync.Mutex
+var count int
 
 func main() {
 	http.HandleFunc("/", handler)
+	http.HandleFunc("/count", counter)
 	log.Println(http.ListenAndServe("localhost:8080", nil))
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
+	lock.Lock()
+	count++
+	lock.Unlock()
 	fmt.Fprintf(w, "URL.Path= %q\n", r.URL.Path)
 }
- 
+
+func counter(w http.ResponseWriter, r *http.Request) {
+	lock.Lock()
+	fmt.Fprintf(w, "Count %d\n", count)
+	lock.Unlock()
+}
