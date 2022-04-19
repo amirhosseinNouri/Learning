3b81ed82b54b8d529586fbe1d0781ceede77c599
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2022-04-19 15:24:34 +0430

    add todos reducer

--- redux-toolkit-js-playground/src/store.js
+++ redux-toolkit-js-playground/src/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import usersReducer from './usersReducer';
 import postsReducer from './postsReducer';
+import todosReducer from './todosReducer';
 
 const store = configureStore({
   reducer: {
     users: usersReducer,
     posts: postsReducer,
+    todos: todosReducer,
   },
 });
 
