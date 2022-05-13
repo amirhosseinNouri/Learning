bcfa23080e11c7a844ea13f9cc31187eb4c48ec7
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2022-05-13 10:36:41 +0430

    add container and flex components

--- chakra-ui/chakra-ui-playground/pages/index.tsx
+++ chakra-ui/chakra-ui-playground/pages/index.tsx
@@ -1,5 +1,27 @@
+import { Container, Flex, VStack } from '@chakra-ui/react';
+
 function IndexPage() {
-  return <h1>Hello nextjs</h1>;
+  return (
+    <Container maxW="container.xl" p={0}>
+      <Flex h="100vh" py={20}>
+        <VStack
+          w="full"
+          h="full"
+          p={10}
+          spacing={10}
+          alignItems="flex-start"
+        ></VStack>
+        <VStack
+          w="full"
+          h="full"
+          p={10}
+          spacing={10}
+          alignItems="flex-start"
+          bg="gray.50"
+        ></VStack>
+      </Flex>
+    </Container>
+  );
 }
 
 export default IndexPage;
