b5877a13fadea0b4660234032da0794401ccacbe
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2022-05-13 11:49:11 +0430

    implement details section

--- chakra-ui/chakra-ui-playground/src/sections/details.tsx
+++ chakra-ui/chakra-ui-playground/src/sections/details.tsx
@@ -1,14 +1,72 @@
-import { VStack } from '@chakra-ui/layout';
+import {
+  VStack,
+  Heading,
+  Text,
+  GridItem,
+  SimpleGrid,
+  FormControl,
+  FormLabel,
+  Input,
+  Select,
+  Checkbox,
+  Button,
+} from '@chakra-ui/react';
 
 function Details() {
   return (
-    <VStack
-      w="full"
-      h="full"
-      p={10}
-      spacing={10}
-      alignItems="flex-start"
-    ></VStack>
+    <VStack w="full" h="full" p={10} spacing={10} alignItems="flex-start">
+      <VStack spacing={3} alignItems="flex-start">
+        <Heading size="2xl">Your details</Heading>
+        <Text>If you already have an account, click here to log in.</Text>
+      </VStack>
+
+      <SimpleGrid column={2} columnGap={3} rowGap={6} w="full">
+        <GridItem colStart={1} colEnd={2}>
+          <FormControl>
+            <FormLabel>First Name</FormLabel>
+            <Input placeholder="john" />
+          </FormControl>
+        </GridItem>
+
+        <GridItem colStart={2} colEnd={3}>
+          <FormControl>
+            <FormLabel>Last Name</FormLabel>
+            <Input placeholder="Doe" />
+          </FormControl>
+        </GridItem>
+        <GridItem colSpan={2}>
+          <FormControl>
+            <FormLabel>Address</FormLabel>
+            <Input placeholder="lorem" />
+          </FormControl>
+        </GridItem>
+
+        <GridItem colStart={1} colEnd={2}>
+          <FormControl>
+            <FormLabel>City</FormLabel>
+            <Input placeholder="Tehran" />
+          </FormControl>
+        </GridItem>
+
+        <GridItem colStart={2} colEnd={3}>
+          <FormControl>
+            <FormLabel>Country</FormLabel>
+            <Select>
+              <option value="usa">United States of America</option>
+              <option value="uae">United Arab Emirates</option>
+              <option value="nmk">North Macedonia</option>
+              <option value="de">Germany</option>
+            </Select>
+          </FormControl>
+        </GridItem>
+        <GridItem colSpan={2}>
+          <Checkbox defaultChecked>Ship to billing address.</Checkbox>
+        </GridItem>
+        <GridItem colSpan={2}>
+          <Button size="lg" w="full">Place order</Button>
+        </GridItem>
+      </SimpleGrid>
+    </VStack>
   );
 }
 
