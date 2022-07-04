93ae51641762d8b0a2bd013286a4ebd0a50f0243
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2022-07-05 00:08:50 +0430

    feat: implement test function

--- jest/kent-c-javascript-testing/simple.js
+++ jest/kent-c-javascript-testing/simple.js
@@ -1,8 +1,20 @@
 import { sum, subtract } from './math.js';
 
-expect(sum(3, 7)).toBe(10);
-
 expect(subtract(7, 3)).toBe(4);
+
+test('sum adds numbers', () => expect(sum(3, 7)).toBe(10));
+
+test('subtract subtract numbers', () => expect(subtract(7, 4)).toBe(3));
+
+function test(title, callback) {
+  try {
+    callback();
+    console.log(`✅ ${title}`);
+  } catch (error) {
+    console.log(`❌ ${title}`);
+    console.error(error);
+  }
+}
 
 function expect(actual) {
   return {
