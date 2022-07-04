fc4507c6486a73b85a257ece62e2657c8815fa0d
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2022-07-05 00:16:04 +0430

    feat: improve test function to support async callbacks

--- jest/kent-c-javascript-testing/math.js
+++ jest/kent-c-javascript-testing/math.js
@@ -1,4 +1,7 @@
 const sum = (a, b) => a + b;
 const subtract = (a, b) => a - b;
 
-export { sum, subtract };
+const asyncSum = (a, b) => Promise.resolve(a - b);
+const asyncSubtract = (a, b) => Promise.resolve(a - b);
+
+export { sum, subtract, asyncSum, asyncSubtract };
--- jest/kent-c-javascript-testing/simple.js
+++ jest/kent-c-javascript-testing/simple.js
@@ -1,4 +1,4 @@
-import { sum, subtract } from './math.js';
+import { sum, subtract, asyncSum, asyncSubtract } from './math.js';
 
 expect(subtract(7, 3)).toBe(4);
 
@@ -6,9 +6,21 @@ test('sum adds numbers', () => expect(sum(3, 7)).toBe(10));
 
 test('subtract subtract numbers', () => expect(subtract(7, 4)).toBe(3));
 
-function test(title, callback) {
+test('sumAsync adds two number asynchronously', async () => {
+  const result = await asyncSum(3, 7);
+  const expected = 10;
+  expect(result).toBe(expected);
+});
+
+test('sumSubtract subtracts two number asynchronously', async () => {
+  const result = await asyncSubtract(7, 4);
+  const expected = 3;
+  expect(result).toBe(expected);
+});
+
+async function test(title, callback) {
   try {
-    callback();
+    await callback();
     console.log(`✅ ${title}`);
   } catch (error) {
     console.log(`❌ ${title}`);
