f8a9646e5a6cdcb729ce2dc5fa4463203a590395
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2022-07-31 17:03:36 +0430

    feat: complete implemantation of task component

--- storybook/taskbox/src/components/task/task.js
+++ storybook/taskbox/src/components/task/task.js
@@ -1,13 +1,59 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-function Task({ task: { id, title, state } }) {
+function Task({ task: { id, title, state }, onArchiveTask, onPinTask }) {
   return (
-    <div className="list-item">
-      <label htmlFor="title">
-        <input type="text" name="title" value={title} readOnly />
+    <div className={`list-item ${state}`}>
+      <label
+        htmlFor="checked"
+        aria-label={`archiveTask-${id}`}
+        className="checkbox"
+      >
+        <input
+          type="checkbox"
+          disabled={true}
+          name="checked"
+          id={`archiveTask-${id}`}
+          checked={state === 'TASK_ARCHIVED'}
+        />
+        <span className="checkbox-custom" onClick={() => onArchiveTask(id)} />
       </label>
+
+      <label htmlFor="title" aria-label={title} className="title">
+        <input
+          type="text"
+          value={title}
+          readOnly={true}
+          name="title"
+          placeholder="Input title"
+        />
+      </label>
+
+      {state !== 'TASK_ARCHIVED' && (
+        <button
+          className="pin-button"
+          onClick={() => onPinTask(id)}
+          id={`pinTask-${id}`}
+          aria-label={`pinTask-${id}`}
+          key={`pinTask-${id}`}
+        >
+          <span className={`icon-star`} />
+        </button>
+      )}
     </div>
   );
 }
+
+Task.propTypes = {
+  task: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    state: PropTypes.string.isRequired,
+  }),
+  onArchiveTask: PropTypes.func,
+  onPinTask: PropTypes.func,
+};
+
+Task.defaultProps = {};
 
 export default Task;
