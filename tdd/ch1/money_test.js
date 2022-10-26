f7967cae3f61243e6826a4932805e2794b16402e
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2022-10-26 10:47:40 +0330

    feat: second green test

--- tdd/ch1/money_test.js
+++ tdd/ch1/money_test.js
@@ -1,16 +1,24 @@
 const assert = require('assert');
 
-class Dollar {
-  constructor(amount) {
+class Money {
+  constructor(amount, currency) {
     this.amount = amount;
+    this.currency = currency;
   }
 
   times(multiplier) {
-    return new Dollar(this.amount * multiplier);
+    return new Money(this.amount * multiplier, this.currency);
   }
 }
 
-const fiver = new Dollar(5);
+const fiver = new Money(5, 'USD');
 const tenner = fiver.times(2);
 
 assert.strictEqual(tenner.amount, 10);
+assert.strictEqual(tenner.currency, 'USD');
+
+const tenEuros = new Money(10, 'EUR');
+const twentyEuros = tenEuros.times(2);
+
+assert.strictEqual(twentyEuros.amount, 20);
+assert.strictEqual(twentyEuros.currency, 'EUR');
