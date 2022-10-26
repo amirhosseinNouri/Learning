62edccbc7303fb43585a2035e6e7f88493812efc
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2022-10-26 11:33:38 +0330

    use for range with arrays and maps

---
+++ go/learning-go/ch4/for-range.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+func main() {
+	evenValues := []int{2, 4, 6, 8, 10, 12}
+
+	for i, v := range evenValues {
+		fmt.Println(i, v)
+	}
+
+	for _, v := range evenValues {
+		fmt.Println(v)
+	}
+
+	uniqueNames := map[string]bool{"fred": true, "Raul": true, "billy": true}
+
+	for k := range uniqueNames {
+		fmt.Println(k)
+	}
+
+	m := map[string]int{
+		"a": 1,
+		"c": 3,
+		"b": 2,
+	}
+
+	for i := 0; i < 3; i++ {
+		fmt.Println("Loop " , i);
+
+		for k, v := range m {
+			fmt.Println(k, v)
+		}
+	}
+
+}
