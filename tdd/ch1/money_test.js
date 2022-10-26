04304eba06ec732cd5c0c0e6eab90786689ddffc
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2022-10-26 10:57:56 +0330

    feat: implement divison and multiplication features

--- tdd/ch1/money_test.js
+++ tdd/ch1/money_test.js
@@ -9,16 +9,24 @@ class Money {
   times(multiplier) {
     return new Money(this.amount * multiplier, this.currency);
   }
+
+  divide(divisor) {
+    return new Money(this.amount / divisor, this.currency);
+  }
 }
 
-const fiver = new Money(5, 'USD');
-const tenner = fiver.times(2);
+const fiveDollars = new Money(5, 'USD');
+const tenDollars = new Money(10, 'USD');
 
-assert.strictEqual(tenner.amount, 10);
-assert.strictEqual(tenner.currency, 'USD');
+assert.deepStrictEqual(fiveDollars.times(2), tenDollars);
 
 const tenEuros = new Money(10, 'EUR');
-const twentyEuros = tenEuros.times(2);
+const twentyEuros = new Money(20, 'EUR');
 
-assert.strictEqual(twentyEuros.amount, 20);
-assert.strictEqual(twentyEuros.currency, 'EUR');
+assert.deepStrictEqual(tenEuros.times(2), twentyEuros);
+
+const originalMoney = new Money(4002, 'KRW');
+const actualMoneyAfterDivision = originalMoney.divide(4);
+const expectedMoneyAfterDivision = new Money(1000.5, 'KRW');
+
+assert.deepStrictEqual(actualMoneyAfterDivision, expectedMoneyAfterDivision);
