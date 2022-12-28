d162263d2be64116709ffbf5f77fddd175f17cc6
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2022-12-28 21:53:51 +0330

    for range and simple for

--- go/learning-go/ch4/main.go
+++ go/learning-go/ch4/main.go
@@ -1,17 +1,32 @@
 package main
 
-import (
-	"fmt"
-	"math/rand"
-)
+import "fmt"
 
 func main() {
 
-	if n := rand.Intn(10); n == 0 {
-		fmt.Println("zero")
-	} else if n > 5 {
-		fmt.Println("too big")
-	} else {
-		fmt.Println("Good one")
+	// if n := rand.Intn(10); n == 0 {
+	// 	fmt.Println("zero")
+	// } else if n > 5 {
+	// 	fmt.Println("too big")
+	// } else {
+	// 	fmt.Println("Good one")
+	// }
+
+	evenValues := []int{2, 4, 6, 8, 10}
+
+	for i, v := range evenValues {
+		if i == 0 {
+			continue
+		}
+
+		if i == len(evenValues)-2 {
+			break
+		}
+
+		fmt.Println(i, v)
+	}
+
+	for i := 1; i < len(evenValues)-1; i++ {
+		fmt.Println(i, evenValues[i])
 	}
 }
