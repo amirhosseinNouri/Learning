098f06b9eac99dc3ef9104ad187f73018b007ebe
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2023-01-12 00:53:00 +0330

    feat: implement simple routing

--- node/nodejs-express-mongodb/node-farm/index.js
+++ node/nodejs-express-mongodb/node-farm/index.js
@@ -1,9 +1,22 @@
 const http = require('http');
+const url = require('url');
 
 const PORT = 8000;
 
 const server = http.createServer((req, res) => {
-  res.end('Hello from the server!');
+  const { url: pathname } = req;
+
+  if (pathname === '/' || pathname === '/overview') {
+    res.end('This is the overview');
+  } else if (pathname === '/product') {
+    res.end('product');
+  } else {
+    res.writeHead(404, {
+      'Content-type': 'text/html',
+      'my-own-header': 'test',
+    });
+    res.end('<h1>Page not found!</h1>');
+  }
 });
 
 server.listen(PORT, '127.0.0.1', () =>
