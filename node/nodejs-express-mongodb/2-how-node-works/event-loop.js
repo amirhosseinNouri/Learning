0e2ad3f6a95e6abcb875dd0716734bd6f8745abf
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2023-02-05 00:06:01 +0330

    change thread pool size

--- node/nodejs-express-mongodb/2-how-node-works/event-loop.js
+++ node/nodejs-express-mongodb/2-how-node-works/event-loop.js
@@ -1,4 +1,9 @@
 const fs = require('fs');
+const crypto = require('crypto');
+
+const start = Date.now();
+
+process.env.UV_THREADPOOL_SIZE = 2;
 
 setTimeout(() => console.log('Timer 1 finished'), 0);
 setImmediate(() => console.log('Immediate 1 finished'));
@@ -11,6 +16,22 @@ fs.readFile('test-file.txt', 'utf-8', () => {
   setImmediate(() => console.log('Immediate 2 finished'));
 
   process.nextTick(() => console.log('Process.nextTick'));
+
+  crypto.pbkdf2('password', 'salt', 100000, 1024, 'sha512', () => {
+    console.log(Date.now() - start, ' password encrypted');
+  });
+
+  crypto.pbkdf2('password', 'salt', 100000, 1024, 'sha512', () => {
+    console.log(Date.now() - start, ' password encrypted');
+  });
+
+  crypto.pbkdf2('password', 'salt', 100000, 1024, 'sha512', () => {
+    console.log(Date.now() - start, ' password encrypted');
+  });
+
+  crypto.pbkdf2('password', 'salt', 100000, 1024, 'sha512', () => {
+    console.log(Date.now() - start, ' password encrypted');
+  });
 });
 
 console.log('Top level code');
