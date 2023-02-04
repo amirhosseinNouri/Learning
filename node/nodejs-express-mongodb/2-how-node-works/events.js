ce9519b274c616aea0e318afaa0ecc5696575a1c
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2023-02-05 00:15:33 +0330

    use inherited class instead of event emitter directly

--- node/nodejs-express-mongodb/2-how-node-works/events.js
+++ node/nodejs-express-mongodb/2-how-node-works/events.js
@@ -1,14 +1,23 @@
-const EventEmitter = require('events')
+const EventEmitter = require('events');
 
-const emitter = new EventEmitter();
+class Sales extends EventEmitter {
+  constructor() {
+    super();
+  }
+}
 
+const emitter = new Sales();
 
-emitter.on("newSale" , () => {
-    console.log('There was a new sale')
-})
+emitter.on('newSale', () => {
+  console.log('There was a new sale');
+});
 
-emitter.on("newSale" , () => {
-    console.log('Customer name: Amirhossein')
-})
+emitter.on('newSale', () => {
+  console.log('Customer name: Amirhossein');
+});
 
-emitter.emit("newSale");
+emitter.on('newSale', (stock) =>
+  console.log(`There are nor ${stock} items left in stock.`),
+);
+
+emitter.emit('newSale', 9);
