72a72c8614a07a6a63550a7e8a66bb939c615214
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2023-03-10 01:02:51 +0330

    define createNewDeck function

---
+++ go/go-complete-developer-guide-2021/cards/deck.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+type deck []string
+
+func (d deck) print() {
+	for i, card := range d {
+		fmt.Println(i, card)
+	}
+}
+
+func createNewDeck() deck {
+	cards := deck{}
+
+	cardSuits := []string{"Spaces", "Hearts", "Diamonds", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+
+	return cards
+}
--- go/go-complete-developer-guide-2021/cards/main.go
+++ go/go-complete-developer-guide-2021/cards/main.go
@@ -1,17 +1,8 @@
 package main
 
-import "fmt"
-
 func main() {
-	cards := []string{"Ace of Diamonds", createNewCard()}
-	cards = append(cards, "Six of Spades")
+	cards := createNewDeck()
 
-	for i, card := range cards {
-		fmt.Println(i, card)
-	}
+	cards.print()
 
-}
-
-func createNewCard() string {
-	return "Five of Diamonds"
 }
