8da8c718a84df0545c9bcd0ff17be2ffa68099d3
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2023-03-13 00:25:40 +0330

    add channel to wait for child routines

--- go/go-complete-developer-guide-2021/channels/main.go
+++ go/go-complete-developer-guide-2021/channels/main.go
@@ -10,21 +10,27 @@ func main() {
 		"http://goolge.com",
 		"http://stackoverflow.com",
 		"http://github.com",
-		"http://golang.org",
-		"http://time.ir",
 	}
 
+	c := make(chan string)
+
 	for _, url := range urls {
-		ping(url)
+		go ping(url, c)
+	}
+
+	for i := 0; i < len(urls); i++ {
+		fmt.Println(<-c)
 	}
 }
 
-func ping(url string) {
+func ping(url string, c chan string) {
 	_, err := http.Get(url)
 
 	if err != nil {
 		fmt.Printf("❌️ %v might be down!\n", url)
+		c <- "Might be down"
 		return
 	}
 	fmt.Printf("✅ %v is up!\n", url)
+	c <- "It is up"
 }
