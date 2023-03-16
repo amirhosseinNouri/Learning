962bd8e70913137d988bbc3ece3c83b647e2c834
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2023-03-16 20:13:29 +0330

    implement dup 2 to handle files as well

---
+++ go/the-go-programming-language/chapter-1-tutorial/dup-2.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	counts := make(map[string]int)
+	files := os.Args[1:]
+
+	if len(files) == 0 {
+		countLines(os.Stdin, counts)
+	} else {
+		for _, arg := range files {
+			f, err := os.Open(arg)
+
+			if err != nil {
+				_, _ = fmt.Fprintf(os.Stderr, "dup-2: %v\n", err)
+				continue
+			}
+
+			countLines(f, counts)
+			_ = f.Close()
+		}
+	}
+
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%d\t%s\n", n, line)
+		}
+	}
+
+}
+
+func countLines(f *os.File, counts map[string]int) {
+	input := bufio.NewScanner(f)
+
+	for input.Scan() {
+		counts[input.Text()]++
+	}
+}
