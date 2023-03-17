f362c93caddedfa25cde17f0f30d4ef415e0f8f6
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2023-03-17 22:47:21 +0330

    use published tempconv package

---
+++ go/the-go-programming-language/use-tempconv/go.mod
@@ -0,0 +1,5 @@
+module use-tempconv
+
+go 1.19
+
+require github.com/amirhosseinnouri/tempconv v0.0.4 // indirect
---
+++ go/the-go-programming-language/use-tempconv/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"github.com/amirhosseinnouri/tempconv"
+)
+
+func main() {
+
+	fmt.Println(tempconv.KtoC(-273))
+}
