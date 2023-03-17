9f386d9cc0fd044981dbb824adb895ebf4783c52
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2023-03-17 21:49:11 +0330

    implement tempconv package

---
+++ go/the-go-programming-language/tempconv/conv.go
@@ -0,0 +1,10 @@
+// Package tempconv performs Celsius and Fahrenheit conversions
+package tempconv
+
+func CtoF(c Celsius) Fahrenheit {
+	return Fahrenheit(c*9/5 + 32)
+}
+
+func FtoC(f Fahrenheit) Celsius {
+	return Celsius((f - 32) * 5 / 9)
+}
