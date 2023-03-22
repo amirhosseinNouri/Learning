155aedaba8cac2c3c66d755298b476a96a395119
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2023-03-22 18:53:38 +0330

    reverse a slice

--- go/the-go-programming-language/chapter-4-composite-types/slices.go
+++ go/the-go-programming-language/chapter-4-composite-types/slices.go
@@ -28,4 +28,15 @@ func main() {
 	endlessSummer := summer[:5]
 	fmt.Println(endlessSummer)
 
+	s := []int{1, 2, 3, 4, 5}
+	fmt.Println(s)
+	reverse(s)
+	fmt.Println(s)
+
+}
+
+func reverse(s []int) {
+	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
+		s[i], s[j] = s[j], s[i]
+	}
 }
