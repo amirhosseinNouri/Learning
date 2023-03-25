e9c15cffa58b927dd3096897dcef43e04651d74e
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2023-03-25 23:35:00 +0330

    implement a function to get github issues by passing terms as command line arguments

---
+++ go/the-go-programming-language/chapter-4-composite-types/github.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+	"strings"
+	"time"
+)
+
+const IssuesURL = "https://api.github.com/search/issues"
+
+type IssuesSearchResult struct {
+	TotalCount int `json:"total_count"`
+	Items      []*Issue
+}
+
+type Issue struct {
+	Number    int
+	HTMLURL   string `json:"html_url"`
+	Title     string
+	State     string
+	User      *User
+	CreatedAt time.Time `json:"created_at"`
+	Body      string
+}
+
+type User struct {
+	Login   string
+	HTMLURL string `json:"html_url"`
+}
+
+func main() {
+	result, err := SearchIssues(os.Args[1:])
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("%d issues:\n", result.TotalCount)
+
+	for _, item := range result.Items {
+		fmt.Printf("#%-5d\t%9.9s\t%.55s\n",
+			item.Number, item.User.Login, item.Title)
+	}
+}
+
+func SearchIssues(terms []string) (*IssuesSearchResult, error) {
+	q := url.QueryEscape(strings.Join(terms, " "))
+
+	resp, err := http.Get(IssuesURL + "?q=" + q)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		_ = resp.Body.Close()
+		return nil, fmt.Errorf("search query failed: %s", err)
+	}
+
+	var result IssuesSearchResult
+
+	if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
+		_ = resp.Body.Close()
+		return nil, err
+	}
+
+	_ = resp.Body.Close()
+	return &result, nil
+}
