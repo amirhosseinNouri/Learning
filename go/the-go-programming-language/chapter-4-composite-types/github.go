34d11ef539c825c24e73243f50cf663c43f2d3fe
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2023-03-26 17:32:05 +0330

    generate html as output of issues report

--- go/the-go-programming-language/chapter-4-composite-types/github.go
+++ go/the-go-programming-language/chapter-4-composite-types/github.go
@@ -41,8 +41,8 @@ func main() {
 	}
 
 	//printResult(result)
-	generateTemplate(result)
-
+	//generateTemplate(result)
+	generateHTML(result)
 }
 
 func printResult(result *IssuesSearchResult) {
@@ -67,6 +67,39 @@ func generateTemplate(result *IssuesSearchResult) {
 	var report = template.Must(template.New("issueList").Funcs(template.FuncMap{"daysAgo": daysAgo}).Parse(templ))
 
 	if err := report.Execute(os.Stdout, result); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func generateHTML(result *IssuesSearchResult) {
+	var issueList = template.Must(template.New("issueList").Parse(`
+	<h1>{{.TotalCount}}</h1>
+	<table>
+		<tr style='text-align: left'>
+			<th>#</th>
+			<th>State</th>
+			<th>User</th>
+			<th>Title</th>
+		</tr>
+
+		{{range .Items}}
+			<tr>
+				<td>
+					<a href='{{.HTMLURL}}'>{{.Number}}</a>
+				</td>
+				<td>{{.State}}</td>
+				<td>
+					<a href='{{.User.HTMLURL}}'>{{.User.Login}}</a>
+				</td>
+				<td>
+					<a href='{{.HTMLURL}}'>{{.Title}}</a>
+				</td>
+			</tr>
+		{{end}}
+	</table>
+`))
+
+	if err := issueList.Execute(os.Stdout, result); err != nil {
 		log.Fatal(err)
 	}
 }
