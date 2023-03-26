cacc8f2bbb74e2a76e03945a81c6d77efb654766
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2023-03-26 17:19:03 +0330

    parse and generate a template

--- go/the-go-programming-language/chapter-4-composite-types/github.go
+++ go/the-go-programming-language/chapter-4-composite-types/github.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 	"net/url"
@@ -39,12 +40,39 @@ func main() {
 		log.Fatal(err)
 	}
 
+	//printResult(result)
+	generateTemplate(result)
+
+}
+
+func printResult(result *IssuesSearchResult) {
 	fmt.Printf("%d issues:\n", result.TotalCount)
 
 	for _, item := range result.Items {
 		fmt.Printf("#%-5d\t%9.9s\t%.55s\n",
 			item.Number, item.User.Login, item.Title)
 	}
+}
+
+func generateTemplate(result *IssuesSearchResult) {
+	const templ = `{{.TotalCount}} issues
+	{{range .Items}}---------------
+	Number: {{.Number}}
+	User: {{.User.Login}}
+	Title: {{.Title | printf "%.64s" }}
+	Age: {{.CreatedAt | daysAgo}} days
+	{{end}}
+	`
+
+	var report = template.Must(template.New("issueList").Funcs(template.FuncMap{"daysAgo": daysAgo}).Parse(templ))
+
+	if err := report.Execute(os.Stdout, result); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func daysAgo(t time.Time) int {
+	return int(time.Since(t).Hours() / 24)
 }
 
 func SearchIssues(terms []string) (*IssuesSearchResult, error) {
