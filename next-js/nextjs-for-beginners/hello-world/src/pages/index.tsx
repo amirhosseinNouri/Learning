5f914044541f2f27b7ad2487dd2344f520357b9f
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2023-04-27 21:01:47 +0330

    navigate to static routes

--- next-js/nextjs-for-beginners/hello-world/src/pages/index.tsx
+++ next-js/nextjs-for-beginners/hello-world/src/pages/index.tsx
@@ -1,3 +1,11 @@
-const Home = () => <h1>Home Page</h1>;
+import Link from 'next/link';
 
-export default Home; 
+const Home = () => (
+  <div>
+    <h1>Home Page</h1>
+    <Link href="/blog">Blog</Link>
+    <Link href="/product">Products</Link>
+  </div>
+);
+
+export default Home;
--- next-js/nextjs-for-beginners/hello-world/src/pages/product/index.tsx
+++ next-js/nextjs-for-beginners/hello-world/src/pages/product/index.tsx
@@ -1,5 +1,8 @@
+import Link from 'next/link';
+
 const ProductList = () => (
   <div>
+    <Link href="/">Home</Link>
     <h2>Product 1</h2>
     <h2>Product 2</h2>
     <h2>Product 3</h2>
