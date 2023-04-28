bb5ae6d8e4fa8b1287d1064addf3b809c6da3773
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2023-04-28 10:57:27 +0330

    navigating programmatically

--- next-js/nextjs-for-beginners/hello-world/src/pages/index.tsx
+++ next-js/nextjs-for-beginners/hello-world/src/pages/index.tsx
@@ -1,11 +1,21 @@
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 
-const Home = () => (
-  <div>
-    <h1>Home Page</h1>
-    <Link href="/blog">Blog</Link>
-    <Link href="/product">Products</Link>
-  </div>
-);
+const Home = () => {
+  const router = useRouter();
+
+  const handleClick = () => {
+    router.push('/product');
+  };
+
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <Link href="/blog">Blog</Link>
+      <Link href="/product">Products</Link>
+      <button onClick={handleClick}>Place order</button>
+    </div>
+  );
+};
 
 export default Home;
