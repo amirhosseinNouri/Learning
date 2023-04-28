b003891c7438501bd80243c1c1157b1c7d20bf69
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2023-04-28 10:54:31 +0330

    client side routing

--- next-js/nextjs-for-beginners/hello-world/src/pages/product/index.tsx
+++ next-js/nextjs-for-beginners/hello-world/src/pages/product/index.tsx
@@ -1,11 +1,22 @@
 import Link from 'next/link';
 
-const ProductList = () => (
+const ProductList = ({ productId = 100 }) => (
   <div>
     <Link href="/">Home</Link>
-    <h2>Product 1</h2>
-    <h2>Product 2</h2>
-    <h2>Product 3</h2>
+    <h2>
+      <Link href="/product/1">Product 1</Link>
+    </h2>
+    <h2>
+      <Link href="/product/2">Product 2</Link>
+    </h2>
+    <h2>
+      <Link href="/product/3" replace>
+        Product 3
+      </Link>
+    </h2>
+    <h2>
+      <Link href={`product/${productId}`}>Product {productId}</Link>
+    </h2>
   </div>
 );
 
