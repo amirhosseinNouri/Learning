e6c328b13ec5503506aa390685c0e8dcc09e8352
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2023-05-13 20:22:01 +0330

    global styles

---
+++ next-js/nextjs-for-beginners/next-styling/src/pages/about.tsx
@@ -0,0 +1,5 @@
+const About = () => {
+  return <button className="btn btn-primary">Primary</button>;
+};
+
+export default About;
---
+++ next-js/nextjs-for-beginners/next-styling/src/pages/contact.tsx
@@ -0,0 +1,5 @@
+const Contact = () => {
+  return <h2>Contact page</h2>;
+};
+
+export default Contact;
