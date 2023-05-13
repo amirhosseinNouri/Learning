c454fb1a9ebce151ebce1428234026bba4aa959c
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2023-05-13 21:09:36 +0330

    implement per page layout

--- next-js/nextjs-for-beginners/next-misc/src/pages/_app.tsx
+++ next-js/nextjs-for-beginners/next-misc/src/pages/_app.tsx
@@ -2,8 +2,22 @@ import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import Header from '@/components/header';
 import Footer from '@/components/footer';
+import type { NextPage } from 'next';
+import type { ReactNode, ReactElement } from 'react';
 
-export default function App({ Component, pageProps }: AppProps) {
+export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+  getLayout?: (page: ReactElement) => ReactNode;
+};
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+};
+
+export default function App({ Component, pageProps }: AppPropsWithLayout) {
+  if (Component.getLayout) {
+    return Component.getLayout(<Component {...pageProps} />);
+  }
+
   return (
     <>
       <Header />
--- next-js/nextjs-for-beginners/next-misc/src/pages/about.tsx
+++ next-js/nextjs-for-beginners/next-misc/src/pages/about.tsx
@@ -1,7 +1,18 @@
+import type { ReactElement } from 'react';
 import s from '@/styles/about.module.css';
+import Footer from '@/components/footer';
 
 const About = () => {
   return <div className={s.content}>About page</div>;
 };
 
 export default About;
+
+About.getLayout = (page: ReactElement) => {
+  return (
+    <>
+      {page}
+      <Footer />
+    </>
+  );
+};
