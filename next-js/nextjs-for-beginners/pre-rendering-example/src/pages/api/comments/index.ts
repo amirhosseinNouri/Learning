264f25e1b9edf479f9b21d6e56a0b0563ebe7a60
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2023-05-13 09:29:53 +0330

    handle post request

--- next-js/nextjs-for-beginners/pre-rendering-example/src/pages/api/comments/index.ts
+++ next-js/nextjs-for-beginners/pre-rendering-example/src/pages/api/comments/index.ts
@@ -1,8 +1,23 @@
 import type { NextApiHandler } from 'next';
 import { comments } from '@/data/comments';
+import { Comment } from '@/types';
 
 const handler: NextApiHandler = (req, res) => {
-  res.status(200).json(comments);
+  if (req.method === 'GET') {
+    res.status(200).json(comments);
+    return;
+  }
+
+  if (req.method === 'POST') {
+    const comment = req.body;
+    console.log({ comment });
+    const newComment = {
+      id: Date.now(),
+      text: comment,
+    };
+    comments.push(newComment);
+    res.status(201).json(newComment);
+  }
 };
 
 export default handler;
--- next-js/nextjs-for-beginners/pre-rendering-example/src/pages/comments/index.tsx
+++ next-js/nextjs-for-beginners/pre-rendering-example/src/pages/comments/index.tsx
@@ -1,8 +1,9 @@
 import { Comment } from '@/types';
-import { useState } from 'react';
+import { ChangeEvent, useState } from 'react';
 
 const CommentList = () => {
   const [comments, setComments] = useState<Comment[]>([]);
+  const [comment, setComment] = useState('');
 
   const fetchComments = async () => {
     const response = await fetch('/api/comments');
@@ -10,8 +11,26 @@ const CommentList = () => {
     setComments(data);
   };
 
+  const handleCommentChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setComment(e.target.value);
+  };
+
+  const submitComment = async () => {
+    const response = await fetch('/api/comments', {
+      method: 'POST',
+      body: JSON.stringify(comment),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    const data = await response.json();
+    console.log(data);
+  };
+
   return (
     <>
+      <input type="text" value={comment} onChange={handleCommentChange} />
+      <button onClick={submitComment}>Submit comment</button>
       <button onClick={fetchComments}>Fetch comments</button>
       {comments.map((comment) => (
         <div key={comment.id}>{comment.text}</div>
