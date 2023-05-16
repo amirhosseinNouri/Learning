c8c1c02a0bf5a54fb8db62bdd97f49d4b5ee4671
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2023-05-16 21:40:41 +0330

    redirect

--- next-js/nextjs-for-beginners/next-misc/next.config.js
+++ next-js/nextjs-for-beginners/next-misc/next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  redirects: async () => {
+    return [{ source: '/about', destination: '/', permanent: true }];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
