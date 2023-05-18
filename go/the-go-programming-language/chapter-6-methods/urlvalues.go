3eb265ca40eb639f6616fe8e6288448f9c20a5ba
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2023-05-18 12:51:21 +0330

    nil struct value example

---
+++ go/the-go-programming-language/chapter-6-methods/urlvalues.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"net/url"
+)
+
+func main() {
+	m := url.Values{"lang": {"en"}}
+	m.Add("item", "1")
+	m.Add("item", "2")
+	fmt.Println(m.Get("lang"))
+	fmt.Println(m.Get("q"))
+	fmt.Println(m.Get("item"))
+	fmt.Println(m["item"]) // direct access
+	m = nil
+	fmt.Println(m.Get("item"))
+	m.Add("item", "3")
+}
