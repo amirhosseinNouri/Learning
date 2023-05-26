f575497a908180a83f7c13e825dc14b10e22678e
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2023-05-27 00:15:22 +0330

    implement Stringer interface

--- go/the-go-programming-language/chapter-7-interfaces/byte-counter.go
+++ go/the-go-programming-language/chapter-7-interfaces/byte-counter.go
@@ -9,6 +9,10 @@ func (c *ByteCounter) Write(p []byte) (int, error) {
 	return len(p), nil
 }
 
+func (c *ByteCounter) String() string {
+	return fmt.Sprintf("I have counted %d bytes", *c)
+}
+
 func main() {
 	var c ByteCounter
 	c.Write([]byte("hello"))
@@ -18,4 +22,5 @@ func main() {
 	var name = "Amirhossein"
 	fmt.Fprintf(&c, "hello %s", name)
 	fmt.Println(c)
+	fmt.Println(c.String())
 }
