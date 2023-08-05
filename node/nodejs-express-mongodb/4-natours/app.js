248cac36086e7ac791c7217cfd82ef7d63970529
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2023-08-05 19:19:28 +0330

    create a server module

--- node/nodejs-express-mongodb/4-natours/app.js
+++ node/nodejs-express-mongodb/4-natours/app.js
@@ -3,19 +3,20 @@ const morgan = require('morgan');
 const tourRouter = require('./routes/tour-route');
 const userRouter = require('./routes/user-route');
 
-const PORT = 3000;
-
 const app = express();
 
 /**
  * Middleware
  */
 app.use(morgan('dev'));
+
 app.use(express.json());
+
 app.use((req, res, next) => {
   console.log('Middleware ⛱️');
   next();
 });
+
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
@@ -27,4 +28,4 @@ app.use((req, res, next) => {
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
-app.listen(PORT, () => console.log(`Server is running on ${PORT}`));
+module.exports = app;
---
+++ node/nodejs-express-mongodb/4-natours/server.js
@@ -0,0 +1,5 @@
+const app = require('./app');
+
+const PORT = 3000;
+
+app.listen(PORT, () => console.log(`Server is running on ${PORT}`));
