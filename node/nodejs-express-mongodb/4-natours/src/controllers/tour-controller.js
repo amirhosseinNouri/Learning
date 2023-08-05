e6fad0039cb4b77f285c0bd8f9487f0266ac83fa
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2023-08-05 19:22:57 +0330

    refactor and restructure the project

--- node/nodejs-express-mongodb/4-natours/src/controllers/tour-controller.js
+++ node/nodejs-express-mongodb/4-natours/src/controllers/tour-controller.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 
 const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`),
+  fs.readFileSync(`${__dirname}/../mock/data/tours-simple.json`),
 );
 
 const getTour = (id) => tours.find((item) => item.id === Number(id));
@@ -38,7 +38,7 @@ const createTour = (req, res) => {
   const newTour = { ...req.body, id: newId };
   tours.push(newTour);
   fs.writeFile(
-    `${__dirname}/dev-data/data/tours-simple.json`,
+    `${__dirname}/mock/data/tours-simple.json`,
     JSON.stringify(tours),
     (err) => {
       res.status(201).json({
