0636ebaac337ba4ee50f3c33080188392ba81cf8
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2023-08-05 17:48:55 +0330

    add query paramter to get a single tour

--- node/nodejs-express-mongodb/4-natours/app.js
+++ node/nodejs-express-mongodb/4-natours/app.js
@@ -5,6 +5,8 @@ const PORT = 3000;
 
 const app = express();
 
+app.use(express.json());
+
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`),
 );
@@ -15,6 +17,40 @@ app.get('/api/v1/tours', (req, res) => {
     results: tours.length,
     data: { tours },
   });
+});
+
+app.get('/api/v1/tours/:id', (req, res) => {
+  const { id } = req.params;
+  const tour = tours.find((item) => item.id === Number(id));
+
+  if (tour) {
+    res.status(200).json({
+      ok: true,
+      data: { tour },
+    });
+    return;
+  }
+
+  res.status(404).json({
+    ok: false,
+    error: { message: 'Tour not found' },
+  });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  const newId = tours[tours.length - 1].id + 1 || Date.now();
+  const newTour = { ...req.body, id: newId };
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        ok: true,
+        data: { tour: newTour },
+      });
+    },
+  );
 });
 
 app.listen(PORT, () => console.log(`Server is running on ${PORT}`));
