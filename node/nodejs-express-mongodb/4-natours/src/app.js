15dc17c244c4d568ac35cc77c89e3e3fcc188ce2
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2023-08-11 19:47:06 +0330

    make morgan enabled on development enviroment

--- node/nodejs-express-mongodb/4-natours/src/app.js
+++ node/nodejs-express-mongodb/4-natours/src/app.js
@@ -8,16 +8,14 @@ const app = express();
 /**
  * Middleware
  */
-app.use(morgan('dev'));
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
 app.use(express.json());
 
 app.use(express.static(`${__dirname}/../public`));
-
-app.use((req, res, next) => {
-  req.requestTime = new Date().toISOString();
-  next();
-});
 
 /**
  * Mounting routers
