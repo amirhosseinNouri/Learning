53b94101be21889315f14c03bace0eb7eddb7fa1
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2023-08-11 11:00:12 +0330

    server static files using express

--- node/nodejs-express-mongodb/4-natours/src/app.js
+++ node/nodejs-express-mongodb/4-natours/src/app.js
@@ -12,6 +12,8 @@ app.use(morgan('dev'));
 
 app.use(express.json());
 
+app.use(express.static(`${__dirname}/../public`));
+
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
