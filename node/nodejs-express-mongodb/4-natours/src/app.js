e1e5f6328a106309f8787269f9e4d1bcb5a3529e
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2023-08-11 10:15:58 +0330

    comment dummy middleware

--- node/nodejs-express-mongodb/4-natours/src/app.js
+++ node/nodejs-express-mongodb/4-natours/src/app.js
@@ -13,11 +13,6 @@ app.use(morgan('dev'));
 app.use(express.json());
 
 app.use((req, res, next) => {
-  console.log('Middleware ⛱️');
-  next();
-});
-
-app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
