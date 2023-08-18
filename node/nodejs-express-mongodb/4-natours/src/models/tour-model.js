3d4c43464c1ad064ab3120862678ce2c97ce9232
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2023-08-18 09:23:47 +0330

    improve tour model

--- node/nodejs-express-mongodb/4-natours/src/models/tour-model.js
+++ node/nodejs-express-mongodb/4-natours/src/models/tour-model.js
@@ -6,14 +6,50 @@ const tourSchema = new mongoose.Schema({
     required: 'A tour must have a name',
     unique: true,
   },
-  rating: {
+  duration: {
+    type: Number,
+    required: 'Tour requires a duration',
+  },
+  maxGroupSize: {
+    type: Number,
+    required: 'Tour requires a group size',
+  },
+  difficulty: {
+    type: String,
+    required: 'Tour requires a difficulty level',
+  },
+  ratingsAverage: {
     type: Number,
     default: 4.5,
+  },
+  ratingsQuantity: {
+    type: Number,
+    default: 0,
   },
   price: {
     type: Number,
     required: 'A tour must have a price',
   },
+  discount: Number,
+  summary: {
+    type: String,
+    trim: true,
+  },
+  description: {
+    type: String,
+    trim: true,
+    required: 'Tour requires a description',
+  },
+  imageCover: {
+    type: String,
+    required: 'Tour requires an image cover',
+  },
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  startDates: [Date],
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
