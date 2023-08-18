5d8db47b378474c5c4debc5139d2591a8edff940
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2023-08-18 08:10:53 +0330

    create the first model using mongoose

--- node/nodejs-express-mongodb/4-natours/src/server.js
+++ node/nodejs-express-mongodb/4-natours/src/server.js
@@ -1,8 +1,43 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config({ path: `${__dirname}/../config.env` });
 
 const app = require('./app');
+
+const dbURI = process.env.DB_STRING_LOCAL.replace(
+  '<PASSWORD>',
+  process.env.DB_PASSWORD,
+);
+mongoose.connect(dbURI).then(() => console.log('db connected successfully'));
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: 'A tour must have a name',
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: 'A tour must have a price',
+  },
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+
+const testTour = new Tour({
+  name: 'The Park Camper',
+  price: 497,
+});
+
+testTour
+  .save()
+  .then((document) => console.log(document))
+  .catch((err) => console.log(err));
 
 const { PORT } = process.env;
 
