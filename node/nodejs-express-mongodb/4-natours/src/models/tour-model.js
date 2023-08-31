47f1f642899d1f746994c9691682de8911f6110a
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2023-08-31 17:34:53 +0330

    add document middleware

--- node/nodejs-express-mongodb/4-natours/src/models/tour-model.js
+++ node/nodejs-express-mongodb/4-natours/src/models/tour-model.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -7,6 +8,7 @@ const tourSchema = new mongoose.Schema(
       required: 'A tour must have a name',
       unique: true,
     },
+    slug: String,
     duration: {
       type: Number,
       required: 'Tour requires a duration',
@@ -62,6 +64,22 @@ const tourSchema = new mongoose.Schema(
 tourSchema.virtual('durationWeeks').get(function () {
   return this.duration / 7;
 });
+
+// Document middleware: runs before the save() command and .create()
+tourSchema.pre('save', function (next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+// tourSchema.pre('save', (next) => {
+//   console.log('Will save document...');
+//   next();
+// });
+
+// tourSchema.post('save', (doc, next) => {
+//   console.log(doc);
+//   next();
+// });
 
 const Tour = mongoose.model('Tour', tourSchema);
 
