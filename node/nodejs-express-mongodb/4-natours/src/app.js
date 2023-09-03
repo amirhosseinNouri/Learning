d1909755d8ee9334a4625edc9315591cb580111f
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2023-09-03 10:14:19 +0330

    implement error handling for unhandled routes

--- node/nodejs-express-mongodb/4-natours/src/app.js
+++ node/nodejs-express-mongodb/4-natours/src/app.js
@@ -22,5 +22,11 @@ app.use(express.static(`${__dirname}/../public`));
  */
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    ok: false,
+    error: { message: `Can not find ${req.originalUrl} on this server.` },
+  });
+});
 
 module.exports = app;
