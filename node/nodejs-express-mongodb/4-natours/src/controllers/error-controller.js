312470f16654beb1b5c13e4e47561b9847eaff42
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2023-09-04 10:15:11 +0330

    improve error controller to provide more info in dev mode

--- node/nodejs-express-mongodb/4-natours/src/controllers/error-controller.js
+++ node/nodejs-express-mongodb/4-natours/src/controllers/error-controller.js
@@ -1,10 +1,28 @@
 const DEFAULT_ERROR_STATUS_CODE = 500;
 
-module.exports = (err, req, res, next) => {
+const sendDevelopmentError = (err, res) => {
+  const { statusCode = DEFAULT_ERROR_STATUS_CODE } = err;
+
+  res.status(statusCode).json({
+    ok: false,
+    error: { message: err.message, stack: err.stack, error: err },
+  });
+};
+
+const sendProductionError = (err, res) => {
   const { statusCode = DEFAULT_ERROR_STATUS_CODE } = err;
 
   res.status(statusCode).json({
     ok: false,
     error: { message: err.message },
   });
+};
+
+module.exports = (err, req, res, next) => {
+  if (process.env.NODE_ENV === 'development') {
+    sendDevelopmentError(err, res);
+    return;
+  }
+
+  sendProductionError(err, res);
 };
