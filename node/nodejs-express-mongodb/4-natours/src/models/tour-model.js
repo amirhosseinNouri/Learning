19f6286057e95e7564d6c1cf82a4f0fcc98d0c0c
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2023-09-17 11:10:40 +0330

    implement locations as a embedded field inside the tour schema

--- node/nodejs-express-mongodb/4-natours/src/models/tour-model.js
+++ node/nodejs-express-mongodb/4-natours/src/models/tour-model.js
@@ -76,6 +76,30 @@ const tourSchema = new mongoose.Schema(
       type: Boolean,
       default: false,
     },
+    startLocation: {
+      // GeoJSON
+      type: {
+        type: String,
+        default: 'Point',
+        enum: ['Point'],
+      },
+      coordinates: [Number],
+      address: String,
+      description: String,
+    },
+    locations: [
+      {
+        type: {
+          type: String,
+          default: 'Point',
+          enum: ['Point'],
+        },
+        coordinates: [Number],
+        address: String,
+        description: String,
+        day: Number,
+      },
+    ],
   },
   {
     toJSON: { virtuals: true },
