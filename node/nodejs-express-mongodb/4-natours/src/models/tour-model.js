61ca43f1d5096c7862c7078d05d4e5ccde691588
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2023-09-18 10:59:09 +0330

    populate tour guide on tour queries

--- node/nodejs-express-mongodb/4-natours/src/models/tour-model.js
+++ node/nodejs-express-mongodb/4-natours/src/models/tour-model.js
@@ -138,6 +138,14 @@ tourSchema.pre(/^find/, function (next) {
   next();
 });
 
+tourSchema.pre(/^find/, function (next) {
+  this.populate({
+    path: 'guides',
+    select: '-__v -passwordChangedAt',
+  });
+  next();
+});
+
 tourSchema.post(/^find/, function (documents, next) {
   console.log(`Query took ${Date.now() - this.start}ms`);
   next();
