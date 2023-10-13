010c2564d11dde82b9a175717e13049c019eaff1
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2023-10-13 13:31:57 +0330

    feat: implement exc one

--- react-native/first-project/App.tsx
+++ react-native/first-project/App.tsx
@@ -4,23 +4,61 @@ import { Text, SafeAreaView, StyleSheet, View } from 'react-native';
 export default function App() {
   return (
     <SafeAreaView style={styles.safeArea}>
-      <View style={[styles.container, styles.background]}>
-        <Text>Hello world</Text>
+      <View style={[styles.container]}>
+        <Text style={styles.title}>
+          Here are some boxes with different colors
+        </Text>
+        <View style={[styles.colorContainer]}>
+          <View style={[styles.box, styles.cyan]}>
+            <Text>Cyan</Text>
+          </View>
+          <View style={[styles.box, styles.blue]}>
+            <Text>Blue</Text>
+          </View>
+          <View style={[styles.box, styles.magenta]}>
+            <Text>Magenta</Text>
+          </View>
+          <View style={[styles.box, styles.orange]}>
+            <Text>Orange</Text>
+          </View>
+        </View>
       </View>
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  background: {
-    backgroundColor: 'teal',
-  },
   safeArea: {
     flex: 1,
   },
   container: {
-    alignItems: 'center',
-    justifyContent: 'center',
     flex: 1,
+    paddingHorizontal: 10,
+    paddingVertical: 40,
+    gap: 10,
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  colorContainer: {
+    gap: 10,
+  },
+  box: {
+    padding: 10,
+    alignItems: 'center',
+    fontWeight: 'bold',
+  },
+  orange: {
+    backgroundColor: 'orange',
+  },
+  cyan: {
+    backgroundColor: 'cyan',
+  },
+  magenta: {
+    backgroundColor: 'magenta',
+  },
+  blue: {
+    backgroundColor: 'blue',
   },
 });
