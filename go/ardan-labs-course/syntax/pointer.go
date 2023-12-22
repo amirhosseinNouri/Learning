2598e7bb225f60104ef9738ee4bcca94fade5ce4
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2023-12-22 10:21:10 +0330

    pointer semantic

--- go/ardan-labs-course/syntax/pointer.go
+++ go/ardan-labs-course/syntax/pointer.go
@@ -6,13 +6,13 @@ func main() {
 	count := 10
 
 	fmt.Println("count:\tValue of [", count, "]\tAddress Of[", &count, "]")
-	increment(count)
+	increment(&count)
 	fmt.Println("count:\tValue of [", count, "]\tAddress Of[", &count, "]")
 
 }
 
-func increment(inc int) {
-	inc++
-	fmt.Println("count:\tValue of [", inc, "]\tAddress Of[", &inc, "]")
+func increment(inc *int) {
+	*inc++
+	fmt.Println("count:\tValue of [", *inc, "]\tAddress Of[", inc, "]")
 
 }
