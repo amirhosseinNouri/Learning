93656218db365eed12e75ac1749e9352bc5091e9
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2024-02-08 19:59:58 +0330

    the first solution using union on generic types

--- type-script/total-typescript/1-type-transformation/src/04-conditional-types-and-infer/28-distributive-conditional-types.problem.ts
+++ type-script/total-typescript/1-type-transformation/src/04-conditional-types-and-infer/28-distributive-conditional-types.problem.ts
@@ -1,9 +1,9 @@
-import { Equal, Expect } from "../helpers/type-utils";
+import { Equal, Expect } from '../helpers/type-utils';
 
-type Fruit = "apple" | "banana" | "orange";
+type Fruit = 'apple' | 'banana' | 'orange';
 
-type AppleOrBanana = Fruit extends "apple" | "banana"
-  ? "apple" | "banana"
-  : never;
+type GetAppleOrBanana<T> = T extends 'apple' | 'banana' ? T : never;
 
-type tests = [Expect<Equal<AppleOrBanana, "apple" | "banana">>];
+type AppleOrBanana = GetAppleOrBanana<Fruit>;
+
+type tests = [Expect<Equal<AppleOrBanana, 'apple' | 'banana'>>];
