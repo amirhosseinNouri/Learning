c5da08b94445eacabae46bf074920a9510df40e9
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2024-02-08 19:09:10 +0330

    infer with raw values

--- type-script/total-typescript/1-type-transformation/src/04-conditional-types-and-infer/23-infer-with-raw-values.problem.ts
+++ type-script/total-typescript/1-type-transformation/src/04-conditional-types-and-infer/23-infer-with-raw-values.problem.ts
@@ -1,14 +1,16 @@
-import { Equal, Expect } from "../helpers/type-utils";
+import { Equal, Expect } from '../helpers/type-utils';
 
-type GetDataValue<T> = unknown;
+type GetDataValue<T extends any> = T extends { data: infer D } ? D : never;
+
+type D = GetDataValue<{ data: 'hi' }>;
 
 type tests = [
-  Expect<Equal<GetDataValue<{ data: "hello" }>, "hello">>,
-  Expect<Equal<GetDataValue<{ data: { name: "hello" } }>, { name: "hello" }>>,
+  Expect<Equal<GetDataValue<{ data: 'hello' }>, 'hello'>>,
+  Expect<Equal<GetDataValue<{ data: { name: 'hello' } }>, { name: 'hello' }>>,
   Expect<
     Equal<
-      GetDataValue<{ data: { name: "hello"; age: 20 } }>,
-      { name: "hello"; age: 20 }
+      GetDataValue<{ data: { name: 'hello'; age: 20 } }>,
+      { name: 'hello'; age: 20 }
     >
   >,
   // Expect that if you pass in string, it
