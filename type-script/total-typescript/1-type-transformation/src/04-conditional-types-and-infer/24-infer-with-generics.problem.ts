8db208c413a25ece648f71b9c91aec7383374f56
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2024-02-08 19:14:31 +0330

    infer on generics

--- type-script/total-typescript/1-type-transformation/src/04-conditional-types-and-infer/24-infer-with-generics.problem.ts
+++ type-script/total-typescript/1-type-transformation/src/04-conditional-types-and-infer/24-infer-with-generics.problem.ts
@@ -1,4 +1,4 @@
-import { Equal, Expect } from "../helpers/type-utils";
+import { Equal, Expect } from '../helpers/type-utils';
 
 interface MyComplexInterface<Event, Context, Name, Point> {
   getEvent: () => Event;
@@ -8,12 +8,14 @@ interface MyComplexInterface<Event, Context, Name, Point> {
 }
 
 type Example = MyComplexInterface<
-  "click",
-  "window",
-  "my-event",
+  'click',
+  'window',
+  'my-event',
   { x: 12; y: 14 }
 >;
 
-type GetPoint<T> = unknown;
+type GetPoint<T> = T extends MyComplexInterface<any, any, any, infer P>
+  ? P
+  : never;
 
 type tests = [Expect<Equal<GetPoint<Example>, { x: 12; y: 14 }>>];
