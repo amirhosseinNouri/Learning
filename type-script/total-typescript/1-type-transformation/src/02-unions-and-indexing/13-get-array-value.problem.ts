5158c4c1259da2b91fa02450352e0f3ae7304b65
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2024-02-08 11:16:53 +0330

    get array value using indexed access on array

--- type-script/total-typescript/1-type-transformation/src/02-unions-and-indexing/13-get-array-value.problem.ts
+++ type-script/total-typescript/1-type-transformation/src/02-unions-and-indexing/13-get-array-value.problem.ts
@@ -1,11 +1,13 @@
-import { Equal, Expect } from "../helpers/type-utils";
+import { Equal, Expect } from '../helpers/type-utils';
 
-const fruits = ["apple", "banana", "orange"];
+const fruits = ['apple', 'banana', 'orange'] as const;
 
-type AppleOrBanana = unknown;
-type Fruit = unknown;
+type Fruits = typeof fruits;
+
+type AppleOrBanana = Fruits['0' | '1'];
+type Fruit = Fruits[number];
 
 type tests = [
-  Expect<Equal<AppleOrBanana, "apple" | "banana">>,
-  Expect<Equal<Fruit, "apple" | "banana" | "orange">>,
+  Expect<Equal<AppleOrBanana, 'apple' | 'banana'>>,
+  Expect<Equal<Fruit, 'apple' | 'banana' | 'orange'>>,
 ];
