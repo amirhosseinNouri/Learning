a0e78c94175a201c100bfdd515059df5f6974375
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2024-02-09 22:33:30 +0330

    discriminated union to object

--- type-script/total-typescript/1-type-transformation/src/05-key-remapping/33-discriminated-union-to-object.problem.ts
+++ type-script/total-typescript/1-type-transformation/src/05-key-remapping/33-discriminated-union-to-object.problem.ts
@@ -1,31 +1,33 @@
-import { Equal, Expect } from "../helpers/type-utils";
+import { Equal, Expect } from '../helpers/type-utils';
 
 type Route =
   | {
-      route: "/";
+      route: '/';
       search: {
         page: string;
         perPage: string;
       };
     }
-  | { route: "/about"; search: {} }
-  | { route: "/admin"; search: {} }
-  | { route: "/admin/users"; search: {} };
+  | { route: '/about'; search: {} }
+  | { route: '/admin'; search: {} }
+  | { route: '/admin/users'; search: {} };
 
-type RoutesObject = unknown;
+type RoutesObject = {
+  [K in Route['route']]: Extract<Route, { route: K }>['search'];
+};
 
 type tests = [
   Expect<
     Equal<
       RoutesObject,
       {
-        "/": {
+        '/': {
           page: string;
           perPage: string;
         };
-        "/about": {};
-        "/admin": {};
-        "/admin/users": {};
+        '/about': {};
+        '/admin': {};
+        '/admin/users': {};
       }
     >
   >,
