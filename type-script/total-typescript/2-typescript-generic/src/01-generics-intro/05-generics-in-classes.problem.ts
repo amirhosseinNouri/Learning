f2be34ee6ab2d6b0d90337c736976460d1ebe8b3
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2024-02-10 15:06:46 +0330

    generic class

--- type-script/total-typescript/2-typescript-generic/src/01-generics-intro/05-generics-in-classes.problem.ts
+++ type-script/total-typescript/2-typescript-generic/src/01-generics-intro/05-generics-in-classes.problem.ts
@@ -1,17 +1,17 @@
-import { expect, it } from "vitest";
-import { Equal, Expect } from "../helpers/type-utils";
+import { expect, it } from 'vitest';
+import { Equal, Expect } from '../helpers/type-utils';
 
-export class Component {
-  private props: unknown;
+export class Component<T> {
+  private props: T;
 
-  constructor(props: unknown) {
+  constructor(props: T) {
     this.props = props;
   }
 
   getProps = () => this.props;
 }
 
-it("Should create an object containing props", () => {
+it('Should create an object containing props', () => {
   const component = new Component({ a: 1, b: 2, c: 3 });
 
   const result = component.getProps();
