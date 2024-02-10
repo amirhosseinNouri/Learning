45422e9a4378ee8464b4cc50c80da0115bd51695
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2024-02-10 15:08:32 +0330

    generic mapper

--- type-script/total-typescript/2-typescript-generic/src/01-generics-intro/06-generic-mapper.problem.ts
+++ type-script/total-typescript/2-typescript-generic/src/01-generics-intro/06-generic-mapper.problem.ts
@@ -1,18 +1,27 @@
-import { expect, it } from "vitest";
-import { Equal, Expect } from "../helpers/type-utils";
+import { expect, it } from 'vitest';
+import { Equal, Expect } from '../helpers/type-utils';
 
-export const concatenateFirstNameAndLastName = (user: unknown) => {
+type WithFirstAndLastName = {
+  firstName: string;
+  lastName: string;
+};
+
+export const concatenateFirstNameAndLastName = <
+  TUser extends WithFirstAndLastName,
+>(
+  user: TUser,
+) => {
   return {
     ...user,
     fullName: `${user.firstName} ${user.lastName}`,
   };
 };
 
-it("Should add fullName to an object which only contains firstName and lastName", () => {
+it('Should add fullName to an object which only contains firstName and lastName', () => {
   const users = [
     {
-      firstName: "Matt",
-      lastName: "Pocock",
+      firstName: 'Matt',
+      lastName: 'Pocock',
     },
   ];
 
@@ -20,9 +29,9 @@ it("Should add fullName to an object which only contains firstName and lastName"
 
   expect(newUsers).toEqual([
     {
-      firstName: "Matt",
-      lastName: "Pocock",
-      fullName: "Matt Pocock",
+      firstName: 'Matt',
+      lastName: 'Pocock',
+      fullName: 'Matt Pocock',
     },
   ]);
 
@@ -36,12 +45,12 @@ it("Should add fullName to an object which only contains firstName and lastName"
   ];
 });
 
-it("Should retain other properties passed in", () => {
+it('Should retain other properties passed in', () => {
   const users = [
     {
       id: 1,
-      firstName: "Matt",
-      lastName: "Pocock",
+      firstName: 'Matt',
+      lastName: 'Pocock',
     },
   ];
 
@@ -50,9 +59,9 @@ it("Should retain other properties passed in", () => {
   expect(newUsers).toEqual([
     {
       id: 1,
-      firstName: "Matt",
-      lastName: "Pocock",
-      fullName: "Matt Pocock",
+      firstName: 'Matt',
+      lastName: 'Pocock',
+      fullName: 'Matt Pocock',
     },
   ]);
 
@@ -70,10 +79,10 @@ it("Should retain other properties passed in", () => {
   ];
 });
 
-it("Should fail when the object passed in does not contain firstName", () => {
+it('Should fail when the object passed in does not contain firstName', () => {
   const users = [
     {
-      firstName: "Matt",
+      firstName: 'Matt',
     },
   ];
 
