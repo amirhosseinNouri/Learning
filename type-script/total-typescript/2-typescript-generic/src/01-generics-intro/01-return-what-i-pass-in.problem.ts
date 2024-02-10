89840f130486d469ae924ba4d23b3218aa986ece
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2024-02-10 14:01:14 +0330

    return what i pass in

--- type-script/total-typescript/2-typescript-generic/src/01-generics-intro/01-return-what-i-pass-in.problem.ts
+++ type-script/total-typescript/2-typescript-generic/src/01-generics-intro/01-return-what-i-pass-in.problem.ts
@@ -1,10 +1,10 @@
-import { Equal, Expect } from "../helpers/type-utils";
+import { Equal, Expect } from '../helpers/type-utils';
 
-const returnWhatIPassIn = (t: unknown) => {
+const returnWhatIPassIn = <T>(t: T) => {
   return t;
 };
 
 const one = returnWhatIPassIn(1);
-const matt = returnWhatIPassIn("matt");
+const matt = returnWhatIPassIn('matt');
 
-type tests = [Expect<Equal<typeof one, 1>>, Expect<Equal<typeof matt, "matt">>];
+type tests = [Expect<Equal<typeof one, 1>>, Expect<Equal<typeof matt, 'matt'>>];
