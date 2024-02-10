457def019286e8c0dcb209f08ea855a36c9e19ef
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2024-02-10 14:53:23 +0330

    multiple generics

--- type-script/total-typescript/2-typescript-generic/src/01-generics-intro/03-multiple-generics.problem.ts
+++ type-script/total-typescript/2-typescript-generic/src/01-generics-intro/03-multiple-generics.problem.ts
@@ -1,18 +1,18 @@
-import { expect, it } from "vitest";
-import { Equal, Expect } from "../helpers/type-utils";
+import { expect, it } from 'vitest';
+import { Equal, Expect } from '../helpers/type-utils';
 
-const returnBothOfWhatIPassIn = (a: unknown, b: unknown) => {
+const returnBothOfWhatIPassIn = <T, U>(a: T, b: U) => {
   return {
     a,
     b,
   };
 };
 
-it("Should return an object of the arguments you pass", () => {
-  const result = returnBothOfWhatIPassIn("a", 1);
+it('Should return an object of the arguments you pass', () => {
+  const result = returnBothOfWhatIPassIn('a', 1);
 
   expect(result).toEqual({
-    a: "a",
+    a: 'a',
     b: 1,
   });
 
