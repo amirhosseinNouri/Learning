b607202a5c91f9c303c392490a2e4cc99aa4497c
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2024-02-23 15:00:57 +0330

    first solution

--- type-script/total-typescript/2-typescript-generic/src/03-art-of-type-arguments/13-typed-object-keys.problem.ts
+++ type-script/total-typescript/2-typescript-generic/src/03-art-of-type-arguments/13-typed-object-keys.problem.ts
@@ -1,22 +1,22 @@
-import { expect, it } from "vitest";
-import { Equal, Expect } from "../helpers/type-utils";
+import { expect, it } from 'vitest';
+import { Equal, Expect } from '../helpers/type-utils';
 
 /**
  * There are two possible solutions to this problem - and it's
  * to do with the way you specify the generic. Can you get
  * both solutions?
  */
-const typedObjectKeys = (obj: unknown) => {
-  return Object.keys(obj);
+const typedObjectKeys = <TObject extends {}>(obj: TObject) => {
+  return Object.keys(obj) as Array<keyof TObject>;
 };
 
-it("Should return the keys of the object", () => {
+it('Should return the keys of the object', () => {
   const result1 = typedObjectKeys({
     a: 1,
     b: 2,
   });
 
-  expect(result1).toEqual(["a", "b"]);
+  expect(result1).toEqual(['a', 'b']);
 
-  type test = Expect<Equal<typeof result1, Array<"a" | "b">>>;
+  type test = Expect<Equal<typeof result1, Array<'a' | 'b'>>>;
 });
