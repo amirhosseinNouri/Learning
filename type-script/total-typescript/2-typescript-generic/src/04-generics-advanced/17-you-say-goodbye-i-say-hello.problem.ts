ef5c13c330bdead78cd121f789448b875dce1d70
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2024-02-24 14:16:39 +0330

    conditonal return type in generics

--- type-script/total-typescript/2-typescript-generic/src/04-generics-advanced/17-you-say-goodbye-i-say-hello.problem.ts
+++ type-script/total-typescript/2-typescript-generic/src/04-generics-advanced/17-you-say-goodbye-i-say-hello.problem.ts
@@ -1,22 +1,30 @@
-import { expect, it } from "vitest";
-import { Equal, Expect } from "../helpers/type-utils";
+import { expect, it } from 'vitest';
+import { Equal, Expect } from '../helpers/type-utils';
 
-function youSayGoodbyeISayHello(greeting: unknown) {
-  return greeting === "goodbye" ? "hello" : "goodbye";
+type GreetingResult<TGreeting> = TGreeting extends 'goodbye'
+  ? 'hello'
+  : 'goodbye';
+
+function youSayGoodbyeISayHello<TGreeting extends 'hello' | 'goodbye'>(
+  greeting: TGreeting,
+): TGreeting extends 'goodbye' ? 'hello' : 'goodbye' {
+  return (
+    greeting === 'goodbye' ? 'hello' : 'goodbye'
+  ) as GreetingResult<TGreeting>;
 }
 
-it("Should return goodbye when hello is passed in", () => {
-  const result = youSayGoodbyeISayHello("hello");
+it('Should return goodbye when hello is passed in', () => {
+  const result = youSayGoodbyeISayHello('hello');
 
-  type test = [Expect<Equal<typeof result, "goodbye">>];
+  type test = [Expect<Equal<typeof result, 'goodbye'>>];
 
-  expect(result).toEqual("goodbye");
+  expect(result).toEqual('goodbye');
 });
 
-it("Should return hello when goodbye is passed in", () => {
-  const result = youSayGoodbyeISayHello("goodbye");
+it('Should return hello when goodbye is passed in', () => {
+  const result = youSayGoodbyeISayHello('goodbye');
 
-  type test = [Expect<Equal<typeof result, "hello">>];
+  type test = [Expect<Equal<typeof result, 'hello'>>];
 
-  expect(result).toEqual("hello");
+  expect(result).toEqual('hello');
 });
