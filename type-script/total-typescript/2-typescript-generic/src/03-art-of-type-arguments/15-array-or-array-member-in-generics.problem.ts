cf784dfc7a319a4566311065f4b79ff486ca1ab6
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2024-02-24 13:58:35 +0330

    array or array member in generic

--- type-script/total-typescript/2-typescript-generic/src/03-art-of-type-arguments/15-array-or-array-member-in-generics.problem.ts
+++ type-script/total-typescript/2-typescript-generic/src/03-art-of-type-arguments/15-array-or-array-member-in-generics.problem.ts
@@ -1,11 +1,11 @@
-import { Equal, Expect } from "../helpers/type-utils";
+import { Equal, Expect } from '../helpers/type-utils';
 
-const makeStatus = <TStatuses extends string[]>(statuses: TStatuses) => {
+const makeStatus = <TStatus extends string>(statuses: TStatus[]) => {
   return statuses;
 };
 
-const statuses = makeStatus(["INFO", "DEBUG", "ERROR", "WARNING"]);
+const statuses = makeStatus(['INFO', 'DEBUG', 'ERROR', 'WARNING']);
 
 type tests = [
-  Expect<Equal<typeof statuses, Array<"INFO" | "DEBUG" | "ERROR" | "WARNING">>>,
+  Expect<Equal<typeof statuses, Array<'INFO' | 'DEBUG' | 'ERROR' | 'WARNING'>>>,
 ];
