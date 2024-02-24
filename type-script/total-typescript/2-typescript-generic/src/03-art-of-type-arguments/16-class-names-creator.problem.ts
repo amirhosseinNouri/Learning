35e7c7597e8b8550d9e65cee3d27664dd54e2019
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2024-02-24 14:03:03 +0330

    class names creator

--- type-script/total-typescript/2-typescript-generic/src/03-art-of-type-arguments/16-class-names-creator.problem.ts
+++ type-script/total-typescript/2-typescript-generic/src/03-art-of-type-arguments/16-class-names-creator.problem.ts
@@ -1,41 +1,43 @@
-import { expect, it } from "vitest";
-import { Equal, Expect } from "../helpers/type-utils";
+import { expect, it } from 'vitest';
+import { Equal, Expect } from '../helpers/type-utils';
 
 const createClassNamesFactory =
-  (classes: unknown) =>
-  (type: unknown, ...otherClasses: unknown[]) => {
+  <TClasses extends Record<string, string>, TType extends keyof TClasses>(
+    classes: TClasses,
+  ) =>
+  (type: TType, ...otherClasses: unknown[]) => {
     const classList = [classes[type], ...otherClasses];
-    return classList.join(" ");
+    return classList.join(' ');
   };
 
 const getBg = createClassNamesFactory({
-  primary: "bg-blue-500",
-  secondary: "bg-gray-500",
+  primary: 'bg-blue-500',
+  secondary: 'bg-gray-500',
 });
 
-it("Should let you create classes from a className factory", () => {
-  expect(getBg("primary")).toEqual("bg-blue-500");
-  expect(getBg("secondary")).toEqual("bg-gray-500");
+it('Should let you create classes from a className factory', () => {
+  expect(getBg('primary')).toEqual('bg-blue-500');
+  expect(getBg('secondary')).toEqual('bg-gray-500');
 });
 
-it("Should let you pass additional classes which get appended", () => {
-  expect(getBg("primary", "text-white", "rounded", "p-4")).toEqual(
-    "bg-blue-500 text-white rounded p-4"
+it('Should let you pass additional classes which get appended', () => {
+  expect(getBg('primary', 'text-white', 'rounded', 'p-4')).toEqual(
+    'bg-blue-500 text-white rounded p-4',
   );
 });
 
-it("Should return a type of string", () => {
-  const result = getBg("primary");
+it('Should return a type of string', () => {
+  const result = getBg('primary');
 
   type test = Expect<Equal<typeof result, string>>;
 });
 
-it("Should not let you pass invalid variants", () => {
+it('Should not let you pass invalid variants', () => {
   // @ts-expect-error
-  getBg("123123");
+  getBg('123123');
 });
 
-it("Should not let you pass an invalid object to createClassNamesFactory", () => {
+it('Should not let you pass an invalid object to createClassNamesFactory', () => {
   // @ts-expect-error
   createClassNamesFactory([]);
 
