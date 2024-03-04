62ef342f8f368203c0eb7687b1b406e1ee65e03a
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2024-03-04 13:49:50 +0330

    generic interface with functions

--- type-script/total-typescript/2-typescript-generic/src/04-generics-advanced/19-generic-interfaces-with-functions.problem.ts
+++ type-script/total-typescript/2-typescript-generic/src/04-generics-advanced/19-generic-interfaces-with-functions.problem.ts
@@ -1,11 +1,11 @@
-import { expect, it } from "vitest";
-import { Equal, Expect } from "../helpers/type-utils";
+import { expect, it } from 'vitest';
+import { Equal, Expect } from '../helpers/type-utils';
 
 export interface Cache<T> {
   get: (key: string) => T | undefined;
   set: (key: string, value: T) => void;
   // You can fix this by only changing the line below!
-  clone: (transform: (elem: unknown) => unknown) => Cache<unknown>;
+  clone: <U>(transform: (elem: T) => U) => Cache<U>;
 }
 
 const createCache = <T>(initialCache?: Record<string, T>): Cache<T> => {
@@ -27,29 +27,29 @@ const createCache = <T>(initialCache?: Record<string, T>): Cache<T> => {
   };
 };
 
-it("Should let you get and set to/from the cache", () => {
+it('Should let you get and set to/from the cache', () => {
   const cache = createCache<number>();
 
-  cache.set("a", 1);
-  cache.set("b", 2);
+  cache.set('a', 1);
+  cache.set('b', 2);
 
-  expect(cache.get("a")).toEqual(1);
-  expect(cache.get("b")).toEqual(2);
+  expect(cache.get('a')).toEqual(1);
+  expect(cache.get('b')).toEqual(2);
 });
 
-it("Should let you clone the cache using a transform function", () => {
+it('Should let you clone the cache using a transform function', () => {
   const numberCache = createCache<number>();
 
-  numberCache.set("a", 1);
-  numberCache.set("b", 2);
+  numberCache.set('a', 1);
+  numberCache.set('b', 2);
 
   const stringCache = numberCache.clone((elem) => {
     return String(elem);
   });
 
-  const a = stringCache.get("a");
+  const a = stringCache.get('a');
 
-  expect(a).toEqual("1");
+  expect(a).toEqual('1');
 
   type tests = [Expect<Equal<typeof a, string | undefined>>];
 });
