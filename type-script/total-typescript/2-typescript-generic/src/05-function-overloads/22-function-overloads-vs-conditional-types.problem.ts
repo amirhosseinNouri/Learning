aae995297a90d0eaacff893215213688fcae5907
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2024-03-04 14:34:23 +0330

    function overload

--- type-script/total-typescript/2-typescript-generic/src/05-function-overloads/22-function-overloads-vs-conditional-types.problem.ts
+++ type-script/total-typescript/2-typescript-generic/src/05-function-overloads/22-function-overloads-vs-conditional-types.problem.ts
@@ -1,26 +1,30 @@
-import { expect, it } from "vitest";
-import { Equal, Expect } from "../helpers/type-utils";
+import { expect, it } from 'vitest';
+import { Equal, Expect } from '../helpers/type-utils';
 
 /**
  * This time, let's try and solve this one
  * with function overloads too!
  */
-export const youSayGoodbyeISayHello = (greeting: "goodbye" | "hello") => {
-  return greeting === "goodbye" ? "hello" : "goodbye";
-};
 
-it("Should return goodbye when hello is passed in", () => {
-  const result = youSayGoodbyeISayHello("hello");
+export function youSayGoodbyeISayHello(greeting: 'goodbye'): 'hello';
+export function youSayGoodbyeISayHello(greeting: 'hello'): 'goodbye';
 
-  type test = [Expect<Equal<typeof result, "goodbye">>];
+export function youSayGoodbyeISayHello(greeting: 'goodbye' | 'hello') {
+  return greeting === 'goodbye' ? 'hello' : 'goodbye';
+}
 
-  expect(result).toEqual("goodbye");
+it('Should return goodbye when hello is passed in', () => {
+  const result = youSayGoodbyeISayHello('hello');
+
+  type test = [Expect<Equal<typeof result, 'goodbye'>>];
+
+  expect(result).toEqual('goodbye');
 });
 
-it("Should return hello when goodbye is passed in", () => {
-  const result = youSayGoodbyeISayHello("goodbye");
+it('Should return hello when goodbye is passed in', () => {
+  const result = youSayGoodbyeISayHello('goodbye');
 
-  type test = [Expect<Equal<typeof result, "hello">>];
+  type test = [Expect<Equal<typeof result, 'hello'>>];
 
-  expect(result).toEqual("hello");
+  expect(result).toEqual('hello');
 });
