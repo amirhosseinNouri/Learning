0802154c8bd8abef92e924aa0eb22381f7435a81
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2024-03-05 14:25:01 +0330

    querySelector function overload

--- type-script/total-typescript/2-typescript-generic/src/05-function-overloads/26-document.queryselector-example.problem.ts
+++ type-script/total-typescript/2-typescript-generic/src/05-function-overloads/26-document.queryselector-example.problem.ts
@@ -1,16 +1,16 @@
-import { Equal, Expect } from "../helpers/type-utils";
+import { Equal, Expect } from '../helpers/type-utils';
 
-const divElement = document.querySelector("div");
-const spanElement = document.querySelector("span");
+const divElement = document.querySelector('div');
+const spanElement = document.querySelector('span');
 
 /**
  * Your challenge: figure out why divElement2 is NOT
  * of type HTMLDivElement.
  */
-const divElement2 = document.querySelector("div.foo");
+const divElement2 = document.querySelector<HTMLDivElement>('div.foo');
 
 type tests = [
   Expect<Equal<typeof divElement, HTMLDivElement | null>>,
   Expect<Equal<typeof spanElement, HTMLSpanElement | null>>,
-  Expect<Equal<typeof divElement2, HTMLDivElement | null>>
+  Expect<Equal<typeof divElement2, HTMLDivElement | null>>,
 ];
