9a429eaf786906080fbaf7c7003f1ba5316f9cb0
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2024-03-09 23:18:17 +0330

    catch all segments

---
+++ next-js/nextjs-14/src/app/docs/[...slug]/page.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+type DocsPageProps = {
+  params: {
+    slug: string[];
+  };
+};
+
+const DocsPage = ({ params }: DocsPageProps) => {
+  if (params.slug.length === 2) {
+    return (
+      <h1>
+        Viewing docs for feature {params.slug[0]} and concept {params.slug[1]}
+      </h1>
+    );
+  }
+
+  if (params.slug.length == 1) {
+    return <h1>Viewing docs for {params.slug[0]}</h1>;
+  }
+
+  return <div>Docs home page</div>;
+};
+
+export default DocsPage;
