f7992d5d6a98a6e14500dce30de9675c96918b85
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2024-03-09 23:22:59 +0330

    not found page

---
+++ next-js/nextjs-14/src/app/products/[productId]/reviews/[reviewId]/not-found.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function NotFoundReviewPage() {
+  return <div>NotFoundReviewPage</div>;
+}
--- next-js/nextjs-14/src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ next-js/nextjs-14/src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,8 +1,15 @@
+import { notFound } from 'next/navigation';
+
 type ReviewDetailsProps = {
   params: { reviewId: string };
 };
 
 const ProductDetailsPage = ({ params }: ReviewDetailsProps) => {
+  if (1 > 0) {
+    notFound();
+    return;
+  }
+
   return <h1>Review details page {params.reviewId}</h1>;
 };
 
