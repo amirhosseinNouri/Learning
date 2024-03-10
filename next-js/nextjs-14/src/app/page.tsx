2d445b6521fc816fcc1fe1cc84a128fd848109e3
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2024-03-10 10:01:56 +0330

    UI routing using the Link component

--- next-js/nextjs-14/src/app/page.tsx
+++ next-js/nextjs-14/src/app/page.tsx
@@ -1,3 +1,11 @@
-const HomePage = () => <div>Home Page</div>;
+import Link from 'next/link';
+
+const HomePage = () => (
+  <div>
+    <h1>Home Page</h1>
+    <Link href="/blog">Blog</Link>
+    <Link href="/products">Products</Link>
+  </div>
+);
 
 export default HomePage;
--- next-js/nextjs-14/src/app/products/page.tsx
+++ next-js/nextjs-14/src/app/products/page.tsx
@@ -1,11 +1,24 @@
 import React from 'react';
+import Link from 'next/link';
 
-const Products = () => {
+type ProductsProps = {
+  params: {
+    productId: string;
+  };
+};
+
+const Products = ({ params }: ProductsProps) => {
   return (
     <div>
-      <h1>Product 1</h1>
-      <h1>Product 2</h1>
-      <h1>Product 3</h1>
+      <Link href="products/1">
+        <h1>Product 1</h1>
+      </Link>
+      <Link href="products/2">
+        <h1>Product 2</h1>
+      </Link>
+      <Link href="products/3" replace>
+        <h1>Product 3</h1>
+      </Link>
     </div>
   );
 };
