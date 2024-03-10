102ff934e3e08064819b457c64cd6159f6b2d0dd
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2024-03-10 09:34:28 +0330

    dynamic metadata

--- next-js/nextjs-14/src/app/products/[productId]/page.tsx
+++ next-js/nextjs-14/src/app/products/[productId]/page.tsx
@@ -1,6 +1,14 @@
+import { Metadata } from 'next';
+
 type ProductDetailsProps = {
   params: { productId: string };
 };
+
+export const generateMetadata = ({
+  params,
+}: ProductDetailsProps): Metadata => ({
+  title: params.productId,
+});
 
 const ProductDetails = ({ params }: ProductDetailsProps) => {
   return <h1>Product details page {params.productId}</h1>;
