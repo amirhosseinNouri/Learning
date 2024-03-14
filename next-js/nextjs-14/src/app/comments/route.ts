363d26a80d929b740854934b589ce09919607a91
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2024-03-14 22:49:59 +0330

    POST handler

--- next-js/nextjs-14/src/app/comments/route.ts
+++ next-js/nextjs-14/src/app/comments/route.ts
@@ -3,3 +3,15 @@ import { comments } from './data';
 export async function GET() {
   return Response.json(comments);
 }
+
+export async function POST(request: Request) {
+  const payload = await request.json();
+  const comment = { id: comments.length + 1, text: payload.text };
+  comments.push(comment);
+  return new Response(JSON.stringify({ data: { comment } }), {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    status: 201,
+  });
+}
