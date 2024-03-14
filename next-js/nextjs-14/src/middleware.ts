d07003a9625df88380e4a5bf7b189b4a2aa33ab8
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2024-03-14 23:47:45 +0330

    redirect using conditional statement approach

--- next-js/nextjs-14/src/middleware.ts
+++ next-js/nextjs-14/src/middleware.ts
@@ -2,9 +2,12 @@ import { NextResponse } from 'next/server';
 import { NextRequest } from 'next/server';
 
 export const middleware = (request: NextRequest) => {
-  return NextResponse.redirect(new URL('/', request.url));
+  //   return NextResponse.redirect(new URL('/', request.url));
+  if (request.nextUrl.pathname === '/profile') {
+    return NextResponse.redirect(new URL('/hello', request.url));
+  }
 };
 
-export const config = {
-  matcher: '/profile',
-};
+// export const config = {
+//   matcher: '/profile',
+// };
