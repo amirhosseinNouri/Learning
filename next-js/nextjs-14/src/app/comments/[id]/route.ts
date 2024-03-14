9b48463ba150febc8cb959d783b9cf1cb545befe
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2024-03-14 23:08:11 +0330

    PATCH handler

---
+++ next-js/nextjs-14/src/app/comments/[id]/route.ts
@@ -0,0 +1,43 @@
+import { comments } from '../data';
+
+type Context = {
+  params: {
+    id: string;
+  };
+};
+
+export async function GET(_: Request, { params }: Context) {
+  const comment = comments.find((comment) => comment.id === Number(params.id));
+
+  if (!comment) {
+    return new Response(
+      JSON.stringify({ data: { message: 'Comment not found' } }),
+      { status: 404 },
+    );
+  }
+
+  return new Response(JSON.stringify({ data: { comment } }), { status: 200 });
+}
+
+export async function PATCH(request: Request, { params }: Context) {
+  const commentIndex = comments.findIndex(
+    (comment) => comment.id === Number(params.id),
+  );
+
+  if (commentIndex === -1) {
+    return new Response(
+      JSON.stringify({ data: { message: 'Comment not found' } }),
+      { status: 404 },
+    );
+  }
+
+  const payload = await request.json();
+
+  const updatedComment = { ...comments[commentIndex], text: payload.text };
+
+  comments.splice(commentIndex, 1, updatedComment);
+
+  return new Response(JSON.stringify({ data: { comment: updatedComment } }), {
+    status: 200,
+  });
+}
