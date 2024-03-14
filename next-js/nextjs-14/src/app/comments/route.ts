66ad465f46350ccb46e4af21a9b100b5c44f509b
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2024-03-14 23:16:30 +0330

    query parameters in route handlers

--- next-js/nextjs-14/src/app/comments/route.ts
+++ next-js/nextjs-14/src/app/comments/route.ts
@@ -1,7 +1,14 @@
+import { NextRequest } from 'next/server';
 import { comments } from './data';
 
-export async function GET() {
-  return Response.json(comments);
+export async function GET(request: NextRequest) {
+  const { searchParams } = request.nextUrl;
+  const query = searchParams.get('search');
+  const filteredComments = query
+    ? comments.filter((comment) => comment.text.includes(query))
+    : comments;
+
+  return Response.json({ data: { comments: filteredComments } });
 }
 
 export async function POST(request: Request) {
