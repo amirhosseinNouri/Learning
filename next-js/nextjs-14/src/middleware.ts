6c4db28ea66066a5dadab26a687dd52870033d0f
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2024-03-14 23:52:17 +0330

    managing cookies and headers in middleware

--- next-js/nextjs-14/src/middleware.ts
+++ next-js/nextjs-14/src/middleware.ts
@@ -1,7 +1,20 @@
+import { cookies } from 'next/headers';
 import { NextResponse } from 'next/server';
 import { NextRequest } from 'next/server';
 
 export const middleware = (request: NextRequest) => {
+  const response = NextResponse.next();
+
+  const theme = response.cookies.get('theme');
+
+  if (!theme) {
+    response.cookies.set('theme', 'dark');
+  }
+
+  response.headers.set('x-custom-header', 'custom value');
+
+  return response;
+
   //   return NextResponse.redirect(new URL('/', request.url));
   if (request.nextUrl.pathname === '/profile') {
     return NextResponse.redirect(new URL('/hello', request.url));
