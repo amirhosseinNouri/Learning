828f0ba706bb8f55a880aec4491309438f78e077
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2024-03-14 23:12:11 +0330

    DELETE handler

--- next-js/nextjs-14/src/app/comments/[id]/route.ts
+++ next-js/nextjs-14/src/app/comments/[id]/route.ts
@@ -38,6 +38,25 @@ export async function PATCH(request: Request, { params }: Context) {
   comments.splice(commentIndex, 1, updatedComment);
 
   return new Response(JSON.stringify({ data: { comment: updatedComment } }), {
-    status: 200,
+    status: 204,
+  });
+}
+
+export async function DELETE(_: Request, { params }: Context) {
+  const commentIndex = comments.findIndex(
+    (comment) => comment.id === Number(params.id),
+  );
+
+  if (commentIndex === -1) {
+    return new Response(
+      JSON.stringify({ data: { message: 'Comment not found' } }),
+      { status: 404 },
+    );
+  }
+
+  comments.splice(commentIndex, 1);
+
+  return new Response(null, {
+    status: 204,
   });
 }
