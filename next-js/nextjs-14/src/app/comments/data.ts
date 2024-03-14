4d9b807bd4ad11b0748b0f2e3fbc0ae4022a956b
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2024-03-14 15:52:32 +0330

    GET handler

---
+++ next-js/nextjs-14/src/app/comments/data.ts
@@ -0,0 +1,14 @@
+export const comments = [
+  {
+    id: 1,
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut',
+  },
+  {
+    id: 2,
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut',
+  },
+  {
+    id: 3,
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut',
+  },
+];
