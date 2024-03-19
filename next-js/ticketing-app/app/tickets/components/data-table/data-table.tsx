551fc18823205838ee1044782f4c79fff043cda0
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2024-03-19 20:50:27 +0330

    implement TicketPripority component

--- next-js/ticketing-app/app/tickets/components/data-table/data-table.tsx
+++ next-js/ticketing-app/app/tickets/components/data-table/data-table.tsx
@@ -1,3 +1,4 @@
+import TicketPriority from '@/components/ticket-priority';
 import TicketStatusBadge from '@/components/ticket-status-badge';
 import {
   Table,
@@ -25,7 +26,9 @@ const DataTable = ({ tickets }: DataTableProps) => {
               <TableHead>
                 <div className="flex justify-center">Status</div>
               </TableHead>
-              <TableHead>Priority</TableHead>
+              <TableHead>
+                <div className="flex flex-row justify-center">Priority</div>
+              </TableHead>
               <TableHead>Created At</TableHead>
             </TableRow>
           </TableHeader>
@@ -39,7 +42,11 @@ const DataTable = ({ tickets }: DataTableProps) => {
                         <TicketStatusBadge status={ticket.status} />
                       </div>
                     </TableCell>
-                    <TableCell>{ticket.priority}</TableCell>
+                    <TableCell>
+                      <div className="flex flex-row justify-center">
+                        <TicketPriority priority={ticket.priority} />
+                      </div>
+                    </TableCell>
                     <TableCell>
                       {ticket.createdAt.toLocaleDateString('en-US', {
                         year: '2-digit',
---
+++ next-js/ticketing-app/components/ticket-priority/ticket-priority.tsx
@@ -0,0 +1,50 @@
+import { Ticket } from '@prisma/client';
+import { Flame } from 'lucide-react';
+import React from 'react';
+
+interface TicketPriorityProps {
+  priority: Ticket['priority'];
+}
+
+type PriorityLevel = 1 | 2 | 3;
+
+type PriorityDetails = { label: string; level: PriorityLevel };
+
+const MAP_PRIORITY_TO_DETAILS: Record<
+  TicketPriorityProps['priority'],
+  PriorityDetails
+> = {
+  HIGH: { label: 'High', level: 3 },
+  MEDIUM: { label: 'Medium', level: 2 },
+  LOW: { label: 'Low', level: 1 },
+};
+
+const TicketPriority = ({ priority }: TicketPriorityProps) => {
+  return (
+    <>
+      <Flame
+        className={`${
+          MAP_PRIORITY_TO_DETAILS[priority].level >= 1
+            ? 'text-red-500'
+            : 'text-muted'
+        }`}
+      />
+      <Flame
+        className={`${
+          MAP_PRIORITY_TO_DETAILS[priority].level >= 2
+            ? 'text-red-500'
+            : 'text-muted'
+        }`}
+      />
+      <Flame
+        className={`${
+          MAP_PRIORITY_TO_DETAILS[priority].level >= 3
+            ? 'text-red-500'
+            : 'text-muted'
+        }`}
+      />
+    </>
+  );
+};
+
+export default TicketPriority;
