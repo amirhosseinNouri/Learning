82f81858d375c02fd0da73c67fbedad0175b722b
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2024-03-25 17:40:07 +0330

    improve main navigation

--- next-js/ticketing-app/components/main-nav/main-nav.tsx
+++ next-js/ticketing-app/components/main-nav/main-nav.tsx
@@ -1,14 +1,35 @@
+'use client';
 import Link from 'next/link';
 import React from 'react';
 import ToggleMode from '@/components/toggle-mode';
+import { usePathname } from 'next/navigation';
+
+type NavigationLink = { label: string; url: string };
+
+const LINKS: NavigationLink[] = [
+  { label: 'Dashboard', url: '/' },
+  { label: 'Tickets', url: '/tickets' },
+  { label: 'Users', url: '/users' },
+];
 
 export default function MainNav() {
+  const currentPath = usePathname();
+
   return (
     <div className="flex flex-row justify-between">
       <div className="flex items-center gap-2">
-        <Link href="/">Dashboard</Link>
-        <Link href="/tickets">Tickets</Link>
-        <Link href="/users">Users</Link>
+        {LINKS.map((link) => (
+          <Link
+            className={`font-medium text-md ${
+              currentPath === link.url &&
+              'text-primary/70 hover:text-primary/60'
+            }`}
+            key={link.label}
+            href={link.url}
+          >
+            {link.label}
+          </Link>
+        ))}
       </div>
 
       <div className="flex items-center gap-2">
