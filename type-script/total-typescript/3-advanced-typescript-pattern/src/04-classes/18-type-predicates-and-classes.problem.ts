76699374a94f3ee44c2fd46acdfcb888085ad59d
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2024-03-29 16:23:53 +0330

    type predicates and classes problem

--- type-script/total-typescript/3-advanced-typescript-pattern/src/04-classes/18-type-predicates-and-classes.problem.ts
+++ type-script/total-typescript/3-advanced-typescript-pattern/src/04-classes/18-type-predicates-and-classes.problem.ts
@@ -8,7 +8,7 @@ class Form<TValues> {
     private validate: (values: TValues) => string | void,
   ) {}
 
-  isInvalid(): this is Form<TValues> & { error: string } {
+  isInvalid(): this is this & { error: string } {
     const result = this.validate(this.values);
 
     if (typeof result === 'string') {
