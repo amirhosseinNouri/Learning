6671de492a36a2caeb4db6d86c1420d4883b5726
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2024-03-29 14:09:04 +0330

    add function to global scope

--- type-script/total-typescript/3-advanced-typescript-pattern/src/02-globals/07-add-function-to-global-scope.problem.ts
+++ type-script/total-typescript/3-advanced-typescript-pattern/src/02-globals/07-add-function-to-global-scope.problem.ts
@@ -1,5 +1,5 @@
-import { expect, it } from "vitest";
-import { Equal, Expect } from "../helpers/type-utils";
+import { expect, it } from 'vitest';
+import { Equal, Expect } from '../helpers/type-utils';
 
 /**
  * Clues:
@@ -17,15 +17,20 @@ import { Equal, Expect } from "../helpers/type-utils";
  * var myVar: number
  */
 
+declare global {
+  function myFunc(): boolean;
+  var myVar: number;
+}
+
 globalThis.myFunc = () => true;
 globalThis.myVar = 1;
 
-it("Should let you call myFunc without it being imported", () => {
+it('Should let you call myFunc without it being imported', () => {
   expect(myFunc()).toBe(true);
   type test1 = Expect<Equal<typeof myFunc, () => boolean>>;
 });
 
-it("Should let you access myVar without it being imported", () => {
+it('Should let you access myVar without it being imported', () => {
   expect(myVar).toBe(1);
   type test1 = Expect<Equal<typeof myVar, number>>;
 });
