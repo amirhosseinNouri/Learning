aac0769a30c1ea746293dc076d4b5a984bd9514c
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2024-04-02 12:44:33 +0330

    override external libs

--- type-script/total-typescript/3-advanced-typescript-pattern/src/05-external-libraries/26.5-declaration.solution.d.ts
+++ type-script/total-typescript/3-advanced-typescript-pattern/src/05-external-libraries/26.5-declaration.solution.d.ts
@@ -1,7 +1,15 @@
-declare module "fake-animation-lib-solution" {
-  export type AnimatingState =
-    | "before-animation"
-    | "animating"
-    | "after-animation";
-  export function getAnimatingState(): AnimatingState;
+// declare module "fake-animation-lib-solution" {
+//   export type AnimatingState =
+//     | "before-animation"
+//     | "animating"
+//     | "after-animation";
+//   export function getAnimatingState(): AnimatingState;
+// }
+
+declare module 'fake-animation-lib' {
+  export type AnimationState =
+    | 'before-animation'
+    | 'animating'
+    | 'after-animation';
+  export function getAnimatingState(): AnimationState;
 }
