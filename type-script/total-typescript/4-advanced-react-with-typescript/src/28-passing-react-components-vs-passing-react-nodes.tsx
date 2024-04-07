b1f24f07c155de27eb4545fdbf8b85839b0a74b0
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2024-04-07 13:25:57 +0330

    add problem

---
+++ type-script/total-typescript/4-advanced-react-with-typescript/src/28-passing-react-components-vs-passing-react-nodes.tsx
@@ -0,0 +1,38 @@
+import { ReactNode } from 'react';
+import { Equal, Expect } from './helpers/type-utils';
+
+interface TableProps {
+  renderRow: ReactNode;
+}
+
+const Table = ({ renderRow }: TableProps) => {
+  return <div>{[1, 2, 3].map(renderRow)}</div>;
+};
+
+const Parent = () => {
+  return (
+    <>
+      <Table
+        renderRow={(index) => {
+          type test = Expect<Equal<typeof index, number>>;
+          return <div key={index}>{index}</div>;
+        }}
+      />
+
+      <Table
+        renderRow={(index) => {
+          return null;
+        }}
+      />
+
+      {/* @ts-expect-error */}
+      <Table renderRow={<div></div>} />
+
+      <Table
+        renderRow={(index) => {
+          return index;
+        }}
+      />
+    </>
+  );
+};
