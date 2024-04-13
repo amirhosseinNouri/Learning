ff5a85d7025c8402516337ea9f7c9d9f833fd7b3
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2024-04-13 11:09:01 +0330

    passing types to components

--- type-script/total-typescript/4-advanced-react-with-typescript/src/05-generics/41-passing-types-to-components.problem.tsx
+++ type-script/total-typescript/4-advanced-react-with-typescript/src/05-generics/41-passing-types-to-components.problem.tsx
@@ -1,5 +1,5 @@
-import { ReactNode } from "react";
-import { Equal, Expect } from "../helpers/type-utils";
+import { ReactNode } from 'react';
+import { Equal, Expect } from '../helpers/type-utils';
 
 interface TableProps<T> {
   rows: T[];
@@ -25,7 +25,7 @@ interface User {
 }
 
 <>
-  <Table
+  <Table<User>
     // @ts-expect-error rows should be User[]
     rows={[1, 2, 3]}
     renderRow={(row) => {
@@ -33,17 +33,17 @@ interface User {
       return <td>{row.name}</td>;
     }}
   />
-  <Table
+  <Table<User>
     rows={[
       {
         id: 1,
-        name: "John",
+        name: 'John',
         age: 30,
       },
       {
         // @ts-expect-error id should be string
-        id: "2",
-        name: "Jane",
+        id: '2',
+        name: 'Jane',
         age: 30,
       },
     ]}
