23788c1c9dc4a239a2d5ff1b933d288c6cf7e0c7
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2024-05-10 18:17:13 +0330

    variables

---
+++ go/go-tutorial-for-begginers/main.go
@@ -0,0 +1,38 @@
+package main
+
+import "fmt"
+
+const (
+	conferenceTickets = 50
+	conferenceName    = "Go conference"
+)
+
+func main() {
+	var remainingTickets uint = conferenceTickets
+
+	fmt.Printf("Welcome to %v booking application.\n", conferenceName)
+	fmt.Printf("We have total of %v tickets and %v are still available.\n", conferenceTickets, remainingTickets)
+
+	var firstName string
+	var lastName string
+	var email string
+	var userTickets uint
+
+	fmt.Printf("Enter your fist name: ")
+	fmt.Scan(&firstName)
+
+	fmt.Printf("Enter your last name: ")
+	fmt.Scan(&lastName)
+
+	fmt.Printf("Enter your email: ")
+	fmt.Scan(&email)
+
+	fmt.Printf("Enter the number of tickets you want: ")
+	fmt.Scan(&userTickets)
+
+	remainingTickets -= userTickets
+
+	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
+
+	fmt.Printf("%v remained tickets.'n", remainingTickets)
+}
