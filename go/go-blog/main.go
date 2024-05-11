99c9314e6fa317f0d1cbaaab37fdfd3449be7fe2
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2024-05-11 10:31:22 +0330

    setup project

---
+++ go/go-blog/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run("localhost:8080")
+}
