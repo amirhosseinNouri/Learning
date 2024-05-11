806153ad97001ce570eb41cb47053be61c72f07b
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2024-05-11 22:47:17 +0330

    implement book model

---
+++ go/book-management/pkg/models/book.go
@@ -0,0 +1,44 @@
+package models
+
+import (
+	"github.com/amirhosseinnouri/Learning/go/book-management/pkg/db"
+	"github.com/jinzhu/gorm"
+)
+
+type Book struct {
+	gorm.Model
+	Name        string `gorm:"" json:"name"`
+	Author      string `json:"author"`
+	Publication string `json:"publication"`
+}
+
+var d *gorm.DB
+
+func init() {
+	d := db.Get()
+	d.AutoMigrate(&Book{})
+}
+
+func (b *Book) CreateBook() *Book {
+	d.NewRecord(b)
+	d.Create(&b)
+	return b
+}
+
+func GetAllBooks() []Book {
+	var books []Book
+	d.Find(&books)
+	return books
+}
+
+func GetBookById(id int64) (*Book, *gorm.DB) {
+	var book Book
+	d.Where("ID=?", id).Find(&book)
+	return &book, d
+}
+
+func DeleteBook(id int64) Book {
+	var book Book
+	d.Where("ID=?", id).Delete(book)
+	return book
+}
