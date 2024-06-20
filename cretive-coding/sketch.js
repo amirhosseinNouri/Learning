3b1db0476014a61f781b64928b60824413ee2da3
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2024-06-20 16:36:41 +0330

    add some margin

--- cretive-coding/sketch.js
+++ cretive-coding/sketch.js
@@ -1,4 +1,5 @@
 const canvasSketch = require('canvas-sketch');
+const { lerp } = require('canvas-sketch-util/math');
 
 const settings = {
   dimensions: [2048, 2048],
@@ -22,14 +23,15 @@ const sketch = () => {
   };
 
   const points = createGrid();
+  const margin = 400;
 
   return ({ context, width, height }) => {
     context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
 
     points.forEach(([u, v]) => {
-      const x = u * width;
-      const y = v * height;
+      const x = lerp(margin, width - margin, u);
+      const y = lerp(margin, height - margin, v);
 
       console.log({ x, y });
 
