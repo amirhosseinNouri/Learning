cd79d08548f09bb09e39b050233deaa60aa52412
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2024-06-20 16:33:38 +0330

    draw a grid

--- cretive-coding/sketch.js
+++ cretive-coding/sketch.js
@@ -1,24 +1,44 @@
 const canvasSketch = require('canvas-sketch');
 
 const settings = {
-  dimensions: 'A4',
+  dimensions: [2048, 2048],
   pixelPerInch: 300,
-  units: 'cm'
 };
 
 const sketch = () => {
+  const createGrid = () => {
+    const points = [];
+    const count = 5;
+
+    for (let x = 0; x < count; x++) {
+      for (let y = 0; y < count; y++) {
+        const u = count <= 1 ? 0.5 : x / (count - 1);
+        const v = count <= 1 ? 0.5 : y / (count - 1);
+        points.push([u, v]);
+      }
+    }
+
+    return points;
+  };
+
+  const points = createGrid();
+
   return ({ context, width, height }) => {
-    context.fillStyle = 'red';
+    context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
 
-    context.beginPath();
-    context.arc(width / 2, height / 2, width * 0.2, 0, Math.PI * 2, false);
-    context.fillStyle = 'blue';
-    context.fill();
+    points.forEach(([u, v]) => {
+      const x = u * width;
+      const y = v * height;
 
-    context.lineWidth = width * 0.05;
-    context.strokeStyle = 'orange';
-    context.stroke();
+      console.log({ x, y });
+
+      context.beginPath();
+      context.arc(x, y, 100, 0, Math.PI * 2, false);
+      context.strokeStyle = 'black';
+      context.lineWidth = 40;
+      context.stroke();
+    });
   };
 };
 
