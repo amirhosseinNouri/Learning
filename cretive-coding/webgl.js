eb9d9b15881de6c6c90004cf98e5b78320cb272d
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2024-06-20 17:30:10 +0330

    earth and moon

--- cretive-coding/webgl.js
+++ cretive-coding/webgl.js
@@ -1,30 +1,30 @@
 // Ensure ThreeJS is in global scope for the 'examples/'
-global.THREE = require("three");
+global.THREE = require('three');
 
 // Include any additional ThreeJS examples below
-require("three/examples/js/controls/OrbitControls");
+require('three/examples/js/controls/OrbitControls');
 
-const canvasSketch = require("canvas-sketch");
+const canvasSketch = require('canvas-sketch');
 
 const settings = {
   // Make the loop animated
   animate: true,
   // Get a WebGL canvas rather than 2D
-  context: "webgl"
+  context: 'webgl',
 };
 
 const sketch = ({ context }) => {
   // Create a renderer
   const renderer = new THREE.WebGLRenderer({
-    canvas: context.canvas
+    canvas: context.canvas,
   });
 
   // WebGL background color
-  renderer.setClearColor("#000", 1);
+  renderer.setClearColor('#000', 1);
 
   // Setup a camera
-  const camera = new THREE.PerspectiveCamera(50, 1, 0.01, 100);
-  camera.position.set(0, 0, -4);
+  const camera = new THREE.PerspectiveCamera(50, 1, 0.01, 500);
+  camera.position.set(3, 3, -5);
   camera.lookAt(new THREE.Vector3());
 
   // Setup camera controller
@@ -36,15 +36,25 @@ const sketch = ({ context }) => {
   // Setup a geometry
   const geometry = new THREE.SphereGeometry(1, 32, 16);
 
+  const loader = new THREE.TextureLoader();
+  const texture = loader.load('earth.jpg');
+  const moonTexture = loader.load('moon.jpg');
+
   // Setup a material
   const material = new THREE.MeshBasicMaterial({
-    color: "red",
-    wireframe: true
+    map: texture,
   });
+
+  const moonMaterial = new THREE.MeshBasicMaterial({ map: moonTexture });
 
   // Setup a mesh with geometry + material
   const mesh = new THREE.Mesh(geometry, material);
   scene.add(mesh);
+
+  const moonMesh = new THREE.Mesh(geometry, moonMaterial);
+  moonMesh.position.set(1.5, 1, 0);
+  moonMesh.scale.setScalar(0.25);
+  scene.add(moonMesh);
 
   // draw each frame
   return {
@@ -57,6 +67,8 @@ const sketch = ({ context }) => {
     },
     // Update & render your scene here
     render({ time }) {
+      mesh.rotation.y = time * 0.15;
+      moonMesh.rotation.y = time * 0.2;
       controls.update();
       renderer.render(scene, camera);
     },
@@ -64,7 +76,7 @@ const sketch = ({ context }) => {
     unload() {
       controls.dispose();
       renderer.dispose();
-    }
+    },
   };
 };
 
