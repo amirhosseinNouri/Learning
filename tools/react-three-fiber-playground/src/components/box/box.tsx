c85dc88c6b16c5e589927581baaabce7c7f8921d
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2024-06-28 10:04:24 +0330

    change geometry dynamically

--- tools/react-three-fiber-playground/src/components/box/box.tsx
+++ tools/react-three-fiber-playground/src/components/box/box.tsx
@@ -4,28 +4,38 @@ import {
   ThreeEvent,
   useFrame,
 } from '@react-three/fiber';
-import { useRef, useState } from 'react';
+import { useMemo, useRef, useState } from 'react';
+import * as THREE from 'three';
 
 const Box = (props: MeshProps) => {
-  const [hovered, setHovered] = useState(false);
-  const [rotate, setRotate] = useState(false);
+  const [counter, setCounter] = useState(0);
 
   const ref = useRef<ThreeElements['mesh']>();
 
+  const geometries = useMemo(
+    () => [new THREE.BoxGeometry(), new THREE.SphereGeometry(0.5)],
+    [],
+  );
+
   useFrame((_, delta) => {
-    if (ref.current) {
-      ref.current.rotation.x += Number(rotate) * delta;
-      ref.current.rotation.y += Number(rotate) * 0.5 * delta;
+    if (ref.current && ref.current.rotation) {
+      ref.current.rotation.x += delta;
+      ref.current.rotation.y += 0.5 * delta;
     }
   });
 
   const handlerPointerDown = (_: ThreeEvent<PointerEvent>) => {
-    setRotate(!rotate);
+    setCounter((counter + 1) % 2);
   };
 
   return (
-    <mesh {...props} ref={ref} onPointerDown={handlerPointerDown}>
-      <boxGeometry />
+    <mesh
+      {...props}
+      // @ts-expect-error I am not sure why this is not working anymore
+      ref={ref}
+      onPointerDown={handlerPointerDown}
+      geometry={geometries[counter]}
+    >
       <meshBasicMaterial color="chocolate" wireframe />
     </mesh>
   );
