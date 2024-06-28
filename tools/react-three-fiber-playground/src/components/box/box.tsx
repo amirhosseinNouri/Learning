d89853ad9a730d5f0e74357fb20e9d3fa8acb717
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2024-06-28 09:47:57 +0330

    handle different states

--- tools/react-three-fiber-playground/src/components/box/box.tsx
+++ tools/react-three-fiber-playground/src/components/box/box.tsx
@@ -4,26 +4,44 @@ import {
   ThreeEvent,
   useFrame,
 } from '@react-three/fiber';
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 
 const Box = (props: MeshProps) => {
+  const [hovered, setHovered] = useState(false);
+  const [rotate, setRotate] = useState(false);
+
   const ref = useRef<ThreeElements['mesh']>();
 
   useFrame((_, delta) => {
-    if (ref.current) {
+    if (ref.current && rotate) {
       ref.current.rotation.x += 1 * delta;
       ref.current.rotation.y += 0.5 * delta;
     }
   });
 
-  const handlerPointerDown = (e: ThreeEvent<PointerEvent>) => {
-    console.log(e);
+  const handlerPointerDown = (_: ThreeEvent<PointerEvent>) => {
+    setRotate(!rotate);
+  };
+
+  const handlePointerOver = () => {
+    setHovered(true);
+  };
+
+  const handlePointerOut = () => {
+    setHovered(false);
   };
 
   return (
-    <mesh {...props} ref={ref} onPointerDown={handlerPointerDown}>
+    <mesh
+      {...props}
+      ref={ref}
+      onPointerDown={handlerPointerDown}
+      onPointerOver={handlePointerOver}
+      onPointerOut={handlePointerOut}
+      scale={hovered ? [1.1, 1.1, 1.1] : [1, 1, 1]}
+    >
       <boxGeometry />
-      <meshBasicMaterial color="chocolate" wireframe />
+      <meshBasicMaterial color={hovered ? 'chocolate' : 'teal'} wireframe />
     </mesh>
   );
 };
