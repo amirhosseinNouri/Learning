ae8b2d752e62a7fae5e63c0e0dd08813b1e9ec2b
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2024-08-17 15:16:11 +0330

    return what I pass in type helper

--- type-script/total-typescript-2/1-type-transformation/src/03.5-type-helpers-pattern/20-type-helpers-pattern.problem.ts
+++ type-script/total-typescript-2/1-type-transformation/src/03.5-type-helpers-pattern/20-type-helpers-pattern.problem.ts
@@ -1,10 +1,10 @@
-import { Equal, Expect } from "../helpers/type-utils";
+import { Equal, Expect } from '../helpers/type-utils';
 
-type ReturnWhatIPassIn = unknown;
+type ReturnWhatIPassIn<T> = T;
 
 type tests = [
   Expect<Equal<ReturnWhatIPassIn<1>, 1>>,
-  Expect<Equal<ReturnWhatIPassIn<"1">, "1">>,
+  Expect<Equal<ReturnWhatIPassIn<'1'>, '1'>>,
   Expect<Equal<ReturnWhatIPassIn<true>, true>>,
   Expect<Equal<ReturnWhatIPassIn<false>, false>>,
   Expect<Equal<ReturnWhatIPassIn<null>, null>>,
