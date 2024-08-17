2c42f8d0a48453b97d1d3232668803dfd8b5d933
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2024-08-17 15:31:20 +0330

    conditional types

--- type-script/total-typescript-2/1-type-transformation/src/04-conditional-types-and-infer/21-conditional-types.problem.ts
+++ type-script/total-typescript-2/1-type-transformation/src/04-conditional-types-and-infer/21-conditional-types.problem.ts
@@ -1,8 +1,9 @@
-import { Equal, Expect } from "../helpers/type-utils";
+import { Equal, Expect } from '../helpers/type-utils';
 
-type YouSayGoodbyeAndISayHello = unknown;
+type YouSayGoodbyeAndISayHello<T extends 'hello' | 'goodbye'> =
+  T extends 'hello' ? 'goodbye' : 'hello';
 
 type tests = [
-  Expect<Equal<YouSayGoodbyeAndISayHello<"hello">, "goodbye">>,
-  Expect<Equal<YouSayGoodbyeAndISayHello<"goodbye">, "hello">>,
+  Expect<Equal<YouSayGoodbyeAndISayHello<'hello'>, 'goodbye'>>,
+  Expect<Equal<YouSayGoodbyeAndISayHello<'goodbye'>, 'hello'>>,
 ];
