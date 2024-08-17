c4e0c91a968ae1eab141ba9c1e9251a211d50d0b
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2024-08-17 15:11:57 +0330

    template literals in object keys

--- type-script/total-typescript-2/1-type-transformation/src/03-template-literals/18-template-literals-in-object-keys.problem.ts
+++ type-script/total-typescript-2/1-type-transformation/src/03-template-literals/18-template-literals-in-object-keys.problem.ts
@@ -1,8 +1,8 @@
-import { Equal, Expect } from "../helpers/type-utils";
+import { Equal, Expect } from '../helpers/type-utils';
 
-type TemplateLiteralKey = `${"user" | "post" | "comment"}${"Id" | "Name"}`;
+type TemplateLiteralKey = `${'user' | 'post' | 'comment'}${'Id' | 'Name'}`;
 
-type ObjectOfKeys = unknown;
+type ObjectOfKeys = Record<TemplateLiteralKey, string>;
 
 type tests = [
   Expect<
