694f0e5a008b6c9b0fa64b3573f22e7833bc59f2
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2024-08-17 15:10:04 +0330

    unions in template literals

--- type-script/total-typescript-2/1-type-transformation/src/03-template-literals/16-unions-in-template-literals.problem.ts
+++ type-script/total-typescript-2/1-type-transformation/src/03-template-literals/16-unions-in-template-literals.problem.ts
@@ -1,24 +1,24 @@
-import { Equal, Expect } from "../helpers/type-utils";
+import { Equal, Expect } from '../helpers/type-utils';
 
-type BreadType = "rye" | "brown" | "white";
+type BreadType = 'rye' | 'brown' | 'white';
 
-type Filling = "cheese" | "ham" | "salami";
+type Filling = 'cheese' | 'ham' | 'salami';
 
-type Sandwich = unknown;
+type Sandwich = `${BreadType} sandwich with ${Filling}`;
 
 type tests = [
   Expect<
     Equal<
       Sandwich,
-      | "rye sandwich with cheese"
-      | "rye sandwich with ham"
-      | "rye sandwich with salami"
-      | "brown sandwich with cheese"
-      | "brown sandwich with ham"
-      | "brown sandwich with salami"
-      | "white sandwich with cheese"
-      | "white sandwich with ham"
-      | "white sandwich with salami"
+      | 'rye sandwich with cheese'
+      | 'rye sandwich with ham'
+      | 'rye sandwich with salami'
+      | 'brown sandwich with cheese'
+      | 'brown sandwich with ham'
+      | 'brown sandwich with salami'
+      | 'white sandwich with cheese'
+      | 'white sandwich with ham'
+      | 'white sandwich with salami'
     >
-  >
+  >,
 ];
