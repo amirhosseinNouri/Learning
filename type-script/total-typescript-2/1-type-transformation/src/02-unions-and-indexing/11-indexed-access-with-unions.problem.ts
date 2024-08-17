ce1667d1cd88974d889dc9c17a8aa324b60aac97
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2024-08-17 15:05:14 +0330

    indexed access with unions

--- type-script/total-typescript-2/1-type-transformation/src/02-unions-and-indexing/11-indexed-access-with-unions.problem.ts
+++ type-script/total-typescript-2/1-type-transformation/src/02-unions-and-indexing/11-indexed-access-with-unions.problem.ts
@@ -1,21 +1,25 @@
-import { Equal, Expect } from "../helpers/type-utils";
+import { Equal, Expect } from '../helpers/type-utils';
 
 export const programModeEnumMap = {
-  GROUP: "group",
-  ANNOUNCEMENT: "announcement",
-  ONE_ON_ONE: "1on1",
-  SELF_DIRECTED: "selfDirected",
-  PLANNED_ONE_ON_ONE: "planned1on1",
-  PLANNED_SELF_DIRECTED: "plannedSelfDirected",
+  GROUP: 'group',
+  ANNOUNCEMENT: 'announcement',
+  ONE_ON_ONE: '1on1',
+  SELF_DIRECTED: 'selfDirected',
+  PLANNED_ONE_ON_ONE: 'planned1on1',
+  PLANNED_SELF_DIRECTED: 'plannedSelfDirected',
 } as const;
 
-export type IndividualProgram = unknown;
+type ExtractObjectValues<T> = T[keyof T];
+
+type ProgramMode = ExtractObjectValues<typeof programModeEnumMap>;
+
+export type IndividualProgram = Exclude<ProgramMode, 'group' | 'announcement'>;
 
 type tests = [
   Expect<
     Equal<
       IndividualProgram,
-      "1on1" | "selfDirected" | "planned1on1" | "plannedSelfDirected"
+      '1on1' | 'selfDirected' | 'planned1on1' | 'plannedSelfDirected'
     >
   >,
 ];
