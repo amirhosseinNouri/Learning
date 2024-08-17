be4e57860519ea1ad085ca1cd57ac61a6d7ceeee
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2024-08-17 15:27:43 +0330

    function constrains

--- type-script/total-typescript-2/1-type-transformation/src/03.5-type-helpers-pattern/20.5-function-constraints.problem.ts
+++ type-script/total-typescript-2/1-type-transformation/src/03.5-type-helpers-pattern/20.5-function-constraints.problem.ts
@@ -1,6 +1,6 @@
-import { Equal, Expect } from "../helpers/type-utils";
+import { Equal, Expect } from '../helpers/type-utils';
 
-type GetParametersAndReturnType<T> = {
+type GetParametersAndReturnType<T extends (...params: any[]) => any> = {
   params: Parameters<T>;
   returnValue: ReturnType<T>;
 };
