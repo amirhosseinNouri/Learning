59469f6da73c2c3210b26ecefcd5a1f37a070783
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2024-08-21 12:53:14 +0330

    generic set with default

--- type-script/total-typescript-2/2-typescript-generic/src/02-passing-type-arguments/08-default-generics.problem.ts
+++ type-script/total-typescript-2/2-typescript-generic/src/02-passing-type-arguments/08-default-generics.problem.ts
@@ -1,6 +1,6 @@
-import { Equal, Expect } from "../helpers/type-utils";
+import { Equal, Expect } from '../helpers/type-utils';
 
-export const createSet = <T>() => {
+export const createSet = <T = string>() => {
   return new Set<T>();
 };
 
@@ -11,5 +11,5 @@ const otherStringSet = createSet();
 type tests = [
   Expect<Equal<typeof numberSet, Set<number>>>,
   Expect<Equal<typeof stringSet, Set<string>>>,
-  Expect<Equal<typeof otherStringSet, Set<string>>>
+  Expect<Equal<typeof otherStringSet, Set<string>>>,
 ];
