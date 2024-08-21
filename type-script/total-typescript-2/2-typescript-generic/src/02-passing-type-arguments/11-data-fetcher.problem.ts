22575a2897d300f9b5dd786c8c67a478c6e8d8a4
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2024-08-21 12:57:52 +0330

    data fetcher problem

--- type-script/total-typescript-2/2-typescript-generic/src/02-passing-type-arguments/11-data-fetcher.problem.ts
+++ type-script/total-typescript-2/2-typescript-generic/src/02-passing-type-arguments/11-data-fetcher.problem.ts
@@ -1,16 +1,16 @@
-import { expect, it } from "vitest";
-import { Equal, Expect } from "../helpers/type-utils";
+import { expect, it } from 'vitest';
+import { Equal, Expect } from '../helpers/type-utils';
 
-const fetchData = async (url: string) => {
+const fetchData = async <TData>(url: string) => {
   const data = await fetch(url).then((response) => response.json());
-  return data;
+  return data as TData;
 };
 
-it("Should fetch data from an API", async () => {
+it('Should fetch data from an API', async () => {
   const data = await fetchData<{ name: string }>(
-    "https://swapi.dev/api/people/1",
+    'https://swapi.dev/api/people/1',
   );
-  expect(data.name).toEqual("Luke Skywalker");
+  expect(data.name).toEqual('Luke Skywalker');
 
   type tests = [Expect<Equal<typeof data, { name: string }>>];
 });
