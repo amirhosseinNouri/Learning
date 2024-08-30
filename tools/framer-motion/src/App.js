90aace7929c3a90f22c55ea34d790484057c3b07
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2024-08-30 07:31:50 +0330

    add files

---
+++ tools/framer-motion/src/App.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { Card, CardGrid, Container, Header } from "./Elements";
+import "./App.css";
+import Menu from "./Menu";
+import blue from "./blue.png";
+import purp from "./purp.png";
+import black from "./black.png";
+import green from "./green.png";
+
+function App() {
+  return (
+    <div>
+      <Header>
+        <Menu />
+        <h1>Header</h1>
+      </Header>
+      <Container>
+        <h2>Super Cool</h2>
+        <CardGrid>
+          <Card style={{ background: "var(--purp)" }}>
+            <h3>Some card</h3>
+            <img src={purp} />
+          </Card>
+          <Card style={{ background: "var(--blue)" }}>
+            <h3>Some card</h3>
+            <img src={blue} />
+          </Card>
+          <Card style={{ background: "var(--black)" }}>
+            <h3>Some card</h3>
+            <img src={black} />
+          </Card>
+          <Card style={{ background: "var(--green)" }}>
+            <h3>Some card</h3>
+            <img src={green} />
+          </Card>
+        </CardGrid>
+      </Container>
+    </div>
+  );
+}
+
+export default App;
