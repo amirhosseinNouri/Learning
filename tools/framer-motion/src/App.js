bbb2afe0b5b08dfe1da1d29487a853f27e0646cf
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2024-08-30 07:55:11 +0330

    motion element

--- tools/framer-motion/src/App.js
+++ tools/framer-motion/src/App.js
@@ -1,15 +1,22 @@
-import React from "react";
-import { Card, CardGrid, Container, Header } from "./Elements";
-import "./App.css";
-import Menu from "./Menu";
-import blue from "./blue.png";
-import purp from "./purp.png";
-import black from "./black.png";
-import green from "./green.png";
+import React from 'react';
+import { Card, CardGrid, Container, Header } from './Elements';
+import './App.css';
+import Menu from './Menu';
+import blue from './blue.png';
+import purp from './purp.png';
+import black from './black.png';
+import green from './green.png';
+import { motion } from 'framer-motion';
 
 function App() {
   return (
-    <div>
+    <motion.div
+      initial={{ x: 200, y: 100, opacity: 0 }}
+      animate={{ x: 0, y: 0, opacity: 1 }}
+      transition={{
+        duration: 3,
+      }}
+    >
       <Header>
         <Menu />
         <h1>Header</h1>
@@ -17,25 +24,25 @@ function App() {
       <Container>
         <h2>Super Cool</h2>
         <CardGrid>
-          <Card style={{ background: "var(--purp)" }}>
+          <Card style={{ background: 'var(--purp)' }}>
             <h3>Some card</h3>
             <img src={purp} />
           </Card>
-          <Card style={{ background: "var(--blue)" }}>
+          <Card style={{ background: 'var(--blue)' }}>
             <h3>Some card</h3>
             <img src={blue} />
           </Card>
-          <Card style={{ background: "var(--black)" }}>
+          <Card style={{ background: 'var(--black)' }}>
             <h3>Some card</h3>
             <img src={black} />
           </Card>
-          <Card style={{ background: "var(--green)" }}>
+          <Card style={{ background: 'var(--green)' }}>
             <h3>Some card</h3>
             <img src={green} />
           </Card>
         </CardGrid>
       </Container>
-    </div>
+    </motion.div>
   );
 }
 
