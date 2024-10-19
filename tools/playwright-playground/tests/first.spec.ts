5f7582f7f51408b22b4cd5a4112a2525a729c963
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2024-10-19 10:28:22 +0330

    add playwright to the project

---
+++ tools/playwright-playground/tests/first.spec.ts
@@ -0,0 +1,7 @@
+import { test, expect } from '@playwright/test';
+
+test('the first test', async ({ page }) => {
+  await page.goto('http://localhost:4200/');
+  await page.getByText('Forms').click();
+  await page.getByText('Form Layouts').click();
+});
