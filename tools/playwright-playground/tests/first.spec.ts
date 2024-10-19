a427f97341380f46c8e662857eed24aac1abc3aa
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2024-10-19 12:14:50 +0330

    beforeEach hook

--- tools/playwright-playground/tests/first.spec.ts
+++ tools/playwright-playground/tests/first.spec.ts
@@ -1,7 +1,14 @@
 import { test, expect } from '@playwright/test';
 
-test('the first test', async ({ page }) => {
+test.beforeEach(async ({ page }) => {
   await page.goto('http://localhost:4200/');
   await page.getByText('Forms').click();
+});
+
+test('the first test', async ({ page }) => {
   await page.getByText('Form Layouts').click();
+});
+
+test('navigate to datepicker page', async ({ page }) => {
+  await page.getByText('Datepicker').click();
 });
