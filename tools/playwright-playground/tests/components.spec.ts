df2079216c45479530a9538fc5e9e43dcddc60f3
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2024-10-22 23:24:07 +0330

    table

--- tools/playwright-playground/tests/components.spec.ts
+++ tools/playwright-playground/tests/components.spec.ts
@@ -142,3 +142,35 @@ test('dialog boxes', async ({ page }) => {
     'mdo@gmail.com',
   );
 });
+
+test('tables', async ({ page }) => {
+  await page.getByText('Tables & Data').click();
+  await page.getByText('Smart Table').click();
+
+  const targetRow = await page
+    .getByRole('table')
+    .locator('tr', { hasText: 'twitter@outlook.com' });
+
+  await targetRow.locator('.nb-edit').click();
+
+  await page.locator('input-editor').getByPlaceholder('Age').clear();
+  await page.locator('input-editor').getByPlaceholder('Age').fill('35');
+  await page.locator('.nb-checkmark').click();
+
+  await page.locator('.ng2-smart-pagination-nav').getByText('2').click();
+
+  const targetRowById = await page
+    .getByRole('row', { name: '11' })
+    .filter({ has: page.locator('td').nth(1).getByText('11') });
+
+  await targetRowById.locator('.nb-edit').click();
+
+  await page.locator('input-editor').getByPlaceholder('E-mail').clear();
+  await page
+    .locator('input-editor')
+    .getByPlaceholder('E-mail')
+    .fill('test@gmail.com');
+  await page.locator('.nb-checkmark').click();
+
+  await expect(targetRowById.locator('td').nth(5)).toHaveText('test@gmail.com');
+});
