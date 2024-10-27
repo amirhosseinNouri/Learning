1ced32633f22dfc2b86f829b3c0f8f69517e1c54
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2024-10-27 16:22:02 +0330

    reusable valid brand

--- type-script/total-typescript-2/advanced-patterns/src/01-branded-types/04-reusable-valid-brand.problem.ts
+++ type-script/total-typescript-2/advanced-patterns/src/01-branded-types/04-reusable-valid-brand.problem.ts
@@ -1,33 +1,34 @@
-import { it } from "vitest";
-import { Brand } from "../helpers/Brand";
+import { it } from 'vitest';
+import { Brand } from '../helpers/Brand';
+import { P } from 'vitest/dist/types-ad1c3f45';
 
-type Valid<T> = unknown;
+type Valid<T> = Brand<T, 'Valid'>;
 
 interface PasswordValues {
   password: string;
   confirmPassword: string;
 }
 
-const validatePassword = (values: PasswordValues) => {
+const validatePassword = (values: PasswordValues): Valid<PasswordValues> => {
   if (values.password !== values.confirmPassword) {
-    throw new Error("Passwords do not match");
+    throw new Error('Passwords do not match');
   }
 
-  return values;
+  return values as Valid<PasswordValues>;
 };
 
 const createUserOnApi = (values: Valid<PasswordValues>) => {
   // Imagine this function creates the user on the API
 };
 
-it("Should fail if you do not validate the values before calling createUserOnApi", () => {
+it('Should fail if you do not validate the values before calling createUserOnApi', () => {
   const onSubmitHandler = (values: PasswordValues) => {
     // @ts-expect-error
     createUserOnApi(values);
   };
 });
 
-it("Should succeed if you DO validate the values before calling createUserOnApi", () => {
+it('Should succeed if you DO validate the values before calling createUserOnApi', () => {
   const onSubmitHandler = (values: PasswordValues) => {
     const validatedValues = validatePassword(values);
     createUserOnApi(validatedValues);
