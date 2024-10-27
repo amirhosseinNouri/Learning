9aa741de283603c0dd3bc369e1f758fa782ffd3b
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2024-10-27 16:14:19 +0330

    entity fetching problem

--- type-script/total-typescript-2/advanced-patterns/src/01-branded-types/03-entity-fetching.problem.ts
+++ type-script/total-typescript-2/advanced-patterns/src/01-branded-types/03-entity-fetching.problem.ts
@@ -1,8 +1,8 @@
-import { it } from "vitest";
-import { Brand } from "../helpers/Brand";
+import { it } from 'vitest';
+import { Brand } from '../helpers/Brand';
 
-type UserId = Brand<string, "UserId">;
-type PostId = Brand<string, "PostId">;
+type UserId = Brand<string, 'UserId'>;
+type PostId = Brand<string, 'PostId'>;
 
 interface User {
   id: string;
@@ -18,36 +18,36 @@ interface Post {
 const db: { users: User[]; posts: Post[] } = {
   users: [
     {
-      id: "1",
-      name: "Miles",
+      id: '1',
+      name: 'Miles',
     },
   ],
   posts: [
     {
-      id: "1",
-      title: "Hello world",
-      content: "This is my first post",
+      id: '1',
+      title: 'Hello world',
+      content: 'This is my first post',
     },
   ],
 };
 
-const getUserById = (id: string) => {
+const getUserById = (id: UserId) => {
   return db.users.find((user) => user.id === id);
 };
 
-const getPostById = (id: string) => {
+const getPostById = (id: PostId) => {
   return db.posts.find((post) => post.id === id);
 };
 
-it("Should only let you get a user by id with a user id", () => {
-  const postId = "1" as PostId;
+it('Should only let you get a user by id with a user id', () => {
+  const postId = '1' as PostId;
 
   // @ts-expect-error
   getUserById(postId);
 });
 
-it("Should only let you get a post by id with a PostId", () => {
-  const userId = "1" as UserId;
+it('Should only let you get a post by id with a PostId', () => {
+  const userId = '1' as UserId;
 
   // @ts-expect-error
   getPostById(userId);
