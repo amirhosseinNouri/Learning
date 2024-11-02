6e8e1dd62d99116fcfe22463f0e3f1a5f9485fe2
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2024-11-02 12:41:10 +0330

    mock API

--- tools/playwright-playground/tests/api.spec.ts
+++ tools/playwright-playground/tests/api.spec.ts
@@ -1,11 +1,36 @@
 import { expect, test } from '@playwright/test';
 
+const mockPosts = [
+  {
+    userId: 1,
+    id: 1,
+    title: 'test 1',
+    body: 'test 1 body',
+  },
+  {
+    userId: 1,
+    id: 2,
+    title: 'test 2',
+    body: 'test 2 body',
+  },
+  {
+    userId: 1,
+    id: 3,
+    title: 'test 3',
+    body: 'test 3 body',
+  },
+];
+
 test.beforeEach(async ({ page }) => {
+  await page.route('**/posts', (route) => {
+    route.fulfill({ body: JSON.stringify(mockPosts) });
+  });
+
   await page.goto('https://jsonplaceholder.typicode.com/posts');
 });
 
 test('shows posts page', async ({ page }) => {
-  await expect(
-    page.locator('[class="json-formatter-container"]'),
-  ).toBeInViewport();
+  await expect(page.locator('pre')).toBeInViewport();
 });
+
+test('mock posts api', async ({ page }) => {});
