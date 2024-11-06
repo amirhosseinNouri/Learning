16c0ef86ee3afef4d8cf699617a2e103591c270b
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2024-11-06 12:40:48 +0330

    usage with zod

--- type-script/total-typescript-2/advanced-patterns/src/05-external-libraries/26-usage-with-zod.problem.ts
+++ type-script/total-typescript-2/advanced-patterns/src/05-external-libraries/26-usage-with-zod.problem.ts
@@ -1,11 +1,11 @@
-import { expect, it } from "vitest";
-import { z } from "zod";
+import { expect, it } from 'vitest';
+import { z } from 'zod';
 
-const makeZodSafeFunction = (
-  schema: unknown,
-  func: (arg: unknown) => unknown
+const makeZodSafeFunction = <TArg, TResult>(
+  schema: z.ZodObject<any, any, any, TArg>,
+  func: (arg: TArg) => TResult,
 ) => {
-  return (arg: unknown) => {
+  return (arg: TArg) => {
     const result = schema.parse(arg);
     return func(result);
   };
@@ -18,18 +18,18 @@ const addTwoNumbersArg = z.object({
 
 const addTwoNumbers = makeZodSafeFunction(
   addTwoNumbersArg,
-  (args) => args.a + args.b
+  (args) => args.a + args.b,
 );
 
-it("Should error on the type level AND the runtime if you pass incorrect params", () => {
+it('Should error on the type level AND the runtime if you pass incorrect params', () => {
   expect(() =>
     addTwoNumbers(
       // @ts-expect-error
-      { a: 1, badParam: 3 }
-    )
+      { a: 1, badParam: 3 },
+    ),
   ).toThrow();
 });
 
-it("Should succeed if you pass the correct type", () => {
+it('Should succeed if you pass the correct type', () => {
   expect(addTwoNumbers({ a: 1, b: 2 })).toBe(3);
 });
