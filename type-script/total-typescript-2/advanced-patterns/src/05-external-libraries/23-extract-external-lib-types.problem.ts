36e1aa39675a73a71b526cf29d34b9089cf4e202
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2024-11-06 12:04:00 +0330

    extract external lib types

--- type-script/total-typescript-2/advanced-patterns/src/05-external-libraries/23-extract-external-lib-types.problem.ts
+++ type-script/total-typescript-2/advanced-patterns/src/05-external-libraries/23-extract-external-lib-types.problem.ts
@@ -1,14 +1,18 @@
-import { fetchUser } from "fake-external-lib";
-import { Equal, Expect, ExpectExtends } from "../helpers/type-utils";
+import { fetchUser } from 'fake-external-lib';
+import { Equal, Expect, ExpectExtends } from '../helpers/type-utils';
 
 /**
  * We're using a function from fake-external lib, but we need
  * to extend the types. Extract the types below.
  */
 
-type ParametersOfFetchUser = unknown;
+type ParametersOfFetchUser = Parameters<typeof fetchUser>;
 
-type ReturnTypeOfFetchUserWithFullName = unknown;
+type ReturnTypeOfFetchUserWithFullName = Awaited<
+  ReturnType<typeof fetchUser>
+> & {
+  fullName: string;
+};
 
 export const fetchUserWithFullName = async (
   ...args: ParametersOfFetchUser
