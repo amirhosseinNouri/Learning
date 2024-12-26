eef087ec35ed93828debe9c4644eae12b4b57f36
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2024-12-26 11:53:10 +0330

    add some other usecases

--- patterns/ioc/src/index.ts
+++ patterns/ioc/src/index.ts
@@ -1,18 +1,68 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-const filter = <TArray extends unknown[]>(array: TArray): TArray => {
+interface Options {
+  filterNull?: boolean;
+  filterUndefined?: boolean;
+  filterEmptyString?: boolean;
+  filterZero?: boolean;
+}
+
+const filter = <TArray extends unknown[]>(
+  array: TArray,
+  {
+    filterEmptyString = false,
+    filterNull = true,
+    filterUndefined = true,
+    filterZero = false,
+  }: Options = {},
+): TArray => {
   const result = [];
 
   for (let i = 0; i < array.length; i++) {
     const element = array[i];
 
-    if (element !== null && element !== undefined) {
-      result.push(element);
+    // if (element !== null && element !== undefined) {
+    //   result.push(element);
+    // }
+
+    if (
+      (filterNull && element === null) ||
+      (filterUndefined && element === undefined) ||
+      (filterEmptyString && element === '') ||
+      (filterZero && element === 0)
+    ) {
+      continue;
     }
+
+    result.push(element);
   }
 
   return result as TArray;
 };
 
-console.log(filter([0, 1, 2, 3, null, undefined, 4, 5, 6, '']));
+console.log('Default: ', filter([0, 1, undefined, 2, null, 3, 'four', '']));
+
+console.log(
+  'Dot not filter null: ',
+  filter([0, 1, undefined, 2, null, 3, 'four', ''], {
+    filterNull: false,
+  }),
+);
+
+console.log(
+  'Do not filter undefined: ',
+  filter([0, 1, undefined, 2, null, 3, 'four', ''], { filterUndefined: false }),
+);
+
+console.log(
+  'Filter zeros: ',
+  filter([0, 1, undefined, 2, null, 3, 'four', ''], { filterZero: true }),
+);
+
+console.log(
+  'Filter empty strings: ',
+  filter([0, 1, undefined, 2, null, 3, 'four', ''], {
+    filterEmptyString: true,
+  }),
+);
