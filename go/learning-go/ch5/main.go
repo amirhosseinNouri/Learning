9a415cfc4e7a62759496f082fe10b4e5a528c35b
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2026-03-12 18:57:43 +0330

    functions are values

--- go/learning-go/ch5/main.go
+++ go/learning-go/ch5/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"errors"
 	"fmt"
+	"strconv"
 )
 
 func main() {
@@ -16,6 +17,7 @@ func main() {
 	fmt.Println(addTo(1, a...))
 	fmt.Println(addTo(1, []int{1, 2}...))
 
+	fmt.Println()
 	fmt.Println("------- Named return values -------")
 	res, rem, err := divAndReminder(10, 2)
 	if err != nil {
@@ -24,6 +26,82 @@ func main() {
 	}
 	fmt.Println(res, rem)
 
+	fmt.Println()
+	fmt.Println("------- Functions are values -------")
+	calculator()
+
+}
+
+type OpFunc func(a int, b int) int
+
+func calculator() {
+	opMap := map[string]OpFunc{
+		"+": add,
+		"-": sub,
+		"*": mul,
+		"/": div,
+	}
+
+	exps := [][]string{
+		{"2", "+", "3"},
+		{"2", "-", "3"},
+		{"2", "*", "3"},
+		{"4", "/", "2"},
+		{"2"},
+		{"a", "+", "2"},
+		{"2", "+", "b"},
+		{"4", "%", "2"},
+	}
+
+	for _, exp := range exps {
+		if len(exp) != 3 {
+			fmt.Println("Invalid expression: ", exp)
+			continue
+		}
+
+		p1, err := strconv.Atoi(exp[0])
+
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+
+		op := exp[1]
+		opFunc, ok := opMap[op]
+
+		if !ok {
+			fmt.Println("Unsupported operator: ", op)
+			continue
+		}
+
+		p2, err := strconv.Atoi(exp[2])
+
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+
+		res := opFunc(p1, p2)
+		fmt.Printf("%d %s %d = %d", p1, op, p2, res)
+		fmt.Println()
+	}
+
+}
+
+func add(a int, b int) int {
+	return a + b
+}
+
+func sub(a int, b int) int {
+	return a - b
+}
+
+func mul(a int, b int) int {
+	return a * b
+}
+
+func div(a int, b int) int {
+	return a / b
 }
 
 func addTo(base int, vals ...int) []int {
