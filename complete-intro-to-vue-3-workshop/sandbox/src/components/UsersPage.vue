a5527391d94674b45f9405315326e493ebca8c3f
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2026-06-17 23:49:18 +0330

    add ref to composition api

--- complete-intro-to-vue-3-workshop/sandbox/src/components/UsersPage.vue
+++ complete-intro-to-vue-3-workshop/sandbox/src/components/UsersPage.vue
@@ -1,18 +1,35 @@
 <script lang="ts">
+import { computed, ref } from 'vue'
+
 export default {
   async setup() {
+    const title = ref('Users')
     await new Promise((resolve) => setTimeout(resolve, 1000))
     const users = await fetch('https://jsonplaceholder.typicode.com/users')
     const usersData = await users.json()
+
+    const titleAllCaps = computed(() => {
+      return title.value.toUpperCase()
+    })
+
     return {
       users: usersData,
+      title,
+      titleAllCaps,
     }
+  },
+  methods: {
+    changeTitle() {
+      this.title = 'Title Updated'
+    },
   },
 }
 </script>
 
 <template>
-  <h1>Users Page</h1>
+  <h1>{{ title }}</h1>
+  <h3>{{ titleAllCaps }}</h3>
+  <button @click="changeTitle">Change title</button>
   <ul>
     <li v-for="user in users" :key="user.id">
       <p>{{ user.name }}: {{ user.email }}</p>
